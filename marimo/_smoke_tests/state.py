0bb8e4570ec3b31ad2f3a51572444cef7eadd1ef
Akshay Agrawal <akshay@marimo.io>
2023-08-24 07:59:59 -0700

    working with UI elements

--- marimo/_smoke_tests/state.py
+++ marimo/_smoke_tests/state.py
@@ -11,6 +11,12 @@ def __():
 
 
 @app.cell
+def __():
+    import random
+    return random,
+
+
+@app.cell
 def __(mo):
     get_state, set_state = mo.state(0)
     return get_state, set_state
@@ -29,9 +35,13 @@ def __(get_state):
 
 
 @app.cell
-def __(get_state):
-    get_state()
-    return
+def __(mo, set_state):
+    _on_click = lambda _: set_state(lambda v: v + 1)
+    button = mo.ui.button(
+        value=0, on_click=_on_click
+    )
+    button
+    return button,
 
 
 if __name__ == "__main__":
