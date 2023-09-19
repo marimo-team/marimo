60c6ffc003c6fc425c56caabb1fb4dadce1218a9
Akshay Agrawal <akshay@marimo.io>
2023-09-19 16:44:55 -0700

    format marimo variable values

---
+++ marimo/_messaging/test_message_types.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Marimo. All rights reserved.
+from marimo._messaging.message_types import VariableValue
+from marimo._output.hypertext import Html
+from marimo._plugins.ui._impl.input import slider
+
+
+def test_value_ui_element() -> None:
+    variable_value = VariableValue(name="s", value=slider(1, 10, value=5))
+    assert variable_value.datatype == "slider"
+    assert variable_value.value == "5"
+
+
+def test_value_html() -> None:
+    h = Html("<span></span>")
+    variable_value = VariableValue(name="h", value=h)
+    assert variable_value.datatype == "Html"
+    assert variable_value.value == h.text
