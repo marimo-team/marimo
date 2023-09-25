ad032870dce664daad52e04bedc22791909cd17d
Akshay Agrawal <akshay@marimo.io>
2023-09-25 15:17:15 -0700

    minor styling of config menu (#151)
    * minor styling of config menu
    
    make distinction bewteen user/app config clear
    
    clean up spacing
    
    * typo

--- frontend/src/components/app-config/common.tsx
+++ frontend/src/components/app-config/common.tsx
@@ -3,8 +3,22 @@ import { PropsWithChildren } from "react";
 
 export const SettingTitle: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="text-sm font-semibold text-muted-foreground uppercase tracking-wide">
+    <div className="text-sm font-semibold text-muted-foreground uppercase tracking-wide  mb-1">
       {children}
     </div>
   );
+};
+
+export const SettingSubtitle: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <div className="text-sm font-semibold underline decoration-solid decoration-gray-400 underline-offset-2 text-muted-foreground uppercase tracking-wide">
+      {children}
+    </div>
+  );
+};
+
+export const SettingDescription: React.FC<PropsWithChildren> = ({
+  children,
+}) => {
+  return <p className="text-sm text-muted-foreground">{children}</p>;
 };
