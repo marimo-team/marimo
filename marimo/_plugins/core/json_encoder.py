5aa47d518bf9106e83aad9f9412e23dccfb01a11
Myles Scolnick <myles@marimo.io>
2023-10-10 17:29:44 -0400

    improvement: auto-encoding pandas/numpy, mo.ui.table takes in Dataframe

--- marimo/_plugins/core/json_encoder.py
+++ marimo/_plugins/core/json_encoder.py
@@ -2,11 +2,54 @@
 from json import JSONEncoder
 from typing import Any
 
+_is_numpy_installed = False
+try:
+    import numpy  # noqa: F401
+
+    _is_numpy_installed = True
+except ImportError:
+    pass
+
+_is_pandas_installed = False
+try:
+    import pandas  # noqa: F401
+
+    _is_pandas_installed = True
+except ImportError:
+    pass
+
 
 class WebComponentEncoder(JSONEncoder):
     """Custom JSON encoder for WebComponents"""
 
     def default(self, obj: Any) -> Any:
+        # Handle numpy objects
+        if _is_numpy_installed:
+            import numpy as np
+
+            dtypes = (np.datetime64, np.complexfloating)
+            if isinstance(obj, dtypes):
+                return str(obj)
+            elif isinstance(obj, np.integer):
+                return int(obj)
+            elif isinstance(obj, np.floating):
+                return float(obj)
+            elif isinstance(obj, np.ndarray):
+                if any([np.issubdtype(obj.dtype, i) for i in dtypes]):
+                    return obj.astype(str).tolist()
+                return obj.tolist()
+
+        # Handle pandas objects
+        if _is_pandas_installed:
+            import pandas as pd
+
+            if isinstance(obj, pd.DataFrame):
+                return obj.to_dict("records")
+            elif isinstance(obj, pd.Series):
+                return obj.to_list()
+            elif isinstance(obj, pd.Timestamp):
+                return str(obj)
+
         # Handle MIME objects
         if hasattr(obj, "_mime_"):
             (mimetype, data) = obj._mime_()
---
+++ marimo/_plugins/ui/_impl/table.py
@@ -0,0 +1,153 @@
+# Copyright 2023 Marimo. All rights reserved.
+from __future__ import annotations
+
+from typing import (
+    TYPE_CHECKING,
+    Callable,
+    Final,
+    List,
+    Literal,
+    Optional,
+    Sequence,
+    Union,
+    cast,
+)
+
+from marimo import _loggers
+from marimo._output.mime import MIME
+from marimo._output.rich_help import mddoc
+from marimo._plugins.ui._core.ui_element import UIElement
+
+LOGGER = _loggers.marimo_logger()
+
+Numeric = Union[int, float]
+
+if TYPE_CHECKING:
+    import pandas as pd
+
+TableData = Union[
+    list[dict[str, str]],
+    Sequence[str | int | float | bool | MIME | None],
+    Sequence[dict[str, str | int | float | bool | MIME | None]],
+    "pd.DataFrame",
+]
+
+
+@mddoc
+class table(UIElement[List[str], List[object]]):
+    """
+    A table component.
+
+    **Example.**
+
+    ```python
+    table = mo.ui.table(
+      data=[
+        {'first_name': 'Michael', 'last_name': 'Scott'},
+        {'first_name': 'Dwight', 'last_name': 'Schrute'}
+      ],
+      label='Users'
+    )
+    ```
+
+    ```python
+    # df is a Pandas dataframe
+    table = mo.ui.table(
+        data=df.to_dict('records'),
+        # use pagination when your table has many rows
+        pagination=True,
+        label='Dataset'
+    )
+    ```
+
+    **Attributes.**
+
+    - `value`: the selected values, or `None` if no selection.
+    - `data`: the table data
+
+    **Initialization Args.**
+
+    - `data`:  a list of values representing a column, or a list of dicts
+        where each dict represents a row in the table
+        (mapping column names to values). values can be
+        primitives (`str`, `int`, `float`, `bool`, or `None`)
+        or Marimo elements: e.g.
+        `mo.ui.button(...)`, `mo.md(...)`, `mo.as_html(...)`, etc.
+    - `pagination`: whether to paginate; if `False`, all rows will be shown
+      defaults to `True` when above 10 rows, `False` otherwise
+    - `selection`: 'single' or 'multi' to enable row selection, or `None` to
+        disable
+    - `label`: text label for the element
+    - `on_change`: optional callback to run when this element's value changes
+    """
+
+    _name: Final[str] = "marimo-table"
+
+    def __init__(
+        self,
+        data: TableData,
+        pagination: Optional[bool] = None,
+        selection: Optional[Literal["single", "multi"]] = "multi",
+        *,
+        label: str = "",
+        on_change: Optional[Callable[[List[object]], None]] = None,
+    ) -> None:
+        self._data = data
+        normalized_data = _normalize_data(data)
+
+        # pagination defaults to True if there are more than 10 rows
+        if pagination is None:
+            pagination = len(self._data) > 10
+
+        super().__init__(
+            component_name=table._name,
+            label=label,
+            initial_value=[],
+            args={
+                "data": normalized_data,
+                "pagination": pagination,
+                "selection": selection,
+            },
+            on_change=on_change,
+        )
+
+    @property
+    def data(
+        self,
+    ) -> TableData:
+        return self._data
+
+    def _convert_value(self, value: list[str]) -> list[object]:
+        return [self._data[int(v)] for v in value]
+
+
+def _normalize_data(data: TableData) -> list[dict[str, object]]:
+    # Handle pandas
+    try:
+        import pandas as pd
+
+        if isinstance(data, pd.DataFrame):
+            return data.to_dict("records")
+    except ImportError:
+        pass
+
+    # Assert that data is a list
+    if not isinstance(data, (list, tuple)):
+        raise ValueError("data must be a list or tuple.")
+
+    # Handle single-column data
+    if not isinstance(data[0], dict) and isinstance(
+        data[0], (str, int, float, bool, type(None))
+    ):
+        # we're going to assume that data has the right shape, after
+        # having checked just the first entry
+        casted = cast(List[Union[str, int, float, bool, MIME, None]], data)
+        return [{"value": datum} for datum in casted]
+    elif not isinstance(data[0], dict):
+        raise ValueError(
+            "data must be a sequence of JSON-serializable types, or a "
+            "sequence of dicts."
+        )
+
+    # Sequence of dicts
+    return data
