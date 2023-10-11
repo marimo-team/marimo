87d08a69b986b5fd6797a6f2b52175b04027192d
Akshay Agrawal <akshay@marimo.io>
2023-10-10 20:46:19 -0700

    remove selection public property, typecheck

--- marimo/_plugins/ui/_impl/chart.py
+++ marimo/_plugins/ui/_impl/chart.py
@@ -23,7 +23,7 @@ from marimo._utils import flatten
 LOGGER = _loggers.marimo_logger()
 
 if TYPE_CHECKING:
-    import altair
+    import altair  # type:ignore[import]
     import pandas as pd
 
 # Selection is a dictionary of the form:
@@ -36,119 +36,14 @@ ChartSelection = Dict[str, Dict[str, Union[List[int], List[float], List[str]]]]
 VegaSpec = Dict[str, Any]
 
 
-@mddoc
-class chart(UIElement[ChartSelection, "pd.DataFrame"]):
-    """Make reactive charts with Altair
-
-    Use `mo.ui.chart` to make Altair charts reactive: select chart data points
-    with your cursor on the frontend, get it as a Pandas dataframe in Python!
-
-    **Example.**
-
-    ```python
-    import altair as alt
-    import marimo as mo
-    from vega_datasets import data
-
-    chart = alt.Chart(data.cars()).mark_point().encode(
-        x='Horsepower',
-        y='Miles_per_Gallon',
-        color='Origin',
-    )
-
-    chart = mo.ui.chart(chart)
-    ```
-
-    ```
-    # View the chart and selected data as a dataframe
-    mo.hstack([chart, chart.value])
-    ```
-
-    **Attributes.**
-
-    - `value`: the data if the chart has any selections
-    - `selections`: the selection of the chart; this may be an interval along
-        the name of an axis or a selection of points
-    - `dataframe`: a Pandas dataframe of the unfiltered chart data
-
-    **Initialization Args.**
-
-    - `chart`: An `altair.Chart` or a Vega-lite spec
-    - `chart_selection`: optional selection type,
-        `"point"`, `"interval"`, or a bool; defaults to `True` which will
-        automatically detect the best selection type
-    - `field_selection`: optional list of fields (columns) for which to
-        enable selecton, `True` to enable selection for all legend items, or
-        `False` to disable selection entirely
-    - `label`: optional text label for the element
-    - `on_change`: optional callback to run when this element's value changes
-    """
-
-    name: Final[str] = "marimo-vega"
-
-    def __init__(
-        self,
-        figure: Union[str, altair.Chart, VegaSpec],
-        chart_selection: Union[
-            Literal["point"], Literal["interval"], bool
-        ] = True,
-        field_selection: Union[List[str], bool] = True,
-        *,
-        label: str = "",
-        on_change: Optional[Callable[[pd.DataFrame], None]] = None,
-    ) -> None:
-        vega_spec = _parse_spec(figure)
-        self.dataframe = _to_dataframe(vega_spec)
-
-        if label:
-            vega_spec["title"] = label
-
-        # Automatically add full width
-        if "width" not in vega_spec:
-            vega_spec["width"] = "container"
-        if "vconcat" in vega_spec:
-            for chart in vega_spec["vconcat"]:
-                if "width" not in chart:
-                    chart["width"] = "container"
-            # without autosize, vconcat will overflow
-            if "autosize" not in vega_spec:
-                vega_spec["autosize"] = "fit-x"
-
-        # Selection for binned charts is not yet implemented
-        if _has_binning(vega_spec):
-            sys.stderr.write(
-                "Binning + selection is not yet supported in "
-                "marimo.ui.chart.\n"
-                "If you'd like this feature, please file an issue: "
-                "https://github.com/marimo-team/marimo/issues\n"
-            )
-            chart_selection = False
-            field_selection = False
-
-        super().__init__(
-            component_name="marimo-vega",
-            initial_value={},
-            label=label,
-            args={
-                "spec": vega_spec,
-                "chart-selection": chart_selection,
-                "field-selection": field_selection,
-            },
-            on_change=on_change,
-        )
-
-    def _convert_value(self, value: ChartSelection) -> Any:
-        self._chart_selection = value
-        flat, _ = flatten.flatten(value)
-        if not value or not flat:
-            import pandas as pd
-
-            return pd.DataFrame()
-        return _filter_dataframe(self.dataframe, value)
-
-    @property
-    def selections(self) -> ChartSelection:
-        return self._chart_selection
+def _has_binning(spec: VegaSpec) -> bool:
+    """Return True if the spec has binning."""
+    if "encoding" not in spec:
+        return False
+    for encoding in spec["encoding"].values():
+        if "bin" in encoding:
+            return True
+    return False
 
 
 def _to_dataframe(vega_spec: VegaSpec) -> pd.DataFrame:
@@ -246,11 +141,110 @@ def _parse_spec(spec: Union[str, altair.Chart, VegaSpec]) -> VegaSpec:
     raise ValueError("Invalid Vega-Lite spec") from None
 
 
-def _has_binning(spec: VegaSpec) -> bool:
-    """Return True if the spec has binning."""
-    if "encoding" not in spec:
-        return False
-    for encoding in spec["encoding"].values():
-        if "bin" in encoding:
-            return True
-    return False
+@mddoc
+class chart(UIElement[ChartSelection, "pd.DataFrame"]):
+    """Make reactive charts with Altair
+
+    Use `mo.ui.chart` to make Altair charts reactive: select chart data points
+    with your cursor on the frontend, get it as a Pandas dataframe in Python!
+
+    **Example.**
+
+    ```python
+    import altair as alt
+    import marimo as mo
+    from vega_datasets import data
+
+    chart = alt.Chart(data.cars()).mark_point().encode(
+        x='Horsepower',
+        y='Miles_per_Gallon',
+        color='Origin',
+    )
+
+    chart = mo.ui.chart(chart)
+    ```
+
+    ```
+    # View the chart and selected data as a dataframe
+    mo.hstack([chart, chart.value])
+    ```
+
+    **Attributes.**
+
+    - `value`: a Pandas dataframe of the plot data filtered by the selections
+    - `dataframe`: a Pandas dataframe of the unfiltered chart data
+
+    **Initialization Args.**
+
+    - `chart`: An `altair.Chart` or a Vega-lite spec
+    - `chart_selection`: optional selection type,
+        `"point"`, `"interval"`, or a bool; defaults to `True` which will
+        automatically detect the best selection type
+    - `legend_selection`: optional list of legend fields (columns) for which to
+        enable selecton, `True` to enable selection for all fields, or
+        `False` to disable selection entirely
+    - `label`: optional text label for the element
+    - `on_change`: optional callback to run when this element's value changes
+    """
+
+    name: Final[str] = "marimo-vega"
+
+    def __init__(
+        self,
+        figure: Union[str, altair.Chart, VegaSpec],
+        chart_selection: Union[
+            Literal["point"], Literal["interval"], bool
+        ] = True,
+        legend_selection: Union[List[str], bool] = True,
+        *,
+        label: str = "",
+        on_change: Optional[Callable[[pd.DataFrame], None]] = None,
+    ) -> None:
+        vega_spec = _parse_spec(figure)
+        self.dataframe = _to_dataframe(vega_spec)
+
+        if label:
+            vega_spec["title"] = label
+
+        # Automatically add full width
+        if "width" not in vega_spec:
+            vega_spec["width"] = "container"
+        if "vconcat" in vega_spec:
+            for chart in vega_spec["vconcat"]:
+                if "width" not in chart:
+                    chart["width"] = "container"
+            # without autosize, vconcat will overflow
+            if "autosize" not in vega_spec:
+                vega_spec["autosize"] = "fit-x"
+
+        # Selection for binned charts is not yet implemented
+        if _has_binning(vega_spec):
+            sys.stderr.write(
+                "Binning + selection is not yet supported in "
+                "marimo.ui.chart.\n"
+                "If you'd like this feature, please file an issue: "
+                "https://github.com/marimo-team/marimo/issues\n"
+            )
+            chart_selection = False
+            legend_selection = False
+
+        super().__init__(
+            component_name="marimo-vega",
+            initial_value={},
+            label=label,
+            args={
+                "spec": vega_spec,
+                "chart-selection": chart_selection,
+                "field-selection": legend_selection,
+            },
+            on_change=on_change,
+        )
+
+    def _convert_value(self, value: ChartSelection) -> Any:
+        self._chart_selection = value
+        flat, _ = flatten.flatten(value)
+        if not value or not flat:
+            import pandas as pd
+
+            return pd.DataFrame()
+        return _filter_dataframe(self.dataframe, value)
