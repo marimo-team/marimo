ac65922ed10e3c05b2c47543fa02ae8fb1d67f06
Akshay Agrawal <akshay@marimo.io>
2023-10-21 09:15:46 -0700

    Register run start for rpc call (#236)
    - backend sends a completed-run once RPC is done

--- frontend/src/core/functions/FunctionRegistry.ts
+++ frontend/src/core/functions/FunctionRegistry.ts
@@ -1,5 +1,6 @@
 /* Copyright 2023 Marimo. All rights reserved. */
 
+import { RuntimeState } from "@/core/RuntimeState";
 import { sendFunctionRequest } from "@/core/network/requests";
 import { FunctionCallResultMessage } from "../kernel/messages";
 import { DeferredRequestRegistry } from "../network/DeferredRequestRegistry";
@@ -9,6 +10,8 @@ export const FUNCTIONS_REGISTRY = new DeferredRequestRegistry<
   Omit<SendFunctionRequest, "functionCallId">,
   FunctionCallResultMessage
 >("function-call-result", async (requestId, req) => {
+  // RPC counts as a kernel invocation
+  RuntimeState.INSTANCE.registerRunStart();
   await sendFunctionRequest({
     functionCallId: requestId,
     ...req,
