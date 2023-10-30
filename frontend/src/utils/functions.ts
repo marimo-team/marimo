84df6666a1cadceae6859718506e1bff097993a2
Myles Scolnick <myles@marimo.io>
2023-10-30 13:35:00 -0400

    fix: fix stale values in DictPlugin and allow re-submitting in FormPLugin

--- frontend/src/utils/functions.ts
+++ frontend/src/utils/functions.ts
@@ -6,4 +6,9 @@ export const Functions = {
   THROW: () => {
     throw new Error("Should not be called");
   },
+  asUpdater: <T>(updater: T | ((value: T) => T)): ((value: T) => T) => {
+    return typeof updater === "function"
+      ? (updater as (value: T) => T)
+      : () => updater;
+  },
 };
