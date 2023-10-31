78658bfb931f93c122256d8c506b778c806b3fa7
Myles Scolnick <myles@marimo.io>
2023-10-31 13:14:24 -0400

    add floating copy button to mo.ui.dataframe (#280)
    * add floating copy button to mo.ui.dataframe
    
    * dark mode

--- frontend/src/plugins/impl/data-frames/python/code-panel.tsx
+++ frontend/src/plugins/impl/data-frames/python/code-panel.tsx
@@ -2,8 +2,13 @@
 import React from "react";
 import { Transformations } from "../schema";
 import { python } from "@codemirror/lang-python";
-import CodeMirror from "@uiw/react-codemirror";
+import CodeMirror, { EditorView } from "@uiw/react-codemirror";
 import { pythonPrintTransforms } from "./python-print";
+import { CopyIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Events } from "@/utils/events";
+import { toast } from "@/components/ui/use-toast";
+import { useThemeForPlugin } from "@/theme/useTheme";
 
 interface Props {
   dataframeName: string;
@@ -23,16 +28,37 @@ export const CodePanel: React.FC<Props> = ({ transforms, dataframeName }) => {
 };
 
 const PythonCode = (props: { code: string }) => {
+  const { theme } = useThemeForPlugin();
   return (
-    <div className="border-t border-x rounded-t overflow-hidden">
+    <div className="relative min-h-[200px]">
+      <FloatingCopyButton text={props.code} />
       <CodeMirror
         minHeight="200px"
+        className="cm"
+        theme={theme === "dark" ? "dark" : "light"}
         height="100%"
         editable={true}
-        extensions={[python()]}
+        extensions={[python(), EditorView.lineWrapping]}
         value={props.code}
         readOnly={true}
       />
     </div>
+  );
+};
+
+const FloatingCopyButton = (props: { text: string }) => {
+  const copy = Events.stopPropagation(() => {
+    navigator.clipboard.writeText(props.text);
+    toast({ title: "Copied to clipboard" });
+  });
+
+  return (
+    <Button
+      onClick={copy}
+      className="absolute top-0 right-0 m-2 z-10"
+      variant="secondary"
+    >
+      <CopyIcon size={16} strokeWidth={1.5} />
+    </Button>
   );
 };
