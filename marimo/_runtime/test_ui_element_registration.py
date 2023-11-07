fee646875ac09ac8f90e72a5a5568e58684b5b23
Akshay Agrawal <akshay@marimo.io>
2023-11-07 09:03:10 -0800

    lint

--- marimo/_runtime/test_ui_element_registration.py
+++ marimo/_runtime/test_ui_element_registration.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Marimo. All rights reserved.
 from marimo._runtime.conftest import ExecReqProvider
-from marimo._runtime.runtime import Kernel
 from marimo._runtime.context import get_context
+from marimo._runtime.runtime import Kernel
 
 
 # TODO: colocate test with UIElementRegistry file; requires refactoring
