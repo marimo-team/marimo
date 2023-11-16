58d283820c1c0daee2f81ec13ac8215970d343d0
Akshay Agrawal <akshay@marimo.io>
2023-11-15 16:56:49 -0800

    Add a unit test for cell runner's output

---
+++ marimo/_runtime/test_cell_runner.py
@@ -0,0 +1,15 @@
+from marimo._runtime.cell_runner import Runner
+from marimo._runtime.runtime import Kernel
+from marimo._runtime.conftest import ExecReqProvider
+
+
+def test_cell_output(k: Kernel, exec_req: ExecReqProvider) -> None:
+    # run the cell to populate the graph, globals
+    k.run([er := exec_req.get("'hello'; 123")])
+
+    runner = Runner(
+        cell_ids=set(k.graph.cells.keys()), graph=k.graph, glbls=k.globals
+    )
+    run_result = runner.run(er.cell_id)
+    # last expression of cell is output
+    assert run_result.output == 123
