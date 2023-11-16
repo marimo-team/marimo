50905b81af63ae2a85cd6f2fee619f6332c143d2
Myles Scolnick <myles@marimo.io>
2023-11-15 20:28:55 -0500

    fix: more accurate elapsed time calculation (#361)

--- frontend/src/hooks/useElapsedTime.ts
+++ frontend/src/hooks/useElapsedTime.ts
@@ -1,20 +1,23 @@
 /* Copyright 2023 Marimo. All rights reserved. */
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 /**
- * Returns the elapsed time since mount.
+ * Returns the elapsed time since mount, in milliseconds.
  */
 export function useElapsedTime() {
-  const [time, setTime] = useState(0);
+  const startTime = useRef(Date.now());
+  const [endTime, setEndTime] = useState(Date.now());
 
   useEffect(() => {
     const step = 17; // 60 FPS
     const interval = setInterval(() => {
-      setTime((time) => time + step);
+      // Need to use Date.now() here because
+      // setInterval could be paused if the tab is inactive.
+      setEndTime(Date.now());
     }, step);
 
     return () => clearInterval(interval);
   });
 
-  return time;
+  return endTime - startTime.current;
 }
