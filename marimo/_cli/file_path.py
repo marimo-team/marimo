22d997598143edd09feebd570d67e1e8602a0155
Akshay Agrawal <akshay@marimo.io>
2023-11-24 12:06:07 -0800

    Helpful error for marimo edit foo.ipynb

--- marimo/_cli/file_path.py
+++ marimo/_cli/file_path.py
@@ -13,6 +13,7 @@ from typing import Optional
 import click
 
 from marimo._utils.url import is_url
+from marimo._cli.print import green
 
 
 def is_github_src(url: str, ext: str) -> bool:
@@ -59,7 +60,15 @@ def validate_name(
         return _handle_github_issue(name, temp_dir), temp_dir
 
     path = pathlib.Path(name)
-    if path.suffix != ".py":
+    if path.suffix == ".ipynb":
+        prefix = str(path)[:-len(".ipynb")]
+        raise click.UsageError(
+            f"Invalid NAME - {name} is not a Python file.\n\n"
+            f"  {green('Tip:')} Convert {name} to a marimo notebook with\n\n"
+            f"    marimo convert {name} > {prefix}.py\n\n"
+            f"  then open with marimo edit {prefix}.py"
+        )
+    elif path.suffix != ".py":
         raise click.UsageError("Invalid NAME - %s is not a Python file" % name)
 
     if is_github_src(name, ext=".py"):
