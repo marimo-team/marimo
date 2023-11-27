37f981359d5fab071ab4eeb145297eca5497eafa
Myles Scolnick <myles@marimo.io>
2023-11-27 16:50:46 -0500

    feat: Add leafmap formatter

---
+++ marimo/_output/formatters/leafmap_formatters.py
@@ -0,0 +1,34 @@
+# Copyright 2023 Marimo. All rights reserved.
+from __future__ import annotations
+
+import marimo._output.data.data as mo_data
+from marimo._output.formatters.formatter_factory import FormatterFactory
+from marimo._output.utils import flatten_string
+
+
+class LeafmapFormatter(FormatterFactory):
+    @staticmethod
+    def package_name() -> str:
+        return "leafmap"
+
+    def register(self) -> None:
+        import leafmap
+
+        from marimo._output import formatting
+
+        @formatting.formatter(leafmap.Map)
+        def _show_dataframe(map: leafmap.Map) -> tuple[str, str]:
+            # 540px is the pixel height that makes the map fit in the
+            # notebook without scrolling
+            html = mo_data.html(map.to_html(height="540px"))
+            return (
+                "text/html",
+                (
+                    flatten_string(
+                        f"<iframe src='{html.url}'"
+                        "frameborder='0' scrolling='auto'"
+                        "style='width: 100%'"
+                        "onload='__resizeIframe(this)'></iframe>"
+                    )
+                ),
+            )
