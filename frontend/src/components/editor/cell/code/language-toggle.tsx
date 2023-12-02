16b91130c7b5772bcafaea87a250dc61e815ec61
Akshay Agrawal <akshay@marimo.io>
2023-12-02 08:21:56 -1000

    "View as" other language

--- frontend/src/components/editor/cell/code/language-toggle.tsx
+++ frontend/src/components/editor/cell/code/language-toggle.tsx
@@ -32,7 +32,7 @@ export const LanguageToggle: React.FC<Props> = ({
 
   return (
     <div className="absolute top-0 right-4 z-20 hover-action">
-      <Tooltip content={`Switch to ${startCase(otherLanguage)}`}>
+      <Tooltip content={`View as ${startCase(otherLanguage)}`}>
         <Button variant="text" size="xs" className="opacity-80">
           <Icon
             className="cursor-pointer"
