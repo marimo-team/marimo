3f2bd09b04ec980734c25513083c859850b61d5d
Myles Scolnick <myles@marimo.io>
2023-12-03 17:39:45 -0500

    Apply suggestions from code review
    Co-authored-by: Akshay Agrawal <akshay@marimo.io>

--- frontend/src/core/codemirror/language/markdown.ts
+++ frontend/src/core/codemirror/language/markdown.ts
@@ -81,6 +81,15 @@ export class MarkdownLanguageAdapter implements LanguageAdapter {
   }
 
   isSupported(pythonCode: string): boolean {
+    const markdownLines = pythonCode                                             
+        .split("\n")                                                               
+        .map((line) => line.startsWith("mo.md("))                                  
+        .filter(Boolean);                                                          
+    if (markdownLines.length > 1) {                                              
+        // more than line starting with mo.md(; as a heuristic,                    
+        // don't show "view as markdown"                                           
+        return false;                                                              
+    }                 
     return regexes.some(([, regex]) => regex.test(pythonCode));
   }
 
@@ -164,6 +173,6 @@ function upgradePrefixKind(kind: PrefixKind, code: string): PrefixKind {
 function indentOneTab(code: string): string {
   return code
     .split("\n")
-    .map((line) => (line.trim() === "" ? line : `\t${line}`))
+    .map((line) => (line.trim() === "" ? line : `    ${line}`))
     .join("\n");
 }
