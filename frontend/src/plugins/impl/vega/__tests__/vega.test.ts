7d11f807fd4c8eac6fbc212a6aa9797fb4bdb316
Myles Scolnick <myles@marimo.io>
2023-12-08 11:23:57 -0500

    fix: Add support for parsing Date in vega loader

---
+++ frontend/src/plugins/impl/vega/__tests__/vega.test.ts
@@ -0,0 +1,32 @@
+/* Copyright 2023 Marimo. All rights reserved. */
+import { describe, expect, it, vi } from "vitest";
+import { vegaLoadData, vegaLoader } from "../loader";
+
+describe("vega loader", () => {
+  it("should parse csv data", async () => {
+    const csvData = `
+joined,username,id
+2023-08-14T19:28:47Z,akshayka,1994308
+2023-08-14T21:30:17Z,mscolnick,5108954
+`.trim();
+
+    vi.spyOn(vegaLoader, "load").mockReturnValue(Promise.resolve(csvData));
+
+    const data = await vegaLoadData(csvData, { type: "csv", parse: "auto" });
+
+    expect(data).toMatchInlineSnapshot(`
+      [
+        {
+          "id": 1994308,
+          "joined": 2023-08-14T19:28:47.000Z,
+          "username": "akshayka",
+        },
+        {
+          "id": 5108954,
+          "joined": 2023-08-14T21:30:17.000Z,
+          "username": "mscolnick",
+        },
+      ]
+    `);
+  });
+});
--- frontend/src/plugins/impl/vega/loader.ts
+++ frontend/src/plugins/impl/vega/loader.ts
@@ -1,7 +1,10 @@
 /* Copyright 2023 Marimo. All rights reserved. */
 // @ts-expect-error - no types
-import { loader as createLoader, read } from "vega-loader";
+import { loader as createLoader, read, typeParsers } from "vega-loader";
 import { DataFormat } from "./types";
+
+// Augment the typeParsers to support Date
+typeParsers.date = (value: string) => new Date(value);
 
 export const vegaLoader = createLoader();
 
