f2b561141e214c0fdea0764b810fbc960eb59e15
Myles Scolnick <myles@marimo.io>
2023-12-13 09:35:51 -0500

    improve: multi-select filter fn

---
+++ frontend/src/plugins/impl/__tests__/MultiSelectPlugin.test.ts
@@ -0,0 +1,42 @@
+/* Copyright 2023 Marimo. All rights reserved. */
+import { expect, it } from "vitest";
+import { exportedForTesting } from "../MultiselectPlugin";
+
+const filterFn = exportedForTesting.multiselectFilterFn;
+function filterOptions(filter: string, items: string[]) {
+  return items.filter((option) => filterFn(option, filter));
+}
+
+it("can filter to relevant words", () => {
+  const options = ["a", "b", "c", "foo", "bar", "foo bar", "foobar"];
+
+  expect(filterOptions("a", options)).toEqual([
+    "a",
+    "bar",
+    "foo bar",
+    "foobar",
+  ]);
+
+  expect(filterOptions("b", options)).toEqual([
+    "b",
+    "bar",
+    "foo bar",
+    "foobar",
+  ]);
+
+  expect(filterOptions("f", options)).toEqual(["foo", "foo bar", "foobar"]);
+
+  expect(filterOptions("foo", options)).toEqual(["foo", "foo bar", "foobar"]);
+
+  expect(filterOptions("foo ", options)).toEqual(["foo", "foo bar", "foobar"]);
+
+  expect(filterOptions("foo b", options)).toEqual(["foo bar", "foobar"]);
+
+  expect(filterOptions("foo ba", options)).toEqual(["foo bar", "foobar"]);
+
+  expect(filterOptions("foo bar", options)).toEqual(["foo bar", "foobar"]);
+
+  expect(filterOptions("foob", options)).toEqual(["foobar"]);
+
+  expect(filterOptions("foob foo", options)).toEqual(["foobar"]);
+});
