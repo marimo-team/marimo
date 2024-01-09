d6487f19eac6759f6be3126b29a7026b1696c028
Myles Scolnick <myles@marimo.io>
2024-01-09 13:14:55 -0800

    close window on shutdown

--- frontend/src/components/editor/Controls.tsx
+++ frontend/src/components/editor/Controls.tsx
@@ -90,7 +90,12 @@ export const Controls = ({
           {presenting && <LayoutSelect />}
           <NotebookMenuDropdown />
           <AppConfigButton />
-          <ShutdownButton onShutdown={onShutdown} />
+          <ShutdownButton
+            onShutdown={() => {
+              onShutdown();
+              window.close();
+            }}
+          />
         </div>
       )}
 
