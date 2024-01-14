4257062b16a4d661b303a7bd7cea25320640a848
Myles Scolnick <myles@marimo.io>
2024-01-14 12:07:11 -0800

    fix: fix native select background color on Linux, improve other dark more support (#565)
    * fix: fix native select background color on Linux
    
    * use color-scheme
    
    * dark theme on docs too

--- marimo/_smoke_tests/inputs.py
+++ marimo/_smoke_tests/inputs.py
@@ -1,7 +1,6 @@
-# Copyright 2023 Marimo. All rights reserved.
 import marimo
 
-__generated_with = "0.1.8"
+__generated_with = "0.1.76"
 app = marimo.App()
 
 
@@ -23,15 +22,25 @@ def __(disabled, mo):
     mo.vstack(
         [
             mo.ui.text(label="Your name", disabled=disabled.value),
-            mo.ui.text(
-                label="Your tagline", max_length=30, disabled=disabled.value
-            ),
-            mo.ui.text_area(
-                label="Your bio", max_length=180, disabled=disabled.value
-            ),
+            mo.ui.text(label="Your tagline", max_length=30, disabled=disabled.value),
+            mo.ui.text_area(label="Your bio", max_length=180, disabled=disabled.value),
         ]
     )
     return
+
+
+@app.cell
+def __(mo):
+    options = ["red", "green", "blue"]
+
+    mo.vstack(
+        [
+            mo.ui.dropdown(options, label="Dropdown"),
+            mo.ui.multiselect(options, label="Multi-select"),
+        ]
+    )
+    return options,
+
 
 if __name__ == "__main__":
     app.run()
