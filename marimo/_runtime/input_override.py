fe52222e50ebcb65ad68b8191f9052bbb85694e2
Akshay Agrawal <akshay@marimo.io>
2024-01-19 09:28:01 -0800

    typo

--- marimo/_runtime/input_override.py
+++ marimo/_runtime/input_override.py
@@ -5,5 +5,5 @@ import sys
 
 @functools.wraps(input)
 def input_override(prompt: str = "") -> str:
-    # sys.stdin is overriden
+    # sys.stdin is overridden
     return sys.stdin._readline_with_prompt(prompt)  # type: ignore[attr-defined, no-any-return]  # noqa: E501
