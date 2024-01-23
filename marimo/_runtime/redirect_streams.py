a546dd3684f06f4f4ed73932ee44059f1b471ff4
Akshay Agrawal <akshay@marimo.io>
2024-01-23 12:03:28 -0800

    Fix: add encoding, fileno to Stdout/Stderr
    - These are attributes that exist on sys.stdout and sys.stderr
    - This change also refactors stream redirection in order to expose
      the duped fd to the stream objects (and reduce code duplication)

--- marimo/_runtime/redirect_streams.py
+++ marimo/_runtime/redirect_streams.py
@@ -4,11 +4,10 @@ from __future__ import annotations
 import contextlib
 import os
 import sys
-import threading
 from typing import Iterator
 
 from marimo._ast.cell import CellId_t
-from marimo._messaging.streams import Stderr, Stdin, Stdout, Stream
+from marimo._messaging.streams import Stderr, Stdin, Stdout, Stream, redirect
 
 
 def forward_os_stream(stream_object: Stdout | Stderr, fd: int) -> None:
@@ -58,26 +57,6 @@ def redirect_streams(
             stream.cell_id = None
         return
 
-    # Redirect file descriptors for writable streams (stdout, stderr).
-    #
-    # All six of these file descriptors will need to be closed later
-    stdout_duped, stdout_read_fd, stdout_fd = dup2newfd(sys.stdout.fileno())
-    stderr_duped, stderr_read_fd, stderr_fd = dup2newfd(sys.stderr.fileno())
-
-    # redirecting the standard streams in this way appears to have an overhead
-    # of ~1-2ms; the following alternatives had high variance, with up to 30ms
-    # overhead
-    # - reusing the same two threads instead of creating and destroying on
-    #   each call; this requires (slow) synchronization with locks
-    # - using a multiprocessing ThreadPool
-    # - using a concurrent.futures.ThreadPool/ProcessPool
-    stdout_thread = threading.Thread(
-        target=forward_os_stream, args=(stdout, stdout_read_fd)
-    )
-    stderr_thread = threading.Thread(
-        target=forward_os_stream, args=(stderr, stderr_read_fd)
-    )
-
     # NB: Python doesn't allow monkey patching methods builtins, so
     # we replace these streams outright
     py_stdout = sys.stdout
@@ -87,35 +66,11 @@ def redirect_streams(
     sys.stderr = stderr  # type: ignore
     sys.stdin = stdin  # type: ignore
 
-    stdout_thread.start()
-    stderr_thread.start()
-
-    try:
+    with redirect(stdout, py_stdout.fileno()), redirect(
+        stderr, py_stderr.fileno()
+    ):
         yield
-    finally:
-        # Restore the original std file descriptors: point stdout_fd and
-        # stderr_fd to their original locations. Before this call, stdout_fd
-        # and stderr_fd are referring to write ends of pipes. dup2 will
-        # close these fds before reusing them, which ensures that the
-        # forwarding stdout_thread and stderr_threads will terminate.
-        os.dup2(stdout_duped, stdout_fd)
-        os.dup2(stderr_duped, stderr_fd)
-
-        stdout_thread.join()
-        stderr_thread.join()
-
-        # Close these descriptors, since the std file descriptors have been
-        # restored.
-        os.close(stdout_duped)
-        os.close(stderr_duped)
-
-        # Close the read ends of the pipes
-        os.close(stdout_read_fd)
-        os.close(stderr_read_fd)
-
-        # Restore Python stdout/stderr/stdin
         sys.stdout = py_stdout
         sys.stderr = py_stderr
         sys.stdin = py_stdin
-
         stream.cell_id = None
