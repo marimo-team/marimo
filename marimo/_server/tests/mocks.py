2457d217b4b3b13370f854fdfa238cf7ef35881e
Myles Scolnick <myles@marimo.io>
2024-01-23 12:05:25 -0500

    more endpoint tests

--- marimo/_server/tests/mocks.py
+++ marimo/_server/tests/mocks.py
@@ -1,6 +1,9 @@
 # Copyright 2024 Marimo. All rights reserved.
 import functools
 import tempfile
+from typing import Callable
+
+from starlette.testclient import TestClient
 
 from marimo._server.model import SessionMode
 from marimo._server.sessions import SessionManager
@@ -38,3 +41,23 @@ if __name__ == "__main__":
         quiet=False,
         include_code=True,
     )
+
+
+def with_session(
+    session_id: str,
+) -> Callable[[Callable[..., None]], Callable[..., None]]:
+    """Decorator to create a session and close it after the test"""
+
+    def decorator(func: Callable[..., None]):
+        def wrapper(client: TestClient):
+            with client.websocket_connect(
+                f"/ws?session_id={session_id}"
+            ) as websocket:
+                data = websocket.receive_text()
+                assert data
+                func(client)
+                client.post("/api/kernel/shutdown")
+
+        return wrapper
+
+    return decorator
---
+++ marimo/_server/tests/test_editing.py
@@ -0,0 +1,90 @@
+# Copyright 2024 Marimo. All rights reserved.
+
+
+from starlette.testclient import TestClient
+
+from marimo._server.tests.mocks import with_session
+
+SESSION_ID = "session-123"
+HEADERS = {
+    "Marimo-Session-Id": SESSION_ID,
+}
+
+
+@with_session(SESSION_ID)
+def test_code_autocomplete(client: TestClient) -> None:
+    response = client.post(
+        "/api/kernel/code_autocomplete",
+        headers=HEADERS,
+        json={
+            "id": "completion-123",
+            "document": "print('Hello, World!')",
+            "cell_id": "cell-123",
+        },
+    )
+    assert response.status_code == 200, response.text
+    assert response.headers["content-type"] == "application/json"
+    assert "success" in response.json()
+
+
+@with_session(SESSION_ID)
+def test_delete_cell(client: TestClient) -> None:
+    response = client.post(
+        "/api/kernel/delete",
+        headers=HEADERS,
+        json={
+            "cell_id": "cell-123",
+        },
+    )
+    assert response.status_code == 200, response.text
+    assert response.headers["content-type"] == "application/json"
+    assert "success" in response.json()
+
+
+@with_session(SESSION_ID)
+def test_format_cell(client: TestClient) -> None:
+    response = client.post(
+        "/api/kernel/format",
+        headers=HEADERS,
+        json={
+            "codes": {
+                "cell-123": "def foo():\n  return 1",
+            },
+            "line_length": 80,
+        },
+    )
+    assert response.status_code == 200, response.text
+    assert response.headers["content-type"] == "application/json"
+    formatted_codes = response.json().get("codes", {})
+    assert "cell-123" in formatted_codes
+    assert formatted_codes["cell-123"] == "def foo():\n    return 1"
+
+
+@with_session(SESSION_ID)
+def test_set_cell_config(client: TestClient) -> None:
+    response = client.post(
+        "/api/kernel/set_cell_config",
+        headers=HEADERS,
+        json={
+            "configs": {
+                "cell-123": {"runnable": True},
+            },
+        },
+    )
+    assert response.status_code == 200, response.text
+    assert response.headers["content-type"] == "application/json"
+    assert "success" in response.json()
+
+
+@with_session(SESSION_ID)
+def test_stdin(client: TestClient) -> None:
+    response = client.post(
+        "/api/kernel/stdin",
+        headers=HEADERS,
+        json={
+            "text": "user input",
+        },
+    )
+    assert response.status_code == 200, response.text
+    assert response.headers["content-type"] == "application/json"
+    assert "success" in response.json()
