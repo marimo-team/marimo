64c555ebde35e1b9dc223e2bda8feac0322ceae0
Myles Scolnick <myles@marimo.io>
2024-01-23 23:26:25 -0500

    fix

--- frontend/src/components/editor/chrome/panels/documentation-panel.tsx
+++ frontend/src/components/editor/chrome/panels/documentation-panel.tsx
@@ -1,4 +1,4 @@
-/* Copyright 2023 Marimo. All rights reserved. */
+/* Copyright 2024 Marimo. All rights reserved. */
 import { documentationAtom } from "@/core/documentation/state";
 import { renderHTML } from "@/plugins/core/RenderHTML";
 import { useAtomValue } from "jotai";
