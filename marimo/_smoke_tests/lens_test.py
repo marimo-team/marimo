44c8ede788f17ccb9a2f902838cf633e4d2ed722
Akshay Agrawal <akshay@marimo.io>
2024-01-25 15:55:30 -0800

    edit smoke

--- marimo/_smoke_tests/lens_test.py
+++ marimo/_smoke_tests/lens_test.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.1.82"
+__generated_with = "0.1.81"
 app = marimo.App()
 
 
@@ -16,9 +16,11 @@ def __(mo):
 
     data = []
 
+
     def append(v, i):
         del v
         data.append(i)
+
 
     x = mo.ui.dictionary(
         {
@@ -31,7 +33,7 @@ def __(mo):
             for i in range(3)
         }
     )
-    #x
+    # x
     return append, data, partial, x
 
 
@@ -45,6 +47,18 @@ def __(mo, x):
 def __(data, x):
     x.value, data
     return
+
+
+@app.cell
+def __(mo, x):
+    composite = mo.ui.array(
+        [
+            mo.ui.slider(1, 10),
+            mo.ui.array([mo.ui.checkbox(False), mo.ui.slider(10, 20)]),
+            mo.ui.dictionary(x),
+        ]
+    )
+    return composite,
 
 
 if __name__ == "__main__":
