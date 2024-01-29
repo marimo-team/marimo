d8209ecc82815f7d2e7d3031251b1c79e3fb2822
Myles Scolnick <myles@marimo.io>
2024-01-29 11:30:04 -0500

    add add_request

--- marimo/_server/sessions.py
+++ marimo/_server/sessions.py
@@ -262,6 +262,7 @@ class Session:
         self.kernel_manager.interrupt_kernel()
 
     def put_request(self, request: requests.Request) -> None:
+        self.session_view.add_request(request)
         self._queue_manager.control_queue.put(request)
 
     def put_input(self, text: str) -> None:
