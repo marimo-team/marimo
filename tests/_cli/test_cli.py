4bc8f4937e26739bdcd3be0bb8fe497e71612cfe
Akshay <akshay@marimo.io>
2024-02-09 12:13:22 -0800

    try to fix windows shutdown tests, but ok if they flake

--- tests/_cli/test_cli.py
+++ tests/_cli/test_cli.py
@@ -4,18 +4,62 @@ Crude CLI tests
 
 Requires frontend to be built
 """
+import contextlib
 import os
 import signal
 import socket
 import subprocess
+import sys
 import tempfile
 import time
 import urllib.request
-from typing import Optional
+from typing import Any, Callable, Iterator, Optional
+
+import pytest
 
 from marimo import __version__
 from marimo._ast import codegen
 from marimo._ast.cell import CellConfig
+
+
+def _is_win32() -> bool:
+    return sys.platform == "win32"
+
+
+@contextlib.contextmanager
+def _patch_signals_win32() -> Iterator[None]:
+    old_handler: Any = None
+    try:
+        if _is_win32():
+            old_handler = signal.signal(signal.SIGINT, lambda *args: ...)
+        yield
+    finally:
+        if old_handler is not None:
+            signal.signal(signal.SIGINT, old_handler)
+
+def _interrupt(pid: int) -> None:
+    if _is_win32():
+        os.kill(pid, signal.CTRL_C_EVENT)
+    else:
+        os.kill(pid, signal.SIGINT)
+
+def _confirm_shutdown(process) -> None:
+    if _is_win32():
+        process.stdin.write(b"y\r\n")
+    else:
+        process.stdin.write(b"y\n")
+    process.stdin.flush()
+
+def _check_shutdown(process, check_fn: Optional[Callable[[int], bool]] = None) -> None:
+    max_tries = 3
+    tries = 0
+    while process.poll() is None and tries < max_tries:
+        time.sleep(1)
+        tries += 1
+    if check_fn is None:
+        assert process.poll() == 0
+    else:
+        assert check_fn(process.poll())
 
 
 def _try_fetch(port: int, host: str = "localhost") -> Optional[bytes]:
@@ -172,19 +216,21 @@ def test_cli_custom_host() -> None:
     _check_contents(p, b"marimo-mode data-mode='edit'", contents)
 
 
+@pytest.mark.xfail(condition=_is_win32(), reason="flaky on Windows")
 def test_cli_edit_interrupt_twice() -> None:
     # two SIGINTs should kill the CLI
     port = _get_port()
     p = subprocess.Popen(["marimo", "edit", "-p", str(port), "--headless"])
     _check_started(port)
-    os.kill(p.pid, signal.SIGINT)
-    assert p.poll() is None
-    os.kill(p.pid, signal.SIGINT)
-    time.sleep(1)
-    # exit code is system dependent when killed by signal
-    assert p.poll() is not None
+    with _patch_signals_win32():
+        _interrupt(p.pid)
+        assert p.poll() is None
+        _interrupt(p.pid)
+        # exit code is system dependent when killed by signal
+        _check_shutdown(p, check_fn=lambda code: code is not None)
 
 
+@pytest.mark.xfail(condition=_is_win32(), reason="flaky on Windows")
 def test_cli_run_interrupt_twice() -> None:
     # two SIGINTs should kill the CLI
     d = tempfile.TemporaryDirectory()
@@ -194,14 +240,15 @@ def test_cli_run_interrupt_twice() -> None:
         ["marimo", "run", path, "-p", str(port), "--headless"]
     )
     _check_started(port)
-    os.kill(p.pid, signal.SIGINT)
-    assert p.poll() is None
-    os.kill(p.pid, signal.SIGINT)
-    time.sleep(1)
-    # exit code is system dependent when killed by signal
-    assert p.poll() is not None
+    with _patch_signals_win32():
+        _interrupt(p.pid)
+        assert p.poll() is None
+        _interrupt(p.pid)
+        # exit code is system dependent when killed by signal
+        _check_shutdown(p, check_fn=lambda code: code is not None)
 
 
+@pytest.mark.xfail(condition=_is_win32(), reason="flaky on Windows")
 def test_cli_edit_shutdown() -> None:
     port = _get_port()
     p = subprocess.Popen(
@@ -209,15 +256,14 @@ def test_cli_edit_shutdown() -> None:
         stdin=subprocess.PIPE,
     )
     _check_started(port)
-    os.kill(p.pid, signal.SIGINT)
-    assert p.poll() is None
-    assert p.stdin is not None
-    p.stdin.write(b"y\n")
-    p.stdin.flush()
-    time.sleep(1)
-    assert p.poll() == 0
+    with _patch_signals_win32():
+        _interrupt(p.pid)
+        assert p.poll() is None
+        assert p.stdin is not None
+        _confirm_shutdown(p)
+        _check_shutdown(p)
 
-
+@pytest.mark.xfail(condition=_is_win32(), reason="flaky on Windows")
 def test_cli_run_shutdown() -> None:
     d = tempfile.TemporaryDirectory()
     path = _temp_run_file(d)
@@ -227,10 +273,11 @@ def test_cli_run_shutdown() -> None:
         stdin=subprocess.PIPE,
     )
     _check_started(port)
-    os.kill(p.pid, signal.SIGINT)
-    assert p.poll() is None
-    assert p.stdin is not None
-    p.stdin.write(b"y\n")
-    p.stdin.flush()
-    time.sleep(1)
-    assert p.poll() == 0
+    with _patch_signals_win32():
+        _interrupt(p.pid)
+        assert p.poll() is None
+        assert p.stdin is not None
+        p.stdin.write(b"y\n")
+        p.stdin.flush()
+        time.sleep(3)
+        assert p.poll() == 0
