8eadeb4d9a6cf35dbc2e607e6010e38d1e52715e
Myles Scolnick <myles@marimo.io>
2024-02-13 16:30:22 -0500

    lint

--- marimo/_pyodide/pyodide_session.py
+++ marimo/_pyodide/pyodide_session.py
@@ -312,7 +312,6 @@ async def launch_pyodide_kernel(
         stdout=stdout,
         stderr=stderr,
         stdin=stdin,
-        input_override=input_override,
     )
     initialize_context(
         kernel=kernel,
