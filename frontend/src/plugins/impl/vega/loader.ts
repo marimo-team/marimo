7a932dab237f9b85a2b20f6cafe527069147db51
Myles Scolnick <myles@marimo.io>
2024-02-28 13:38:58 -0500

    bigint only on its

--- frontend/src/plugins/impl/vega/loader.ts
+++ frontend/src/plugins/impl/vega/loader.ts
@@ -7,7 +7,6 @@ import { isNumber } from "lodash-es";
 // Augment the typeParsers to support Date
 typeParsers.date = (value: string) => new Date(value).toISOString();
 const previousIntegerParser = typeParsers.integer;
-const previousNumberParser = typeParsers.number;
 // We need to use BigInt for numbers to support large numbers
 const bigIntIntegerParser = (v: string) => {
   if (v === "") {
@@ -15,21 +14,13 @@ const bigIntIntegerParser = (v: string) => {
   }
   return isNumber(Number.parseInt(v)) ? BigInt(v) : "";
 };
-const bigIntNumberParser = (v: string) => {
-  if (v === "") {
-    return "";
-  }
-  return isNumber(Number.parseFloat(v)) ? BigInt(v) : "";
-};
 
 function enableBigInt() {
   typeParsers.integer = bigIntIntegerParser;
-  typeParsers.number = bigIntNumberParser;
 }
 
 function disableBigInt() {
   typeParsers.integer = previousIntegerParser;
-  typeParsers.number = previousNumberParser;
 }
 
 export const vegaLoader = createLoader();
