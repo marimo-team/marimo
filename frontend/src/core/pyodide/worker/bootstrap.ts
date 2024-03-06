d2f912c4991d7155e2fb184c48b5cac9615d08e9
Akshay Agrawal <akshay@marimo.io>
2024-03-05 17:17:09 -0800

    0.2.14

--- frontend/src/core/pyodide/worker/bootstrap.ts
+++ frontend/src/core/pyodide/worker/bootstrap.ts
@@ -31,7 +31,7 @@ export async function bootstrap() {
   const marimoWheel =
     process.env.NODE_ENV === "production"
       ? "marimo >= 0.2.5"
-      : "http://localhost:8000/dist/marimo-0.2.13-py3-none-any.whl";
+      : "http://localhost:8000/dist/marimo-0.2.14-py3-none-any.whl";
   await pyodide.runPythonAsync(`
     import micropip
 
