2b8ee278528c65c76f298d2af1bf86ed48d5144f
Akshay Agrawal <akshay@marimo.io>
2024-03-20 20:12:22 -0700

    dont explicitly exit

--- marimo/_server/uvicorn_utils.py
+++ marimo/_server/uvicorn_utils.py
@@ -12,9 +12,7 @@ LOGGER = _loggers.marimo_logger()
 
 def close_uvicorn(server: uvicorn.Server) -> None:
     LOGGER.debug("Shutting down uvicorn")
-    # 0.29.0 changed how uvicorn handles signals
+    # 0.29.0 changed how uvicorn handles signals, making this unnecessary
     # https://github.com/encode/uvicorn/pull/1600
     if version.parse(uvicorn.__version__) < version.parse("0.29.0"):
         server.handle_exit(signal.SIGINT, None)
-    else:
-        sys.exit(0)
