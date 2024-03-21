41fa5e0e3d28abec54569750ef81a5536f0a5896
Akshay Agrawal <akshay@marimo.io>
2024-03-20 19:59:47 -0700

    fix uvicorn shutdown

--- marimo/_server/uvicorn_utils.py
+++ marimo/_server/uvicorn_utils.py
@@ -1,7 +1,9 @@
 # Copyright 2024 Marimo. All rights reserved.
 import signal
+import sys
 
 import uvicorn
+from packaging import version
 
 from marimo import _loggers
 
@@ -10,5 +12,9 @@ LOGGER = _loggers.marimo_logger()
 
 def close_uvicorn(server: uvicorn.Server) -> None:
     LOGGER.debug("Shutting down uvicorn")
-    server.handle_exit(signal.SIGINT, None)
-    LOGGER.debug("Uvicorn shut down")
+    # 0.29.0 changed how uvicorn handles signals
+    # https://github.com/encode/uvicorn/pull/1600
+    if version.parse(uvicorn.__version__) < version.parse("0.29.0"):
+        server.handle_exit(signal.SIGINT, None)
+    else:
+        sys.exit(0)
