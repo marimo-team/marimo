c2e530aacd443224a13d45595106511e0d79e69b
Akshay Agrawal <akshay@marimo.io>
2024-03-21 16:02:51 -0700

    0.3.4 (#986)

--- frontend/src/core/pyodide/worker/bootstrap.ts
+++ frontend/src/core/pyodide/worker/bootstrap.ts
@@ -139,7 +139,7 @@ function getMarimoWheel(version: string) {
     return "marimo >= 0.3.0";
   }
   if (version === "local") {
-    return "http://localhost:8000/dist/marimo-0.3.3-py3-none-any.whl";
+    return "http://localhost:8000/dist/marimo-0.3.4-py3-none-any.whl";
   }
   return `marimo==${version}`;
 }
