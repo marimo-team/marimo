af206a889a07e3a94efcc37c125d2e559b13b9e9
Myles Scolnick <myles@marimo.io>
2024-04-03 14:27:04 -0600

    feat: editor placeholder for AI prompt

--- frontend/src/core/codemirror/extensions.ts
+++ frontend/src/core/codemirror/extensions.ts
@@ -1,55 +1,11 @@
 /* Copyright 2024 Marimo. All rights reserved. */
-import { EditorView, keymap, placeholder } from "@codemirror/view";
+import { EditorView, keymap } from "@codemirror/view";
 import { CellId } from "../cells/ids";
 import { formatEditorViews, toggleMarkdown } from "./format";
 import { smartScrollIntoView } from "../../utils/scroll";
 import { HOTKEYS } from "@/core/hotkeys/hotkeys";
 import { CellActions } from "../cells/cells";
 import { invariant } from "@/utils/invariant";
-
-function acceptPlaceholder(cm: EditorView, text: string) {
-  // if empty, insert the placeholder
-  if (cm.state.doc.length === 0) {
-    cm.dispatch({
-      changes: {
-        from: 0,
-        to: cm.state.doc.length,
-        insert: text,
-      },
-    });
-    // move cursor to end of placeholder
-    cm.dispatch({
-      selection: {
-        anchor: cm.state.doc.length,
-        head: cm.state.doc.length,
-      },
-    });
-    return true;
-  }
-  return false;
-}
-
-/**
- * A placeholder that will be shown when the editor is empty and support
- * auto-complete on right arrow or Tab.
- */
-export function smartPlaceholderExtension(text: string) {
-  return [
-    placeholder(text),
-    keymap.of([
-      {
-        key: "ArrowRight",
-        preventDefault: true,
-        run: (cm) => acceptPlaceholder(cm, text),
-      },
-      {
-        key: "Tab",
-        preventDefault: true,
-        run: (cm) => acceptPlaceholder(cm, text),
-      },
-    ]),
-  ];
-}
 
 /**
  * Add a keymap to format the code in the editor.
