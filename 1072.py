d86851976767f8987a2753a22480e0e059578c18
Myles Scolnick <myles@marimo.io>
2024-04-08 10:55:42 -0600

    fix: inifinite resize (#1087)

---
+++ 1072.py
@@ -0,0 +1,22 @@
+import marimo
+
+__generated_with = "0.3.9"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def __():
+    import polars as pl
+    from sklearn.datasets import fetch_california_housing
+
+    housing = fetch_california_housing()
+    df = pl.DataFrame(
+        data=housing.data, schema=housing.feature_names
+    ).with_columns(Price=housing.target)
+
+    df.plot.scatter(x="MedInc", y="Price")
+    return df, fetch_california_housing, housing, pl
+
+
+if __name__ == "__main__":
+    app.run()
