34df4e9f401f0e5378f126e331b71cf5085ee299
Akshay Agrawal <akshay@marimo.io>
2024-04-11 21:19:32 -0700

    update intro.py

--- marimo/_tutorials/intro.py
+++ marimo/_tutorials/intro.py
@@ -259,13 +259,9 @@ def __(mo):
         marimo edit
         ```
 
-        in a terminal to create a new marimo notebook, or
+        in a terminal to start the marimo notebook server. From here
+        you can create a new notebook or edit existing ones.
 
-        ```
-        marimo edit notebook.py
-        ```
-
-        to create/edit a notebook called `notebook.py`.
 
         **Running as apps.** Use
 
