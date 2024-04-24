5cf533d708e331ce6a3db34ad919339b36787d26
Akshay Agrawal <akshay@marimo.io>
2024-04-24 12:43:13 -0700

    add missing file

---
+++ marimo/_runtime/context/utils.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Marimo. All rights reserved.
+from marimo._output.rich_help import mddoc
+from marimo._runtime.context import (
+    ContextNotInitializedError,
+    get_context,
+)
+from marimo._runtime.context.kernel_context import KernelRuntimeContext
+
+
+@mddoc
+def running_in_notebook() -> bool:
+    """Returns True if running in a marimo notebook, False otherwise"""
+
+    try:
+        ctx = get_context()
+    except ContextNotInitializedError:
+        return False
+    else:
+        return isinstance(ctx, KernelRuntimeContext)
