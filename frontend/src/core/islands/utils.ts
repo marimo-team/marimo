85f9f14e46635c991d4af4094adbeaf5d3b87ff7
dylan madisetti <dylan@madisetti.me>
2024-04-25 14:06:17 -0400

    remove more vite:islands dependency

--- frontend/src/core/islands/utils.ts
+++ frontend/src/core/islands/utils.ts
@@ -5,9 +5,8 @@ export function isIslands() {
   // which is not the case duyring testing.
   const tag = document.querySelector("marimo-mode");
   const initialMode =
-    tag === null || !(tag instanceof HTMLElement)
-      ? null
-      : tag.getAttribute("mode");
+    tag === null || !(tag instanceof HTMLElement) ? null : tag.dataset.mode;
+  console.log("initialMode", initialMode, tag);
   return (
     import.meta.env.VITE_MARIMO_ISLANDS === true || initialMode === "island"
   );
