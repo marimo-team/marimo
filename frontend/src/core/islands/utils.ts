18eb7ae6e281cca14ecaabfe853adb7310126909
dylan madisetti <dylan@madisetti.me>
2024-04-25 09:18:53 -0400

    fix fe test

--- frontend/src/core/islands/utils.ts
+++ frontend/src/core/islands/utils.ts
@@ -1,7 +1,13 @@
 /* Copyright 2024 Marimo. All rights reserved. */
-import { initialMode } from "@/core/mode";
 
 export function isIslands() {
+  // importing "@/core/mode" has the expectation of a valid mode,
+  // which is not the case duyring testing.
+  const tag = document.querySelector("marimo-mode");
+  const initialMode =
+    tag === null || !(tag instanceof HTMLElement)
+      ? null
+      : tag.getAttribute("mode");
   return (
     import.meta.env.VITE_MARIMO_ISLANDS === true || initialMode === "island"
   );
--- frontend/src/core/mode.ts
+++ frontend/src/core/mode.ts
@@ -11,6 +11,7 @@ import { assertNever } from "@/utils/assertNever";
  * - `edit`: A user is editing the notebook. Can switch to present mode.
  * - `present`: A user is presenting the notebook, it looks like read mode but with some editing features. Cannot switch to present mode.
  * - `home`: A user is in the home page.
+ * - `island`: A user is interacting with marimo from an external application.
  */
 export type AppMode = "read" | "edit" | "present" | "home" | "island";
 
