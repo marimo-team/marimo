b9cf0e17153b125d6caeb4c812a9c6aa50f69aeb
dylan madisetti <dylan@madisetti.me>
2024-04-25 14:32:39 -0400

    Remove log + update snapshot

--- frontend/src/core/islands/utils.ts
+++ frontend/src/core/islands/utils.ts
@@ -2,11 +2,10 @@
 
 export function isIslands() {
   // importing "@/core/mode" has the expectation of a valid mode,
-  // which is not the case duyring testing.
+  // which is not the case during testing.
   const tag = document.querySelector("marimo-mode");
   const initialMode =
     tag === null || !(tag instanceof HTMLElement) ? null : tag.dataset.mode;
-  console.log("initialMode", initialMode, tag);
   return (
     import.meta.env.VITE_MARIMO_ISLANDS === true || initialMode === "island"
   );
