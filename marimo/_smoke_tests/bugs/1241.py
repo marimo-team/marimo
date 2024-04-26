0b23b779ee0a1ae3730479058a2fd8844431c780
Myles Scolnick <myles@marimo.io>
2024-04-26 12:43:27 -0400

    fix: leafmap.folium.Map formatting (#1243)

---
+++ marimo/_smoke_tests/bugs/1241.py
@@ -0,0 +1,29 @@
+import marimo
+
+__generated_with = "0.4.5"
+app = marimo.App()
+
+
+@app.cell
+def __():
+    import marimo as mo
+    import leafmap.foliumap as leafmap
+
+    m = leafmap.Map(center=(40, -100), zoom=4)
+    return leafmap, m, mo
+
+
+@app.cell
+def __(m):
+    m  # Using our custom formatter
+    return
+
+
+@app.cell
+def __(m, mo):
+    mo.Html(m._repr_html_())  # Using the built-in ipython formatter
+    return
+
+
+if __name__ == "__main__":
+    app.run()
