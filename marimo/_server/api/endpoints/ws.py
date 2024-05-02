7c6c7bf9472071ff82c79215da347cea25d82471
Myles Scolnick <myles@marimo.io>
2024-05-02 10:20:57 -0400

    fix: disconnect from consumer if there is a stale connection

--- marimo/_server/api/endpoints/ws.py
+++ marimo/_server/api/endpoints/ws.py
@@ -257,6 +257,8 @@ class WebsocketHandler(SessionConsumer):
             existing_session = mgr.get_session(session_id)
             if existing_session is not None:
                 LOGGER.debug("Reconnecting session %s", session_id)
+                # In case their is a lingering connection, close it
+                existing_session.disconnect_consumer()
                 await self._reconnect_session(existing_session, replay=False)
                 return existing_session
 
