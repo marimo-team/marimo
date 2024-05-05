a5c9008e0d04bbd05f69e73fe4f5ed6785f1c6c2
Myles Scolnick <myles@marimo.io>
2024-05-05 18:55:57 -0500

    fix

--- frontend/src/components/data-table/data-table.tsx
+++ frontend/src/components/data-table/data-table.tsx
@@ -25,10 +25,10 @@ import { DataTablePagination } from "./pagination";
 import { DownloadActionProps, DownloadAs } from "./download-actions";
 import { cn } from "@/utils/cn";
 
-interface DataTableProps<TData, TValue> extends Partial<DownloadActionProps> {
+interface DataTableProps<TData> extends Partial<DownloadActionProps> {
   wrapperClassName?: string;
   className?: string;
-  columns: Array<ColumnDef<TData, TValue>>;
+  columns: Array<ColumnDef<TData>>;
   data: TData[];
   pagination?: boolean;
   pageSize?: number;
@@ -37,101 +37,101 @@ interface DataTableProps<TData, TValue> extends Partial<DownloadActionProps> {
   onRowSelectionChange?: OnChangeFn<RowSelectionState>;
 }
 
-export const DataTable = memo(
-  <TData, TValue>({
-    wrapperClassName,
-    className,
-    columns,
+const DataTableInternal = <TData,>({
+  wrapperClassName,
+  className,
+  columns,
+  data,
+  rowSelection,
+  pageSize = 10,
+  downloadAs,
+  pagination = false,
+  onRowSelectionChange,
+}: DataTableProps<TData>) => {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [paginationState, setPaginationState] = React.useState<PaginationState>(
+    { pageSize: pageSize, pageIndex: 0 },
+  );
+
+  const table = useReactTable({
     data,
-    rowSelection,
-    pageSize = 10,
-    downloadAs,
-    pagination = false,
-    onRowSelectionChange,
-  }: DataTableProps<TData, TValue>) => {
-    const [sorting, setSorting] = React.useState<SortingState>([]);
-    const [paginationState, setPaginationState] =
-      React.useState<PaginationState>({ pageSize: pageSize, pageIndex: 0 });
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    // pagination
+    onPaginationChange: setPaginationState,
+    getPaginationRowModel: pagination ? getPaginationRowModel() : undefined,
+    // sorting
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    // selection
+    onRowSelectionChange: onRowSelectionChange,
+    state: {
+      sorting,
+      pagination: pagination
+        ? { ...paginationState, pageSize: pageSize }
+        : { pageIndex: 0, pageSize: data.length },
+      rowSelection,
+    },
+  });
 
-    const table = useReactTable({
-      data,
-      columns,
-      getCoreRowModel: getCoreRowModel(),
-      // pagination
-      onPaginationChange: setPaginationState,
-      getPaginationRowModel: pagination ? getPaginationRowModel() : undefined,
-      // sorting
-      onSortingChange: setSorting,
-      getSortedRowModel: getSortedRowModel(),
-      // selection
-      onRowSelectionChange: onRowSelectionChange,
-      state: {
-        sorting,
-        pagination: pagination
-          ? { ...paginationState, pageSize: pageSize }
-          : { pageIndex: 0, pageSize: data.length },
-        rowSelection,
-      },
-    });
-
-    return (
-      <div className={cn(wrapperClassName, "flex flex-col space-y-2")}>
-        <div className={cn(className || "rounded-md border")}>
-          <Table>
-            <TableHeader>
-              {table.getHeaderGroups().map((headerGroup) => (
-                <TableRow key={headerGroup.id}>
-                  {headerGroup.headers.map((header) => {
-                    return (
-                      <TableHead key={header.id}>
-                        {header.isPlaceholder
-                          ? null
-                          : flexRender(
-                              header.column.columnDef.header,
-                              header.getContext(),
-                            )}
-                      </TableHead>
-                    );
-                  })}
+  return (
+    <div className={cn(wrapperClassName, "flex flex-col space-y-2")}>
+      <div className={cn(className || "rounded-md border")}>
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
                 </TableRow>
-              ))}
-            </TableHeader>
-            <TableBody>
-              {table.getRowModel().rows?.length ? (
-                table.getRowModel().rows.map((row) => (
-                  <TableRow
-                    key={row.id}
-                    data-state={row.getIsSelected() && "selected"}
-                  >
-                    {row.getVisibleCells().map((cell) => (
-                      <TableCell key={cell.id}>
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext(),
-                        )}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                ))
-              ) : (
-                <TableRow>
-                  <TableCell
-                    colSpan={columns.length}
-                    className="h-24 text-center"
-                  >
-                    No results.
-                  </TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </div>
-        <div className="flex align-items justify-between flex-shrink-0">
-          {pagination ? <DataTablePagination table={table} /> : <div />}
-          {downloadAs && <DownloadAs downloadAs={downloadAs} />}
-        </div>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
       </div>
-    );
-  },
-);
-DataTable.displayName = "DataTable";
+      <div className="flex align-items justify-between flex-shrink-0">
+        {pagination ? <DataTablePagination table={table} /> : <div />}
+        {downloadAs && <DownloadAs downloadAs={downloadAs} />}
+      </div>
+    </div>
+  );
+};
+
+export const DataTable = memo(DataTableInternal) as typeof DataTableInternal;
