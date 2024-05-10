c3954852997901a707516e55ab97106501ee69a6
Myles Scolnick <myles@marimo.io>
2024-05-10 12:52:07 -0400

    fix: use theme in bokeh formatter (#1353)
    * fix: use theme in bokeh formatter
    
    * move comment

---
+++ marimo/_smoke_tests/bugs/1351.py
@@ -0,0 +1,58 @@
+# Copyright 2024 Marimo. All rights reserved.
+
+import marimo
+
+__generated_with = "0.5.1"
+app = marimo.App()
+
+
+@app.cell
+def __(mo):
+    themes = ["dark_minimal", "light_minimal", "contrast", "night_sky", "caliber"]
+    selected_theme = mo.ui.radio(themes, label="Theme", value="dark_minimal")
+    selected_theme
+    return selected_theme, themes
+
+
+@app.cell
+def __(selected_theme):
+    import polars as pl
+    import holoviews as hv
+    import hvplot.polars
+    hv.extension("bokeh")
+    hvplot.extension("bokeh")
+    hv.renderer('bokeh').theme = selected_theme.value
+    return hv, hvplot, pl
+
+
+@app.cell
+def __(pl):
+    df = pl.DataFrame({"a": range(1, 10), "b": range(1, 10)})
+    df.plot.line(x="a", y="b",)
+    return df,
+
+
+@app.cell
+def __(selected_theme):
+    from bokeh.plotting import curdoc, figure
+
+    x = [1, 2, 3, 4, 5]
+    y = [6, 7, 6, 4, 5]
+
+    curdoc().theme = selected_theme.value
+
+    p = figure(width=300, height=300)
+    p.line(x, y)
+
+    p
+    return curdoc, figure, p, x, y
+
+
+@app.cell
+def __():
+    import marimo as mo
+    return mo,
+
+
+if __name__ == "__main__":
+    app.run()
