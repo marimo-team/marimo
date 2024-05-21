289d9c98dc9d78b47ea7d503a8828befb2cabb42
Myles Scolnick <myles@marimo.io>
2024-05-21 16:54:48 -0400

    fix: pin pyodide (#1437)

--- frontend/src/core/pyodide/worker/getPyodideVersion.ts
+++ frontend/src/core/pyodide/worker/getPyodideVersion.ts
@@ -1,11 +1,15 @@
 /* Copyright 2024 Marimo. All rights reserved. */
+const ALLOW_DEV_VERSIONS = false;
+
 export function getPyodideVersion(marimoVersion: string) {
-  return marimoVersion.includes("dev") ? "dev" : "v0.25.0";
+  return marimoVersion.includes("dev") && ALLOW_DEV_VERSIONS
+    ? "dev"
+    : "v0.25.0";
 }
 
 export async function importPyodide(marimoVersion: string) {
   // Vite does not like imports with dynamic urls
-  return marimoVersion.includes("dev")
+  return marimoVersion.includes("dev") && ALLOW_DEV_VERSIONS
     ? // @ts-expect-error typescript does not like
       await import(`https://cdn.jsdelivr.net/pyodide/dev/full/pyodide.js`)
     : // @ts-expect-error typescript does not like
