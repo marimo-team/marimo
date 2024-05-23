74efcfeaabab45a1735e1c87b971547aca805073
Myles Scolnick <myles@marimo.io>
2024-05-23 14:36:26 -0400

    feat: add download as html or png in Run mode

--- frontend/src/components/static-html/static-banner.tsx
+++ frontend/src/components/static-html/static-banner.tsx
@@ -31,7 +31,7 @@ export const StaticBanner: React.FC = () => {
   }
 
   return (
-    <div className="px-4 py-2 bg-[var(--sky-2)] border-b border-[var(--sky-7)] text-md text-[var(--sky-11)] font-semibold flex justify-between items-center gap-4">
+    <div className="px-4 py-2 bg-[var(--sky-2)] border-b border-[var(--sky-7)] text-md text-[var(--sky-11)] font-semibold flex justify-between items-center gap-4 no-print">
       <span>
         This is a static Python notebook built using{" "}
         <a
