95bb4b286d940204f72641929eca5cb36310f2b7
Akshay Agrawal <akshay@marimo.io>
2024-05-23 23:43:59 -0700

    panel footer icons -> sidebar
    This change adds a sidebar with panel icons, moving the panel icons
    from the footer to the sidebar (except for the errors icon) while
    keeping the footer.
    
    This has two effects:
    
    1. It makes the panel icons much more discoverable. We received feedback
       that they were not visible.
    2. It frees up space in the footer for more UI elements -- in the
       future, we can surface options like runtime config in the footer,
       instead of hiding it in the settings menu.

---
+++ frontend/src/components/editor/chrome/wrapper/sidebar.tsx
@@ -0,0 +1,103 @@
+/* Copyright 2024 Marimo. All rights reserved. */
+import React, { PropsWithChildren } from "react";
+import { MessageCircleQuestionIcon } from "lucide-react";
+import { cn } from "@/utils/cn";
+import { useChromeActions, useChromeState } from "../state";
+import { Tooltip } from "@/components/ui/tooltip";
+import { FeedbackButton } from "../components/feedback-button";
+import { PANEL_ICONS, PanelType } from "../types";
+
+export const Sidebar: React.FC = () => {
+  const { selectedPanel } = useChromeState();
+  const { openApplication } = useChromeActions();
+
+  const renderIcon = (type: PanelType, className?: string) => {
+    const Icon = PANEL_ICONS[type];
+    return <Icon className={cn("h-5 w-5", className)} />;
+  };
+
+  return (
+    <div className="h-full py-4 px-1 flex flex-col items-start text-muted-foreground text-md border-t border-border select-none no-print text-sm z-50 absolute">
+      <SidebarItem
+        tooltip="View files"
+        selected={selectedPanel === "files"}
+        onClick={() => openApplication("files")}
+      >
+        {renderIcon("files")}
+      </SidebarItem>
+      <SidebarItem
+        tooltip="Explore variables"
+        selected={selectedPanel === "variables"}
+        onClick={() => openApplication("variables")}
+      >
+        {renderIcon("variables")}
+      </SidebarItem>
+      <SidebarItem
+        tooltip="Explore dependencies"
+        selected={selectedPanel === "dependencies"}
+        onClick={() => openApplication("dependencies")}
+      >
+        {renderIcon("dependencies")}
+      </SidebarItem>
+      <SidebarItem
+        tooltip="View outline"
+        selected={selectedPanel === "outline"}
+        onClick={() => openApplication("outline")}
+      >
+        {renderIcon("outline")}
+      </SidebarItem>
+      <SidebarItem
+        tooltip="View live docs"
+        selected={selectedPanel === "documentation"}
+        onClick={() => openApplication("documentation")}
+      >
+        {renderIcon("documentation")}
+      </SidebarItem>
+      <SidebarItem
+        tooltip="Notebook logs"
+        selected={selectedPanel === "logs"}
+        onClick={() => openApplication("logs")}
+      >
+        {renderIcon("logs")}
+      </SidebarItem>
+      <SidebarItem
+        tooltip="Snippets"
+        selected={selectedPanel === "snippets"}
+        onClick={() => openApplication("snippets")}
+      >
+        {renderIcon("snippets")}
+      </SidebarItem>
+
+      <FeedbackButton>
+        <SidebarItem tooltip="Send feedback!" selected={false}>
+          <MessageCircleQuestionIcon className="h-5 w-5" />
+        </SidebarItem>
+      </FeedbackButton>
+    </div>
+  );
+};
+
+const SidebarItem: React.FC<
+  PropsWithChildren<
+    {
+      selected: boolean;
+      tooltip: React.ReactNode;
+    } & React.HTMLAttributes<HTMLDivElement>
+  >
+> = ({ children, tooltip, selected, className, ...rest }) => {
+  return (
+    <Tooltip content={tooltip} side="top" delayDuration={200}>
+      <div
+        className={cn(
+          "flex items-center p-2 text-sm mx-[1px] shadow-inset font-mono cursor-pointer rounded",
+          !selected && "hover:bg-[var(--sage-3)]",
+          selected && "bg-[var(--sage-4)]",
+          className,
+        )}
+        {...rest}
+      >
+        {children}
+      </div>
+    </Tooltip>
+  );
+};
