6e8ccd936ba5761cf3179bda45d061513e2f1296
gvarnavi <gvarnavi@mit.edu>
2024-05-27 15:05:10 -0700

    svg tag closing typo

--- marimo/_islands/island_generator.py
+++ marimo/_islands/island_generator.py
@@ -395,14 +395,6 @@ class MarimoIslandGenerator:
                   class="size-20 animate-spin text-primary"
                 >
                   <path d="M21 12a9 9 0 1 1-6.219-8.56"></path>
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="1"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="size-20 animate-spin text-primary"
-                >
-                  <path d="M21 12a9 9 0 1 1-6.219-8.56"></path>
                 </svg>
                 <div>Initializing...</div>
               </div>
