a855ae77a277ec1f9a58cf946bd2eb9670b789b8
Myles Scolnick <myles@marimo.io>
2024-05-31 12:46:13 -0500

    improvement: persist sidebar across refreshes (#1527)
    * improvement: persist sidebar across refreshes
    
    * add lg step
    
    * move indicator

--- frontend/src/components/editor/chrome/state.ts
+++ frontend/src/components/editor/chrome/state.ts
@@ -2,12 +2,27 @@
 import { createReducerAndAtoms } from "@/utils/createReducer";
 import { useAtomValue } from "jotai";
 import { PanelType } from "./types";
+import { ZodLocalStorage } from "@/utils/localStorage";
+import { z } from "zod";
 
 export interface ChromeState {
   selectedPanel: PanelType | undefined;
   isOpen: boolean;
   panelLocation: "left" | "bottom";
 }
+
+const storage = new ZodLocalStorage<ChromeState>(
+  "marimo:sidebar",
+  z.object({
+    selectedPanel: z
+      .string()
+      .optional()
+      .transform((v) => v as PanelType),
+    isOpen: z.boolean(),
+    panelLocation: z.union([z.literal("left"), z.literal("bottom")]),
+  }),
+  initialState,
+);
 
 function initialState(): ChromeState {
   return {
@@ -22,23 +37,27 @@ const {
   createActions,
   valueAtom: chromeAtom,
   useActions,
-} = createReducerAndAtoms(initialState, {
-  openApplication: (state, selectedPanel: PanelType) => ({
-    ...state,
-    selectedPanel,
-    // If it was closed, open it
-    // If it was open, keep it open unless it was the same application
-    isOpen: state.isOpen ? state.selectedPanel !== selectedPanel : true,
-  }),
-  openPanel: (state) => ({ ...state, isOpen: true }),
-  closePanel: (state) => ({ ...state, isOpen: false }),
-  togglePanel: (state) => ({ ...state, isOpen: !state.isOpen }),
-  setIsOpen: (state, isOpen: boolean) => ({ ...state, isOpen }),
-  changePanelLocation: (state, panelLocation: "left" | "bottom") => ({
-    ...state,
-    panelLocation,
-  }),
-});
+} = createReducerAndAtoms(
+  () => storage.get(),
+  {
+    openApplication: (state, selectedPanel: PanelType) => ({
+      ...state,
+      selectedPanel,
+      // If it was closed, open it
+      // If it was open, keep it open unless it was the same application
+      isOpen: state.isOpen ? state.selectedPanel !== selectedPanel : true,
+    }),
+    openPanel: (state) => ({ ...state, isOpen: true }),
+    closePanel: (state) => ({ ...state, isOpen: false }),
+    togglePanel: (state) => ({ ...state, isOpen: !state.isOpen }),
+    setIsOpen: (state, isOpen: boolean) => ({ ...state, isOpen }),
+    changePanelLocation: (state, panelLocation: "left" | "bottom") => ({
+      ...state,
+      panelLocation,
+    }),
+  },
+  [(_prevState, newState) => storage.set(newState)],
+);
 
 export const useChromeState = () => {
   const state = useAtomValue(chromeAtom);
