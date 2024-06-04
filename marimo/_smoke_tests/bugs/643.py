56fb1dd392b34b2322f33cfd5949d89bb2822108
Myles Scolnick <myles@marimo.io>
2024-06-04 15:38:34 -0500

    improvement: filter variable by cell name (#1556)

---
+++ marimo/_smoke_tests/bugs/643.py
@@ -0,0 +1,53 @@
+import marimo
+
+__generated_with = "0.6.13"
+app = marimo.App()
+
+
+@app.cell
+def __():
+    import marimo as mo
+    return mo,
+
+
+@app.cell
+def __(mo):
+    get_v, set_v = mo.state(True)
+    get_v2, set_v2 = mo.state(True)
+    return get_v, get_v2, set_v, set_v2
+
+
+@app.cell
+def __(get_v, get_v2):
+    [get_v(), get_v2()]
+    return
+
+
+@app.cell
+def __(get_v, mo, set_v):
+    x = mo.ui.checkbox(get_v(), on_change=set_v)
+    x
+    return x,
+
+
+@app.cell
+def __(get_v, mo, set_v):
+    y = mo.ui.checkbox(get_v(), on_change=set_v)
+    y
+    return y,
+
+
+@app.cell
+def __(get_v2, mo, set_v2):
+    mo.ui.checkbox(get_v2(), on_change=set_v2)
+    return
+
+
+@app.cell
+def __(get_v2, mo, set_v2):
+    mo.ui.checkbox(get_v2(), on_change=set_v2)
+    return
+
+
+if __name__ == "__main__":
+    app.run()
