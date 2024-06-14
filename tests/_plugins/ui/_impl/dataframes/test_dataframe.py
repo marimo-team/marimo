2cb217ba21942292945254787c0d786132b01b5c
Myles Scolnick <myles@marimo.io>
2024-06-13 21:38:24 -0400

    fixes

--- tests/_plugins/ui/_impl/dataframes/test_dataframe.py
+++ tests/_plugins/ui/_impl/dataframes/test_dataframe.py
@@ -24,57 +24,58 @@ if HAS_DEPS:
 
 
 @pytest.mark.skipif(not HAS_DEPS, reason="optional dependencies not installed")
-@pytest.mark.parametrize(
-    "df",
-    [
-        pd.DataFrame({"A": [1, 2, 3], "B": ["a", "a", "a"]}),
-        pl.DataFrame({"A": [1, 2, 3], "B": ["a", "a", "a"]}),
-    ],
-)
-def test_dataframe(
-    df: Any,
-) -> None:
-    subject = ui.dataframe(df)
+class TestDataframes:
+    @staticmethod
+    @pytest.mark.parametrize(
+        "df",
+        [
+            pd.DataFrame({"A": [1, 2, 3], "B": ["a", "a", "a"]}),
+            pl.DataFrame({"A": [1, 2, 3], "B": ["a", "a", "a"]}),
+        ],
+    )
+    def test_dataframe(
+        df: Any,
+    ) -> None:
+        subject = ui.dataframe(df)
 
-    assert subject.value is df
-    assert subject._component_args["columns"] == [
-        ["A", "integer"],
-        ["B", "string"],
-    ]
-    assert subject.get_column_values(
-        GetColumnValuesArgs(column="A")
-    ) == GetColumnValuesResponse(values=[1, 2, 3], too_many_values=False)
-    assert subject.get_column_values(
-        GetColumnValuesArgs(column="B")
-    ) == GetColumnValuesResponse(values=["a"], too_many_values=False)
+        assert subject.value is df
+        assert subject._component_args["columns"] == [
+            ["A", "integer"],
+            ["B", "string"],
+        ]
+        assert subject.get_column_values(
+            GetColumnValuesArgs(column="A")
+        ) == GetColumnValuesResponse(values=[1, 2, 3], too_many_values=False)
+        assert subject.get_column_values(
+            GetColumnValuesArgs(column="B")
+        ) == GetColumnValuesResponse(values=["a"], too_many_values=False)
 
-    with pytest.raises(ColumnNotFound):
-        subject.get_column_values(GetColumnValuesArgs(column="idk"))
+        with pytest.raises(ColumnNotFound):
+            subject.get_column_values(GetColumnValuesArgs(column="idk"))
 
+    @staticmethod
+    @pytest.mark.parametrize(
+        "df",
+        [
+            pd.DataFrame({1: [1, 2, 3], 2: ["a", "a", "a"]}),
+        ],
+    )
+    def test_dataframe_numeric_columns(
+        df: Any,
+    ) -> None:
+        subject = ui.dataframe(df)
 
-@pytest.mark.skipif(not HAS_DEPS, reason="optional dependencies not installed")
-@pytest.mark.parametrize(
-    "df",
-    [
-        pd.DataFrame({1: [1, 2, 3], 2: ["a", "a", "a"]}),
-    ],
-)
-def test_dataframe_numeric_columns(
-    df: Any,
-) -> None:
-    subject = ui.dataframe(df)
+        assert subject.value is df
+        assert subject._component_args["columns"] == [
+            [1, "integer"],
+            [2, "string"],
+        ]
 
-    assert subject.value is df
-    assert subject._component_args["columns"] == [
-        [1, "integer"],
-        [2, "string"],
-    ]
+        assert subject.get_column_values(
+            GetColumnValuesArgs(column=1)
+        ) == GetColumnValuesResponse(values=[1, 2, 3], too_many_values=False)
 
-    assert subject.get_column_values(
-        GetColumnValuesArgs(column=1)
-    ) == GetColumnValuesResponse(values=[1, 2, 3], too_many_values=False)
-
-    with pytest.raises(ColumnNotFound):
-        subject.get_column_values(GetColumnValuesArgs(column="idk"))
-    with pytest.raises(ColumnNotFound):
-        subject.get_column_values(GetColumnValuesArgs(column="1"))
+        with pytest.raises(ColumnNotFound):
+            subject.get_column_values(GetColumnValuesArgs(column="idk"))
+        with pytest.raises(ColumnNotFound):
+            subject.get_column_values(GetColumnValuesArgs(column="1"))
