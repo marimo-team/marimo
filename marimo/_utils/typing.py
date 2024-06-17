cde4abd0ce51f6028a09e5094b51b1ae0f633baf
Myles Scolnick <myles@marimo.io>
2024-06-17 09:56:22 -0400

    fix not required

--- marimo/_utils/typing.py
+++ marimo/_utils/typing.py
@@ -6,13 +6,16 @@ import sys
 if sys.version_info < (3, 10):
     from typing_extensions import (
         Annotated as _Annotated,
-        NotRequired as _NotRequired,
     )
 else:
     from typing import (
         Annotated as _Annotated,
-        NotRequired as _NotRequired,
     )
+
+if sys.version_info < (3, 11):
+    from typing_extensions import NotRequired as _NotRequired
+else:
+    from typing import NotRequired as _NotRequired
 
 Annotated = _Annotated
 NotRequired = _NotRequired
