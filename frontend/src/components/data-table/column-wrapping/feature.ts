f0e4fa0dd8f7646f754b7dd778857584f1e1ac9a
Myles Scolnick <myles@marimo.io>
2024-06-30 20:29:17 -0700

    feat: add column wrapping (#1698)

---
+++ frontend/src/components/data-table/column-wrapping/feature.ts
@@ -0,0 +1,62 @@
+/* Copyright 2024 Marimo. All rights reserved. */
+import {
+  TableFeature,
+  RowData,
+  makeStateUpdater,
+  Table,
+  Column,
+  Updater,
+} from "@tanstack/react-table";
+import {
+  ColumnWrappingTableState,
+  ColumnWrappingOptions,
+  ColumnWrappingState,
+} from "./types";
+
+export const ColumnWrappingFeature: TableFeature = {
+  getInitialState: (state): ColumnWrappingTableState => {
+    return {
+      columnWrapping: {},
+      ...state,
+    };
+  },
+
+  getDefaultOptions: <TData extends RowData>(
+    table: Table<TData>,
+  ): ColumnWrappingOptions => {
+    return {
+      enableColumnWrapping: true,
+      onColumnWrappingChange: makeStateUpdater("columnWrapping", table),
+    } as ColumnWrappingOptions;
+  },
+
+  createColumn: <TData extends RowData>(
+    column: Column<TData>,
+    table: Table<TData>,
+  ) => {
+    column.getColumnWrapping = () => {
+      return table.getState().columnWrapping[column.id] || "nowrap";
+    };
+
+    column.getCanWrap = () => {
+      return table.options.enableColumnWrapping ?? false;
+    };
+
+    column.toggleColumnWrapping = (value?: "nowrap" | "wrap") => {
+      const safeUpdater: Updater<ColumnWrappingState> = (old) => {
+        const prevValue = old[column.id] || "nowrap";
+        if (value) {
+          return {
+            ...old,
+            [column.id]: value,
+          };
+        }
+        return {
+          ...old,
+          [column.id]: prevValue === "nowrap" ? "wrap" : "nowrap",
+        };
+      };
+      table.options.onColumnWrappingChange?.(safeUpdater);
+    };
+  },
+};
