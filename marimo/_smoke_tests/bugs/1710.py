6d7223b03c1698eb8b685a9adc3b34b931d8c8c9
Myles Scolnick <myles@marimo.io>
2024-07-02 08:58:32 -0700

    fix: don't error on null html

---
+++ marimo/_smoke_tests/bugs/1710.py
@@ -0,0 +1,21 @@
+import marimo
+
+__generated_with = "0.6.25"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def __():
+    import marimo as mo
+    import os
+    import keras
+    model = keras.models.Sequential()
+    model.add(keras.layers.Input(shape=(1,)))
+    model.add(keras.layers.Dense(2, activation='relu'))
+    model.add(keras.layers.Dense(1, activation='sigmoid'))
+    keras.utils.plot_model(model)
+    return keras, mo, model, os
+
+
+if __name__ == "__main__":
+    app.run()
