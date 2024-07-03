d533745af10c8793a10a6716269eb17ce7ba8663
Akshay Agrawal <akshay@marimo.io>
2024-07-03 11:08:53 -0700

    revert a change ...

--- frontend/src/components/app-config/user-config-form.tsx
+++ frontend/src/components/app-config/user-config-form.tsx
@@ -56,7 +56,7 @@ export const UserConfigForm: React.FC = () => {
 
   const renderCopilotProvider = () => {
     const copilot = form.getValues("completion.copilot");
-    if (!copilot) {
+    if (copilot === false) {
       return null;
     }
     if (copilot === "codeium") {
@@ -590,11 +590,11 @@ export const UserConfigForm: React.FC = () => {
                         }
                       }}
                       value={
-                        field.value
+                        field.value === true
                           ? "github"
-                          : field.value
-                            ? field.value
-                            : "none"
+                          : field.value === false
+                            ? "none"
+                            : field.value
                       }
                       disabled={field.disabled}
                       className="inline-flex mr-2"
