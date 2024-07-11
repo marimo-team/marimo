139b0a0181758fcd1d1a4540ff6b52e8afc00991
Akshay Agrawal <akshay@marimo.io>
2024-07-11 11:26:19 -0700

    fix: remove side-effectful isinstance check (#1779)
    this was breaking import of xdsl

--- marimo/_plugins/ui/_impl/tables/types.py
+++ marimo/_plugins/ui/_impl/tables/types.py
@@ -1,12 +1,11 @@
 # Copyright 2024 Marimo. All rights reserved.
 from __future__ import annotations
 
-from typing import Protocol, runtime_checkable
+from typing import Protocol
 
 from marimo._plugins.ui._impl.tables.dataframe_protocol import DataFrame
 
 
-@runtime_checkable
 class DataFrameLike(Protocol):
     def __dataframe__(
         self, nan_as_null: bool = False, allow_copy: bool = True
@@ -15,8 +14,7 @@ class DataFrameLike(Protocol):
 
 def is_dataframe_like(value: object) -> bool:
     return (
-        isinstance(value, DataFrameLike)
-        and hasattr(value, "__dataframe__")
+        hasattr(value, "__dataframe__")
         and callable(value.__dataframe__)
         # By checking these are equal, we likely guarding against
         # __getattr__ implementations that may return different callables
