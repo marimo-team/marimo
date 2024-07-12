b18bbf932fe76446e24bb90a1f8cf309858b447a
Akshay Agrawal <akshay@marimo.io>
2024-07-12 13:34:37 -0700

    py3.8 compat

--- marimo/_runtime/app/common.py
+++ marimo/_runtime/app/common.py
@@ -1,11 +1,11 @@
 # Copyright 2024 Marimo. All rights reserved.
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, Dict, Tuple
 
 from marimo._ast.cell import CellId_t
 
-OutputsType = dict[CellId_t, Any]
-DefsType = dict[str, Any]
+OutputsType = Dict[CellId_t, Any]
+DefsType = Dict[str, Any]
 
-RunOutput = tuple[OutputsType, DefsType]
+RunOutput = Tuple[OutputsType, DefsType]
