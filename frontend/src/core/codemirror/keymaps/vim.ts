0e737a8579cd75c7162816b3123685eec9d7ccf5
Myles Scolnick <myles@marimo.io>
2024-07-16 13:00:13 -0400

    improvement: sync vim mode (#1807)
    * vim sync mode
    
    * fixes
    
    * fixes
    
    * fixes

--- frontend/src/core/codemirror/keymaps/vim.ts
+++ frontend/src/core/codemirror/keymaps/vim.ts
@@ -1,16 +1,20 @@
 /* Copyright 2024 Marimo. All rights reserved. */
 
-import { keymap } from "@codemirror/view";
+import { getCM, Vim } from "@replit/codemirror-vim";
+import { type EditorView, keymap, ViewPlugin } from "@codemirror/view";
 import {
   isAtEndOfEditor,
   isAtStartOfEditor,
   isInVimNormalMode,
 } from "../utils";
+import { invariant } from "@/utils/invariant";
+import type { Extension } from "@codemirror/state";
+import { Logger } from "@/utils/Logger";
 
 export function vimKeymapExtension(callbacks: {
   focusUp: () => void;
   focusDown: () => void;
-}) {
+}): Extension[] {
   return [
     keymap.of([
       {
@@ -36,5 +40,115 @@ export function vimKeymapExtension(callbacks: {
         },
       },
     ]),
+    ViewPlugin.define((view) => {
+      CodeMirrorVimSync.INSTANCES.addInstance(view);
+      return {
+        destroy() {
+          CodeMirrorVimSync.INSTANCES.removeInstance(view);
+        },
+      };
+    }),
   ];
+}
+
+class CodeMirrorVimSync {
+  private instances = new Set<EditorView>();
+  private isBroadcasting = false;
+
+  public static INSTANCES: CodeMirrorVimSync = new CodeMirrorVimSync();
+
+  private constructor() {
+    // noop
+  }
+
+  addInstance(instance: EditorView) {
+    this.instances.add(instance);
+
+    const cm = getCM(instance);
+    if (!cm) {
+      Logger.warn(
+        "Expected CodeMirror instance to have CodeMirror instance state",
+      );
+      return;
+    }
+
+    // Create an event listener for Vim mode changes
+    // When it changes, we broadcast it to all other CodeMirror instances
+    cm.on("vim-mode-change", (e: { mode: string }) => {
+      if (this.isBroadcasting) {
+        return;
+      }
+      invariant("mode" in e, 'Expected event to have a "mode" property');
+      const mode = e.mode;
+      this.isBroadcasting = true;
+      this.broadcastModeChange(instance, mode);
+      this.isBroadcasting = false;
+    });
+  }
+
+  removeInstance(instance: EditorView) {
+    this.instances.delete(instance);
+  }
+
+  broadcastModeChange(originInstance: EditorView, mode: string) {
+    invariant(
+      "exitInsertMode" in Vim,
+      "Vim does not have an exitInsertMode method",
+    );
+    invariant(
+      "exitVisualMode" in Vim,
+      "Vim does not have an exitVisualMode method",
+    );
+
+    for (const instance of this.instances) {
+      if (instance !== originInstance) {
+        const cm = getCM(instance);
+        if (!cm) {
+          Logger.warn(
+            "Expected CodeMirror instance to have CodeMirror instance state",
+          );
+          continue;
+        }
+
+        // HACK: setSelections will steal focus from the current editor
+        // so we remove it and set it back afterwards
+        const prevSetSelections = cm.setSelections.bind(cm);
+        cm.setSelections = () => {
+          // noop
+          return [];
+        };
+
+        const vim = cm.state.vim;
+        if (!vim) {
+          Logger.warn("Expected CodeMirror instance to have Vim state");
+          continue;
+        }
+
+        switch (mode) {
+          case "normal":
+            // Only exit insert mode if we're in it
+            if (vim.insertMode) {
+              Vim.exitInsertMode(cm, true);
+            }
+            // Only exit visual mode if we're in it
+            if (vim.visualMode) {
+              Vim.exitVisualMode(cm, true);
+            }
+            break;
+          case "insert":
+            // Only enter insert mode if we're not already in it
+            if (!vim.insertMode) {
+              Vim.handleKey(cm, "i", "");
+            }
+            break;
+          case "visual":
+            // We don't switch to visual mode across instances
+            break;
+        }
+
+        // HACK: restore selection
+        cm.setSelections = prevSetSelections;
+      }
+    }
+  }
 }
