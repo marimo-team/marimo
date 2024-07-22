876a4e75e76bab7df727ff07436418e69fd7525c
ontowhee <82607723+ontowhee@users.noreply.github.com>
2024-07-21 17:21:31 -0700

    add test for script with output

--- tests/_runtime/test_script.py
+++ tests/_runtime/test_script.py
@@ -14,6 +14,26 @@ def test_script():
     assert "y = y / x" in result
 
 
+def test_script_with_output():
+    p = subprocess.run(
+        [
+            "python",
+            "tests/_runtime/script_data/script_exception_with_output.py",
+        ],
+        capture_output=True,
+    )
+    assert p.returncode == 1
+
+    result = p.stderr.decode()
+    assert "NameError: name 'y' is not defined" in result
+    assert (
+        'tests/_runtime/script_data/script_exception_with_output.py"'
+        + ", line 10"
+        in result
+    )
+    assert "y / x" in result
+
+
 def test_script_with_imported_file():
     p = subprocess.run(
         [
