1318a00328e039fdd37b858013246f82c7b8d325
Akshay Agrawal <akshay@marimo.io>
2024-07-26 15:07:40 -0700

    try fix mock

--- tests/_output/formatters/test_formatters.py
+++ tests/_output/formatters/test_formatters.py
@@ -133,7 +133,7 @@ def test_broken_formatter():
     "marimo._output.formatters.formatters.THIRD_PARTY_FACTORIES",
     new_callable=dict,
 )
-@patch("marimo._output.formatters.formatters.sys.modules", new_callable=dict)
+@patch("sys.modules", new_callable=dict)
 def test_pre_imported_formatter(mock_sys_modules, mock_third_party_factories):
     mock_sys_modules["fake_module"] = Mock()
 
