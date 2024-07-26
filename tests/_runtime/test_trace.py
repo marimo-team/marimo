4244b73f75589032c7ef3ea362e8195cc8253db5
dylan madisetti <dylan@madisetti.me>
2024-07-26 07:23:39 -0400

    Add tests

---
+++ tests/_runtime/test_trace.py
@@ -0,0 +1,212 @@
+import re
+import subprocess
+import textwrap
+
+from marimo._runtime.requests import (
+    ExecutionRequest,
+)
+from marimo._runtime.runtime import Kernel
+
+
+class TestScriptTrace:
+    @staticmethod
+    def test_script_trace() -> None:
+        p = subprocess.run(
+            ["python", "tests/_runtime/script_data/script_exception.py"],
+            capture_output=True,
+        )
+        assert p.returncode == 1
+
+        result = p.stderr.decode()
+        assert "NameError: name 'y' is not defined" in result
+        assert (
+            'tests/_runtime/script_data/script_exception.py", line 10'
+            in result
+        )
+        assert "y = y / x" in result
+        # Test col_offset
+        # Expected output:
+        #    y = y / x
+        #        ^
+        assert result.split("y / x")[1].split("\n")[1].startswith("        ^")
+
+    @staticmethod
+    def test_script_trace_with_output() -> None:
+        p = subprocess.run(
+            [
+                "python",
+                "tests/_runtime/script_data/script_exception_with_output.py",
+            ],
+            capture_output=True,
+        )
+        assert p.returncode == 1
+
+        result = p.stderr.decode()
+        assert "NameError: name 'y' is not defined" in result
+        assert (
+            'tests/_runtime/script_data/script_exception_with_output.py"'
+            + ", line 10"
+            in result
+        )
+        assert "y / x" in result
+
+    @staticmethod
+    def test_script_trace_with_imported_file() -> None:
+        p = subprocess.run(
+            [
+                "python",
+                "tests/_runtime/script_data/script_exception_with_imported_function.py",
+            ],
+            capture_output=True,
+        )
+        assert p.returncode == 1
+
+        result = p.stderr.decode()
+        assert "ZeroDivisionError: division by zero" in result
+        assert 'tests/_runtime/script_data/func.py", line 3' in result
+        assert (
+            'tests/_runtime/script_data/script_exception_with_imported_function.py"'
+            + ", line 11"
+            in result
+        )
+        assert "y = y / x" in result
+
+
+class TestAppTrace:
+    @staticmethod
+    async def test_app_trace_body_line_number(
+        execution_kernel: Kernel,
+    ) -> None:
+        k = execution_kernel
+        await k.run(
+            [
+                ExecutionRequest(
+                    cell_id="0",
+                    code=textwrap.dedent(
+                        """
+                        x = 0 #L2
+                        y = 0 #L3
+                        y = y / x #L4
+                        # filler line L5
+                        # filler line L6
+                        None #L7 for output expression
+                    """
+                    ),
+                )
+            ]
+        )
+
+        # Naively strip tags to cehck trace
+        tag_re = re.compile(r"(<!--.*?-->|<[^>]*>)")
+        result = k.stderr.messages[-1]
+        result = tag_re.sub("", result)
+
+        assert "ZeroDivisionError: division by zero" in result
+        assert "y / x" in result
+        assert "__marimo__cell_0_.py" in result
+        # Test col_offset
+        # Expected output:
+        # File &quot;/tmp/marimo_0000000/__marimo__cell_0_.py&quot;, line 4 ...
+        #   y = y / x #L4
+        #       ~~^~~
+        # ZeroDivisionError: division by zero
+        post_file = result.split("marimo__cell")[1].split("\n")
+        assert "line 4" in post_file[0]
+        assert post_file[1].startswith("    y = y / x #L4")
+        assert post_file[2].startswith("        ~~^~~")
+
+    @staticmethod
+    async def test_app_trace_output_line_number(
+        execution_kernel: Kernel,
+    ) -> None:
+        k = execution_kernel
+        await k.run(
+            [
+                ExecutionRequest(
+                    cell_id="0",
+                    code=textwrap.dedent(
+                        """
+                        def foo(): #L2
+                            x = 0 #L3
+                            y = 0 #L4
+                            y = y / x #L5
+                            # filler line L5
+                            # filler line L6
+                            None #L7 for output expression
+                    """
+                    ),
+                ),
+                ExecutionRequest(
+                    cell_id="1",
+                    code="foo()",
+                ),
+            ]
+        )
+
+        # Naively strip tags to cehck trace
+        tag_re = re.compile(r"(<!--.*?-->|<[^>]*>)")
+        result = k.stderr.messages[-1]
+        result = tag_re.sub("", result)
+
+        # Test col_offset
+        # Expected output:
+        #  File .../__marimo__cell_1_.py&quot;, line 1, in ...
+        #    foo()
+        #  File .../__marimo__cell_0_.py&quot;, line 4, in foo
+        #    y = y / x #L5
+        #        ~~^~~
+        # ZeroDivisionError: division by zero
+        post_file_call = result.split("marimo__cell_1")[1].split("\n")
+        assert "line 1" in post_file_call[0]
+        assert "foo()" in post_file_call[1]
+        post_file_error = result.split("marimo__cell_0")[1].split("\n")
+        assert post_file_error[1].startswith("    y = y / x #L5")
+        assert post_file_error[2].startswith("        ~~^~~")
+
+    @staticmethod
+    async def test_app_trace_name_error_reference_caught(
+        execution_kernel: Kernel,
+    ) -> None:
+        k = execution_kernel
+        await k.run(
+            [
+                ExecutionRequest(
+                    cell_id="0",
+                    code=textwrap.dedent(
+                        """
+                    try:
+                        R = R # Causes error since no def
+                        C = 0 # Unaccessible
+                    except:
+                        pass
+                    """
+                    ),
+                ),
+                ExecutionRequest(
+                    cell_id="1",
+                    code=textwrap.dedent(
+                        """
+                    C
+                    """
+                    ),
+                ),
+            ]
+        )
+        # Runtime error expected- since not a kernel error check stderr
+        assert "C" not in k.globals
+        if k.execution_type == "strict":
+            assert (
+                "name `R` is referenced before definition."
+                in k.stream.messages[-4][1]["output"]["data"][0]["msg"]
+            )
+            assert (
+                "This cell wasn't run"
+                in k.stream.messages[-1][1]["output"]["data"][0]["msg"]
+            )
+        else:
+            assert (
+                "marimo came across the undefined variable `C` during runtime."
+                in k.stream.messages[-2][1]["output"]["data"][0]["msg"]
+            )
+            assert "NameError" in k.stderr.messages[0]
+            assert "NameError" in k.stderr.messages[-1]
