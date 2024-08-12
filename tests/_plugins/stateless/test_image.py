c4822071919cf7f5c4766504fa8c90f1696d7ab3
Myles Scolnick <myles@marimo.io>
2024-08-12 15:41:10 -0400

    fix

--- tests/_plugins/stateless/test_image.py
+++ tests/_plugins/stateless/test_image.py
@@ -11,7 +11,7 @@ from marimo._runtime.context import get_context
 from marimo._runtime.runtime import Kernel
 from tests.conftest import ExecReqProvider
 
-HAS_DEPS = DependencyManager.numpy() and DependencyManager.pillow.has()
+HAS_DEPS = DependencyManager.numpy.has() and DependencyManager.pillow.has()
 
 
 async def test_image() -> None:
