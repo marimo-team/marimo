366a7351da7729a55292782d808baaf40db2b7e8
Akshay Agrawal <akshay@marimo.io>
2024-08-20 13:17:13 -0400

    linear scale for numbers too

--- frontend/src/components/data-table/chart-spec-model.tsx
+++ frontend/src/components/data-table/chart-spec-model.tsx
@@ -16,7 +16,7 @@ export class ColumnChartSpecModel<T> {
     readonly summaries: ColumnHeaderSummary[],
     private readonly opts: {
       includeCharts: boolean;
-    },
+    }
   ) {
     this.columnSummaries = new Map(summaries.map((s) => [s.column, s]));
   }
@@ -95,7 +95,7 @@ export class ColumnChartSpecModel<T> {
               aggregate: "count",
               type: "quantitative",
               axis: null,
-              scale: { type: "symlog" },
+              scale: { type: "linear" },
             },
             tooltip: [
               {
