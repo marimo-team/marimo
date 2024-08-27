b09f13c23494811ed1c7e93184c13bf16a997785
Myles Scolnick <myles@marimo.io>
2024-08-27 23:36:13 +0200

    feat: marimo edit --sandbox nb.py (#2134)
    * feat: marimo edit --sandbox nb.py
    
    * add tests
    
    * fix
    
    * fix again
    
    * fix
    
    * Update marimo/_cli/cli.py
    
    * Update marimo/_cli/cli.py
    
    * fix
    
    ---------
    
    Co-authored-by: Akshay Agrawal <akshay@marimo.io>

---
+++ tests/_cli/test_sandbox.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from marimo._cli.sandbox import _get_dependencies
+
+
+def test_get_dependencies():
+    SCRIPT = """
+# Copyright 2024 Marimo. All rights reserved.
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "polars",
+#     "marimo>=0.8.0",
+#     "quak",
+#     "vega-datasets",
+# ]
+# ///
+
+import marimo
+
+__generated_with = "0.8.2"
+app = marimo.App(width="medium")
+"""
+    assert _get_dependencies(SCRIPT) == [
+        "polars",
+        "marimo>=0.8.0",
+        "quak",
+        "vega-datasets",
+    ]
+
+
+def test_no_dependencies():
+    SCRIPT = """
+import marimo
+
+__generated_with = "0.8.2"
+app = marimo.App(width="medium")
+"""
+    assert _get_dependencies(SCRIPT) == []
