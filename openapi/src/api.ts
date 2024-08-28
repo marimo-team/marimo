7fef02386255554e581a74e4c126b9980f527c8d
metaboulie <eugeneheiner14@gmail.com>
2024-08-28 12:04:07 +0800

    revert fe changes

--- openapi/src/api.ts
+++ openapi/src/api.ts
@@ -2099,7 +2099,7 @@ export interface components {
         /** @enum {string} */
         default_width: "normal" | "compact" | "medium" | "full";
         /** @enum {string} */
-        theme: "light" | "dark";
+        theme: "light" | "dark" | "system";
       };
       experimental: {
         [key: string]: unknown;
