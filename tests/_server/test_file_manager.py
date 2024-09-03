246bb3da06c4c3bce360bab2d2b5b2676fc65325
Akshay Agrawal <akshay@marimo.io>
2024-09-03 15:18:01 -0700

    maybe fix windows

--- tests/_server/test_file_manager.py
+++ tests/_server/test_file_manager.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import os
-import pathlib
 import tempfile
 from typing import Generator
 
@@ -155,9 +154,7 @@ def test_save_app_config_valid(app_file_manager: AppFileManager) -> None:
 
 
 def test_save_app_config_exception(app_file_manager: AppFileManager) -> None:
-    app_file_manager.filename = str(
-        pathlib.Path("/invalid/path/app_config.py")
-    )
+    app_file_manager.filename = "/invalid/path/app_config.py"
     with pytest.raises(HTTPException) as e:  # noqa: PT012
         app_file_manager.save_app_config({})
     assert e.value.status_code == HTTPStatus.SERVER_ERROR
@@ -196,7 +193,7 @@ def test_save_successful(app_file_manager: AppFileManager) -> None:
 
 
 def test_save_cannot_rename(app_file_manager: AppFileManager) -> None:
-    save_request.filename = "/invalid/path/save_exception.py"
+    save_request.filename = "ABCD://invalid/path/save_exception.py"
     with pytest.raises(HTTPException) as e:
         app_file_manager.save(save_request)
     assert e.value.status_code == HTTPStatus.BAD_REQUEST
