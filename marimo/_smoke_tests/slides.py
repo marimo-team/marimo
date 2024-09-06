837a4a7043574b823f0988d75fd834621e31260b
Myles Scolnick <myles@marimo.io>
2024-09-06 10:09:27 -0400

    improvement: add typoegraphy setting for slides

--- marimo/_smoke_tests/slides.py
+++ marimo/_smoke_tests/slides.py
@@ -1,20 +1,28 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "altair",
+#     "pandas",
+#     "marimo",
+# ]
+# ///
 # Copyright 2024 Marimo. All rights reserved.
 
 import marimo
 
-__generated_with = "0.7.1"
+__generated_with = "0.8.11"
 app = marimo.App(layout_file="layouts/slides.slides.json")
 
 
 @app.cell
 def __(mo):
-    mo.md("# A Presentation on Iris Data")
+    mo.md("""# A Presentation on `Iris` Data""")
     return
 
 
 @app.cell
-def __():
-    "By the marimo team"
+def __(mo):
+    mo.md("""## By the marimo team (`@marimo_io`)""")
     return
 
 
@@ -55,7 +63,83 @@ def __(alt, df, mo):
 
 @app.cell
 def __(mo):
-    mo.md("# Thank you!")
+    mo.md("""# Thank you!""")
+    return
+
+
+@app.cell
+def __():
+    # Some markdown testing
+    return
+
+
+@app.cell
+def __(mo):
+    mo.md(
+        r"""
+        # H1 (`H1`)
+        ## H2 (`H2`)
+        ### H3 (`H3`)
+        #### H4 (`H4`)
+        ##### H5 (`H5`)
+        ###### H6 (`H6`)
+        """
+    )
+    return
+
+
+@app.cell
+def __(mo):
+    mo.md(
+        r"""
+        - Item 1
+        - `Item 2`
+        - **Item 3**
+        - _Item 4_
+        """
+    )
+    return
+
+
+@app.cell
+def __(mo):
+    mo.md(
+        r"""
+        !!! note "Callouts"
+            This is a callout
+        """
+    )
+    return
+
+
+@app.cell
+def __(mo):
+    mo.callout("""
+    This is another callout
+    """)
+    return
+
+
+@app.cell
+def __():
+    return
+
+
+@app.cell
+def __(mo):
+    mo.md(r"""## Items that don't quite work in slides""")
+    return
+
+
+@app.cell
+def __(mo):
+    mo.accordion({"Accodrions too small": mo.md("Content")})
+    return
+
+
+@app.cell
+def __(mo):
+    mo.ui.tabs({"Tabs to small": mo.md("Content")})
     return
 
 
