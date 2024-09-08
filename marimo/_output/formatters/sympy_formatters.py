1ad4cadfffba831a279c93f779dbe52ccea72434
Francisco Agosto <fco@agostof.com>
2024-09-07 20:43:06 -0400

    cleanup sympy formatter

--- marimo/_output/formatters/sympy_formatters.py
+++ marimo/_output/formatters/sympy_formatters.py
@@ -3,10 +3,7 @@ from marimo._plugins.ui._impl.anywidget.init import init_marimo_widget
 
 
 from marimo._output.formatters.formatter_factory import FormatterFactory
-#import marimo as mo
 
-#print("LOADING SIMPY")
-#from sympy import latex
 def patch_sympy(*objs):
     import marimo as mo
     """adds the _mime_() method to the sympy objects
@@ -17,10 +14,8 @@ def patch_sympy(*objs):
     """
     from sympy import latex
     for obj in objs:
-        #print("Monkey-Patching", obj)
         # the lambda below is our sympy_html 
         obj._mime_ = lambda obj: ("text/html", mo.md(f"""\\[{latex(obj)}\\]""").text)
-        #obj._mime_ = lambda obj: ("text/html", mo.md(f"""{latex(obj)}""").text)
 
 
 def sympy_as_md(obj):
@@ -41,22 +36,16 @@ class SympyFormatter(FormatterFactory):
         return "sympy"
 
     def register(self) -> None:
-        print("SYMPY FORMMATER REGISTARTION")
         import sympy  # type:ignore
-        #from sympy import Symbol, Integral, Derivative, Pow, Matrix, Mul, Add, Piecewise
-        from sympy.core.basic import Basic
         from sympy.core.basic import Printable
 
-        #from sympy.physics.quantum.state import Wavefunction
-        #monkey patching Symbol, Derivative, Pow etc
-        #monkey patch the printable class so anything that can produce output with
-        # latex(expr) e.g. latex(x**2) --> x^{2}
+        # We will monkey-patch the Printable class so most Sympy constructs
+        # that can be "pretty-printed" that with sympy.latex
+        # can also be rendered in marimo.
+        # One way to test if an experssion is supported is
+        # with latex(expr) 
+        # e.g. latex(x**2) --> x^{2}
         patch_sympy(Printable)
-        #patch_sympy(Symbol, Derivative, Pow, Matrix, Integral, Mul, 
-        #            Add, Piecewise)
-
-
-        #from sympy import symbols, Matrix, latex
 
         #from marimo._output import formatting
         # @formatting.formatter(Integral)
