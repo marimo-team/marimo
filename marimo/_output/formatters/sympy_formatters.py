16c4666e348cbce75a34073ef11ffc40aa313c0c
Francisco Agosto <fco@agostof.com>
2024-09-08 14:05:52 -0400

    add tests, cleanup

--- marimo/_output/formatters/sympy_formatters.py
+++ marimo/_output/formatters/sympy_formatters.py
@@ -1,27 +1,41 @@
+# Copyright 2024 Marimo. All rights reserved.
+from __future__ import annotations
+
+from typing import Any
+
+from marimo._messaging.mimetypes import KnownMimeType
 from marimo._output.formatters.formatter_factory import FormatterFactory
 
-def patch_sympy(*objs):
+
+def patch_sympy(*objs: Any) -> None:
     import marimo as mo
-    """adds the _mime_() method to the sympy objects
+
+    """adds the _mime_() method to sympy objects
     e.g.
     Symbol._mime_ = sympy_html
     example:
     patch_sympy(Symbol, Integral)
     """
-    from sympy import latex
+    from sympy import latex  # type: ignore
+
     for obj in objs:
-        # the lambda below is our sympy_html 
-        obj._mime_ = lambda obj: ("text/html", mo.md(f"""\\[{latex(obj)}\\]""").text)
+        # the lambda below is our sympy_html
+        obj._mime_ = lambda obj: (
+            "text/html",
+            mo.md(f"""\\[{latex(obj)}\\]""").text,
+        )
 
 
-def sympy_as_md(obj):
-    import marimo as mo
+def sympy_as_html(obj: Any) -> tuple[KnownMimeType, str]:
     from sympy import latex
-    """adds the _mime_() method to the sympy objects
+
+    import marimo as mo
+
+    """creates HTML output from a Printable Sympy object
     e.g.
-    Symbol._mime_ = sympy_html
     example:
-    patch_sympy(Symbol, Integral)
+    integral = Integral(x**2, x)
+    sympy_as_html(integral)
     """
     return ("text/html", mo.md(f"""\\[{latex(obj)}\\]""").text)
 
@@ -32,19 +46,17 @@ class SympyFormatter(FormatterFactory):
         return "sympy"
 
     def register(self) -> None:
-        import sympy  # type:ignore
-        from sympy.core.basic import Printable
+        from sympy.core.basic import Printable  # type: ignore
 
         # We will monkey-patch the Printable class so most Sympy constructs
         # that can be "pretty-printed" that with sympy.latex
         # can also be rendered in marimo.
-        # One way to test if an experssion is supported is
-        # with latex(expr) 
+        # One way to test if an expression is supported is
+        # with latex(expr)
         # e.g. latex(x**2) --> x^{2}
         patch_sympy(Printable)
 
-        #from marimo._output import formatting
+        # from marimo._output import formatting
         # @formatting.formatter(Integral)
-        #def _show_integral(integral: Integral) -> tuple[str, str]:
-        #    return sympy_as_md(integral)
-
+        # def _show_integral(integral: Integral) -> tuple[KnownMimeType, str]:
+        #    return sympy_as_html(integral)
