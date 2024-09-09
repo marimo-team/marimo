342cd62e542933ca3c2875976b496374cc558081
Akshay Agrawal <akshay@marimo.io>
2024-09-08 23:29:27 -0700

    example: unsloth (llama 3.1 8B LoRA + inference) (#2263)
    * start porting unsloth
    
    * add requirements.txt
    
    * add inline script meta

---
+++ examples/third_party/unsloth/llama_3_1_8b_2x_faster_finetuning.py
@@ -0,0 +1,404 @@
+# /// script
+# requires-python = ">=3.10"
+# dependencies = [
+#     "accelerate",
+#     "bitsandbytes",
+#     "marimo",
+#     "peft",
+#     "torch",
+#     "triton",
+#     "trl",
+#     "xformers",
+# ]
+# ///
+import marimo
+
+__generated_with = "0.8.13"
+app = marimo.App(width="full")
+
+
+@app.cell
+def __():
+    import marimo as mo
+    return mo,
+
+
+@app.cell
+def __():
+    from unsloth import FastLanguageModel
+    from transformers import TextStreamer
+
+    import torch
+    return FastLanguageModel, TextStreamer, torch
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(
+        """
+        <span class="align-center" style="display:flex; flex-direction:row; gap: 16px">
+          <a href="https://github.com/unslothai/unsloth"><img src="https://github.com/unslothai/unsloth/raw/main/images/unsloth%20new%20logo.png" width="115"></a>
+          <a href="https://discord.gg/u54VK8m8tk"><img src="https://github.com/unslothai/unsloth/raw/main/images/Discord button.png" width="145"></a>
+          <a href="https://ko-fi.com/unsloth"><img src="https://github.com/unslothai/unsloth/raw/main/images/Kofi button.png" width="145"></a></a>
+        </span>
+        This example shows how to use [Unsloth](https://github.com/unslothai/unsloth) to finetune Llama-3.1 8b. It uses
+        marimo's UI elements to make the experience interactive, so you don't need to edit any code!
+
+        _Join Discord if you need help + ⭐ Star us on <a href="https://github.com/unslothai/unsloth">Github</a>⭐_
+
+        To install Unsloth on your own computer, follow the installation instructions on our Github page [here](https://github.com/unslothai/unsloth?tab=readme-ov-file#-installation-instructions).
+
+        You will learn how to do [data prep](#Data), how to [train](#Train), how to [run the model](#Inference), & [how to save it](#Save) (eg for Llama.cpp).
+
+        [NEW] Llama-3.1 8b, 70b & 405b are trained on a crazy 15 trillion tokens with 128K long context lengths!
+
+        **[NEW] Try 2x faster inference in a free Colab for Llama-3.1 8b Instruct [here](https://colab.research.google.com/drive/1T-YBVfnphoVc8E2E854qF3jdia2Ll2W2?usp=sharing)**
+        """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(
+        r"""
+        * We support Llama, Mistral, Phi-3, Gemma, Yi, DeepSeek, Qwen, TinyLlama, Vicuna, Open Hermes etc
+        * We support 16bit LoRA or 4bit QLoRA. Both 2x faster.
+        * `max_seq_length` can be set to anything, since we do automatic RoPE Scaling via [kaiokendev's](https://kaiokendev.github.io/til) method.
+        * [**NEW**] We make Gemma-2 9b / 27b **2x faster**! See our [Gemma-2 9b notebook](https://colab.research.google.com/drive/1vIrqH5uYDQwsJ4-OO3DErvuv4pBgVwk4?usp=sharing)
+        * [**NEW**] To finetune and auto export to Ollama, try our [Ollama notebook](https://colab.research.google.com/drive/1WZDi7APtQ9VsvOrQSSC5DDtxq159j8iZ?usp=sharing)
+        * [**NEW**] We make Mistral NeMo 12B 2x faster and fit in under 12GB of VRAM! [Mistral NeMo notebook](https://colab.research.google.com/drive/17d3U-CAIwzmbDRqbZ9NnpHxCkmXB6LZ0?usp=sharing)
+        """
+    )
+    return
+
+
+@app.cell
+def __(mo):
+    mo.md("### Load the model")
+    return
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md("""Start by choosing a couple of parameters:""")
+    return
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    max_seq_length = mo.ui.number(
+        value=2048, start=1, stop=4096, label="Max sequence length"
+    )
+
+    load_in_4bit = mo.ui.checkbox(value=True, label="Load in 4-bit?")
+
+    mo.vstack([max_seq_length, load_in_4bit])
+    return load_in_4bit, max_seq_length
+
+
+@app.cell
+def __():
+    dtype = None  # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+    return dtype,
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(r"""We now add LoRA adapters so we only need to update 1 to 10% of all parameters!""")
+    return
+
+
+@app.cell
+def __(FastLanguageModel, dtype, load_in_4bit, max_seq_length):
+    model, tokenizer = FastLanguageModel.from_pretrained(
+        model_name="unsloth/Meta-Llama-3.1-8B",
+        max_seq_length=max_seq_length.value,
+        dtype=dtype,
+        load_in_4bit=load_in_4bit.value,
+        # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
+    )
+    model = FastLanguageModel.get_peft_model(
+        model,
+        r=16,  # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+        target_modules=[
+            "q_proj",
+            "k_proj",
+            "v_proj",
+            "o_proj",
+            "gate_proj",
+            "up_proj",
+            "down_proj",
+        ],
+        lora_alpha=16,
+        lora_dropout=0,  # Supports any, but = 0 is optimized
+        bias="none",  # Supports any, but = "none" is optimized
+        # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+        use_gradient_checkpointing="unsloth",  # True or "unsloth" for very long context
+        random_state=3407,
+        use_rslora=False,  # We support rank stabilized LoRA
+        loftq_config=None,  # And LoftQ
+    )
+    return model, tokenizer
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(
+        r"""
+        <a name="Data"></a>
+        ### Data Prep
+        We now use the Alpaca dataset from [yahma](https://huggingface.co/datasets/yahma/alpaca-cleaned), which is a filtered version of 52K of the original [Alpaca dataset](https://crfm.stanford.edu/2023/03/13/alpaca.html). You can replace this code section with your own data prep.
+
+        **[NOTE]** To train only on completions (ignoring the user's input) read TRL's docs [here](https://huggingface.co/docs/trl/sft_trainer#train-on-completions-only).
+
+        **[NOTE]** Remember to add the **EOS_TOKEN** to the tokenized output!! Otherwise you'll get infinite generations!
+
+        If you want to use the `llama-3` template for ShareGPT datasets, try our conversational [notebook](https://colab.research.google.com/drive/1XamvWYinY6FOSX9GLvnqSjjsNflxdhNc?usp=sharing).
+
+        For text completions like novel writing, try this [notebook](https://colab.research.google.com/drive/1ef-tab5bhkvWmBOObepl1WgJvfvSzn5Q?usp=sharing).
+        """
+    )
+    return
+
+
+@app.cell
+def __(tokenizer):
+    alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+    ### Instruction:
+    {}
+
+    ### Input:
+    {}
+
+    ### Response:
+    {}"""
+
+    EOS_TOKEN = tokenizer.eos_token  # Must add EOS_TOKEN
+
+
+    def formatting_prompts_func(examples):
+        instructions = examples["instruction"]
+        inputs = examples["input"]
+        outputs = examples["output"]
+        texts = []
+        for instruction, input, output in zip(instructions, inputs, outputs):
+            # Must add EOS_TOKEN, otherwise your generation will go on forever!
+            text = alpaca_prompt.format(instruction, input, output) + EOS_TOKEN
+            texts.append(text)
+        return {
+            "text": texts,
+        }
+
+
+    pass
+
+    from datasets import load_dataset
+
+    dataset = load_dataset("yahma/alpaca-cleaned", split="train")
+    dataset = dataset.map(
+        formatting_prompts_func,
+        batched=True,
+    )
+    return (
+        EOS_TOKEN,
+        alpaca_prompt,
+        dataset,
+        formatting_prompts_func,
+        load_dataset,
+    )
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(
+        r"""
+        ### Train the model
+        Now let's use Huggingface TRL's `SFTTrainer`! More docs here: [TRL SFT docs](https://huggingface.co/docs/trl/sft_trainer). We do 60 steps to speed things up, but you can set `num_train_epochs=1` for a full run, and turn off `max_steps=None`. We also support TRL's `DPOTrainer`!
+        """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def __(dataset, max_seq_length, model, tokenizer):
+    from trl import SFTTrainer
+    from transformers import TrainingArguments
+    from unsloth import is_bfloat16_supported
+
+    trainer = SFTTrainer(
+        model=model,
+        tokenizer=tokenizer,
+        train_dataset=dataset,
+        dataset_text_field="text",
+        max_seq_length=max_seq_length,
+        dataset_num_proc=2,
+        packing=False,  # Can make training 5x faster for short sequences.
+        args=TrainingArguments(
+            per_device_train_batch_size=2,
+            gradient_accumulation_steps=4,
+            warmup_steps=5,
+            # num_train_epochs = 1, # Set this for 1 full training run.
+            max_steps=60,
+            learning_rate=2e-4,
+            fp16=not is_bfloat16_supported(),
+            bf16=is_bfloat16_supported(),
+            logging_steps=1,
+            optim="adamw_8bit",
+            weight_decay=0.01,
+            lr_scheduler_type="linear",
+            seed=3407,
+            output_dir="outputs",
+        ),
+    )
+    return SFTTrainer, TrainingArguments, is_bfloat16_supported, trainer
+
+
+@app.cell(hide_code=True)
+def __(torch):
+    # @title Show current memory stats
+    gpu_stats = torch.cuda.get_device_properties(0)
+    start_gpu_memory = round(
+        torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3
+    )
+    max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
+    print(f"GPU = {gpu_stats.name}. Max memory = {max_memory} GB.")
+    print(f"{start_gpu_memory} GB of memory reserved.")
+    return gpu_stats, max_memory, start_gpu_memory
+
+
+@app.cell
+def __(mo):
+    train_button = mo.ui.run_button(label="Click to train!", kind="danger"); train_button.center()
+    return train_button,
+
+
+@app.cell
+def __(train_button, trainer):
+    if train_button.value:
+        trainer_stats = trainer.train()
+    else:
+        trainer_stats = None
+    return trainer_stats,
+
+
+@app.cell
+def __(max_memory, mo, start_gpu_memory, torch, trainer_stats):
+    mo.stop(trainer_stats is None, mo.md("Train the model 👆"))
+
+    # Show final memory and time stats
+    used_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+    used_memory_for_lora = round(used_memory - start_gpu_memory, 3)
+    used_percentage = round(used_memory / max_memory * 100, 3)
+    lora_percentage = round(used_memory_for_lora / max_memory * 100, 3)
+    print(f"{trainer_stats.metrics['train_runtime']} seconds used for training.")
+    print(
+        f"{round(trainer_stats.metrics['train_runtime']/60, 2)} minutes used for training."
+    )
+    print(f"Peak reserved memory = {used_memory} GB.")
+    print(f"Peak reserved memory for training = {used_memory_for_lora} GB.")
+    print(f"Peak reserved memory % of max memory = {used_percentage} %.")
+    print(
+        f"Peak reserved memory for training % of max memory = {lora_percentage} %."
+    )
+    return (
+        lora_percentage,
+        used_memory,
+        used_memory_for_lora,
+        used_percentage,
+    )
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(
+        r"""
+        <a name="Inference"></a>
+        ### Inference
+        Let's run the model! You can change the instruction and input - leave the output blank!
+
+        **[NEW] Try 2x faster inference in a free Colab for Llama-3.1 8b Instruct [here](https://colab.research.google.com/drive/1T-YBVfnphoVc8E2E854qF3jdia2Ll2W2?usp=sharing)**
+        """
+    )
+    return
+
+
+@app.cell
+def __(mo):
+    inf_instr_inp = mo.md(
+        """
+        {instr}
+
+        {inp}
+        """
+    ).batch(
+        instr=mo.ui.text_area(placeholder="Your instruction ..."),
+        inp=mo.ui.text_area(placeholder="Your input ..."),
+    ).form()
+
+
+    inf_instr_inp
+    return inf_instr_inp,
+
+
+@app.cell
+def __(
+    FastLanguageModel,
+    alpaca_prompt,
+    inf_instr_inp,
+    mo,
+    model,
+    tokenizer,
+):
+    mo.stop(inf_instr_inp.value is None)
+
+    FastLanguageModel.for_inference(model)  # Enable native 2x faster inference
+    _inputs = tokenizer(
+        [
+            alpaca_prompt.format(
+                inf_instr_inp.value["instr"],
+                inf_instr_inp.value["inp"],
+                "",  # output - leave this blank for generation!
+            )
+        ],
+        return_tensors="pt",
+    ).to("cuda")
+
+    outputs = model.generate(**_inputs, max_new_tokens=64, use_cache=True)
+    tokenizer.batch_decode(outputs)
+    return outputs,
+
+
+@app.cell(hide_code=True)
+def __(mo):
+    mo.md(
+        r"""
+        And we're done! If you have any questions on Unsloth, we have a [Discord](https://discord.gg/u54VK8m8tk) channel! If you find any bugs or want to keep updated with the latest LLM stuff, or need help, join projects etc, feel free to join our Discord!
+
+        Some other links:
+        1. Zephyr DPO 2x faster [free Colab](https://colab.research.google.com/drive/15vttTpzzVXv_tJwEk-hIcQ0S9FcEWvwP?usp=sharing)
+        2. Llama 7b 2x faster [free Colab](https://colab.research.google.com/drive/1lBzz5KeZJKXjvivbYvmGarix9Ao6Wxe5?usp=sharing)
+        3. TinyLlama 4x faster full Alpaca 52K in 1 hour [free Colab](https://colab.research.google.com/drive/1AZghoNBQaMDgWJpi4RbffGM1h6raLUj9?usp=sharing)
+        4. CodeLlama 34b 2x faster [A100 on Colab](https://colab.research.google.com/drive/1y7A0AxE3y8gdj4AVkl2aZX47Xu3P1wJT?usp=sharing)
+        5. Mistral 7b [free Kaggle version](https://www.kaggle.com/code/danielhanchen/kaggle-mistral-7b-unsloth-notebook)
+        6. We also did a [blog](https://huggingface.co/blog/unsloth-trl) with 🤗 HuggingFace, and we're in the TRL [docs](https://huggingface.co/docs/trl/main/en/sft_trainer#accelerate-fine-tuning-2x-using-unsloth)!
+        7. `ChatML` for ShareGPT datasets, [conversational notebook](https://colab.research.google.com/drive/1Aau3lgPzeZKQ-98h69CCu1UJcvIBLmy2?usp=sharing)
+        8. Text completions like novel writing [notebook](https://colab.research.google.com/drive/1ef-tab5bhkvWmBOObepl1WgJvfvSzn5Q?usp=sharing)
+        9. [**NEW**] We make Phi-3 Medium / Mini **2x faster**! See our [Phi-3 Medium notebook](https://colab.research.google.com/drive/1hhdhBa1j_hsymiW9m-WzxQtgqTH_NHqi?usp=sharing)
+        10. [**NEW**] We make Gemma-2 9b / 27b **2x faster**! See our [Gemma-2 9b notebook](https://colab.research.google.com/drive/1vIrqH5uYDQwsJ4-OO3DErvuv4pBgVwk4?usp=sharing)
+        11. [**NEW**] To finetune and auto export to Ollama, try our [Ollama notebook](https://colab.research.google.com/drive/1WZDi7APtQ9VsvOrQSSC5DDtxq159j8iZ?usp=sharing)
+        12. [**NEW**] We make Mistral NeMo 12B 2x faster and fit in under 12GB of VRAM! [Mistral NeMo notebook](https://colab.research.google.com/drive/17d3U-CAIwzmbDRqbZ9NnpHxCkmXB6LZ0?usp=sharing)
+
+        <div class="align-center">
+          <a href="https://github.com/unslothai/unsloth"><img src="https://github.com/unslothai/unsloth/raw/main/images/unsloth%20new%20logo.png" width="115"></a>
+          <a href="https://discord.gg/u54VK8m8tk"><img src="https://github.com/unslothai/unsloth/raw/main/images/Discord.png" width="145"></a>
+          <a href="https://ko-fi.com/unsloth"><img src="https://github.com/unslothai/unsloth/raw/main/images/Kofi button.png" width="145"></a></a> Support our work if you can! Thanks!
+        </div>
+        """
+    )
+    return
+
+
+if __name__ == "__main__":
+    app.run()
