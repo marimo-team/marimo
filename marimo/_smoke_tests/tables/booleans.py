53254b0e2098b5bae2fc56ed087409310b74f2c0
Myles Scolnick <myles@marimo.io>
2024-09-10 14:25:39 -0400

    better test case

--- marimo/_smoke_tests/tables/booleans.py
+++ marimo/_smoke_tests/tables/booleans.py
@@ -15,22 +15,33 @@ app = marimo.App(width="medium")
 @app.cell
 def __():
     import marimo as mo
-    return mo,
+    import pandas as pd
+    import polars as pl
+    return mo, pd, pl
 
 
 @app.cell
-def __():
-    import pandas as pd
-
+def __(pd):
     data = {
         "A": [True, True, True],
         "B": [False, False, False],
         "C": [True, True, False],
     }
 
-    df = pd.DataFrame(data)
-    df
-    return data, df, pd
+    pd.DataFrame(data)
+    return data,
+
+
+@app.cell
+def __(data, pl):
+    pl.DataFrame(data)
+    return
+
+
+@app.cell
+def __(data, mo):
+    mo.ui.table(data)
+    return
 
 
 if __name__ == "__main__":
