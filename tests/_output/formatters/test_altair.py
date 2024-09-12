a6c6630e31435fe0eabd36f36cb96973787e1d3e
Myles Scolnick <myles@marimo.io>
2024-09-12 18:21:55 +0200

    fix: handle altair usermeta embedoptions manually (#2303)

--- tests/_output/formatters/test_altair.py
+++ tests/_output/formatters/test_altair.py
@@ -49,6 +49,55 @@ async def test_altair(
 
 
 @pytest.mark.skipif(
+    not HAS_ALTAIR, reason="optional dependencies not installed"
+)
+async def test_altair_with_embed_options(
+    executing_kernel: Kernel, exec_req: ExecReqProvider
+) -> None:
+    from marimo._output.formatters.formatters import register_formatters
+
+    register_formatters()
+
+    await executing_kernel.run(
+        [
+            exec_req.get(
+                """
+                import altair as alt
+                import pandas as pd
+                from marimo._output.formatting import get_formatter
+
+                data = pd.DataFrame({"a": ["A", "B"], "b": [28, 55]})
+                chart = alt.Chart(data).mark_bar().encode(x="a", y="b")
+                formatter = get_formatter(chart)
+
+                before_options_result = formatter(chart)
+                alt.renderers.set_embed_options(actions=False)
+                after_options_result = formatter(chart)
+                """
+            )
+        ]
+    )
+
+    result = executing_kernel.globals["before_options_result"]
+    assert result is not None
+    assert result[0] == "application/vnd.vegalite.v5+json"
+    json_result = json.loads(result[1])
+    assert json_result["usermeta"] == {
+        "embedOptions": {},
+    }
+
+    result = executing_kernel.globals["after_options_result"]
+    assert result is not None
+    assert result[0] == "application/vnd.vegalite.v5+json"
+    json_result = json.loads(result[1])
+    assert json_result["usermeta"] == {
+        "embedOptions": {
+            "actions": False,
+        },
+    }
+
+
+@pytest.mark.skipif(
     not HAS_ALTAIR or not HAS_VL_CONVERT,
     reason="optional dependencies not installed and fails on mac",
 )
