cee05ccdbe8e13ec183b195f5f17d9b1b9df0b9d
eugene <eugeneheiner14@gmail.com>
2024-09-21 00:25:33 +0800

    fix#2364: implement marimo.status.toast (#2365)
    * add missing doc for `disabled` arg in marimo.status.progress_bar
    
    * implement marimo.status.toast
    
    * add smoke_test for marimo.status.toast
    
    * fix lint error
    
    * rename variant to kind
    
    * update smoke test

--- marimo/_plugins/stateless/status/__init__.py
+++ marimo/_plugins/stateless/status/__init__.py
@@ -1,6 +1,10 @@
 # Copyright 2024 Marimo. All rights reserved.
 """Create loading indicators."""
 
-__all__ = ["progress_bar", "spinner"]
+__all__ = ["progress_bar", "spinner", "toast"]
 
-from marimo._plugins.stateless.status._progress import progress_bar, spinner
+from marimo._plugins.stateless.status._progress import (
+    progress_bar,
+    spinner,
+    toast,
+)
