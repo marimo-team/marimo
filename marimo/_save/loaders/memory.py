8bee1e92cdbd68203e66787e80fab423a710d1fd
Akshay Agrawal <akshay@marimo.io>
2024-09-26 16:37:09 -0400

    lru_cache thread-safety, remove maxsize from cache (#2420)
    * - some more docs (in progress)
    - remove maxsize from cache()
    - make lru_cache threadsafe
    
    * update
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update marimo/_save/loaders/memory.py
    
    Co-authored-by: Dylan Madisetti <dylan@madisetti.me>
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Dylan Madisetti <dylan@madisetti.me>

--- marimo/_save/loaders/memory.py
+++ marimo/_save/loaders/memory.py
@@ -1,14 +1,18 @@
 # Copyright 2024 Marimo. All rights reserved.
 from __future__ import annotations
 
+import threading
 from collections import OrderedDict
-from typing import TYPE_CHECKING, Any, Optional, Union
+from typing import TYPE_CHECKING, Any, Callable, Optional, TypeVar, Union
 
 from marimo._save.cache import Cache, CacheType
 from marimo._save.loaders.loader import INCONSISTENT_CACHE_BOILER_PLATE, Loader
 
 if TYPE_CHECKING:
     from pathlib import Path
+
+
+T = TypeVar("T")
 
 
 class MemoryLoader(Loader):
@@ -29,16 +33,26 @@ class MemoryLoader(Loader):
         # Normal python dicts are atomic, ordered dictionaries are not.
         # As such, default to normal dict if not LRU.
         self._cache = {}
+        # ordered dict is protected by a lock
+        self._cache_lock: threading.Lock | None = None
         if self.is_lru:
             self._cache = OrderedDict()
+            self._cache_lock = threading.Lock()
         self.max_size = max_size
         self.hits = 0
         if cache is not None:
-            self._cache.update(cache)
+            self._maybe_lock(lambda: self._cache.update(cache))
+
+    def _maybe_lock(self, fn: Callable[..., T]) -> T:
+        if self._cache_lock is not None:
+            with self._cache_lock:
+                return fn()
+        else:
+            return fn()
 
     def cache_hit(self, hashed_context: str, cache_type: CacheType) -> bool:
         key = self.build_path(hashed_context, cache_type)
-        return key in self._cache
+        return self._maybe_lock(lambda: key in self._cache)
 
     def load_cache(self, hashed_context: str, cache_type: CacheType) -> Cache:
         assert self.cache_hit(
@@ -48,27 +62,40 @@ class MemoryLoader(Loader):
         key = self.build_path(hashed_context, cache_type)
         if self.is_lru:
             assert isinstance(self._cache, OrderedDict)
-            self._cache.move_to_end(key)
+            assert self._cache_lock is not None
+            with self._cache_lock:
+                self._cache.move_to_end(key)
         return self._cache[key]
 
     def save_cache(self, cache: Cache) -> None:
         key = self.build_path(cache.hash, cache.cache_type)
-        self._cache[key] = cache
         # LRU
         if self.is_lru:
             assert isinstance(self._cache, OrderedDict)
-            self._cache.move_to_end(key)
-            if len(self._cache) > self.max_size:
-                self._cache.popitem(last=False)
+            assert self._cache_lock is not None
+            with self._cache_lock:
+                self._cache[key] = cache
+                self._cache.move_to_end(key)
+                if len(self._cache) > self.max_size:
+                    self._cache.popitem(last=False)
+        self._cache[key] = cache
 
     def resize(self, max_size: int) -> None:
         if not self.is_lru:
             self.is_lru = max_size > 0
             if self.is_lru:
                 self._cache = OrderedDict(self._cache.items())
+                self._cache_lock = threading.Lock()
             self.max_size = max_size
             return
         assert isinstance(self._cache, OrderedDict)
-        while len(self._cache) > max_size:
-            self._cache.popitem(last=False)
+        assert self._cache_lock is not None
+        with self._cache_lock:
+            self.is_lru = max_size > 0
+            if not self.is_lru:
+                self._cache = dict(self._cache.items())
+                self.max_size = max_size
+                return
+            while len(self._cache) > max_size:
+                self._cache.popitem(last=False)
         self.max_size = max_size
