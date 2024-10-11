9511312fcef3b9808f166ad1beba97480b230403
Akshay Agrawal <akshay@marimo.io>
2024-10-11 16:58:29 -0700

    chore: move marimo-base logic into bash script (#2606)
    Consolidate logic for adapting pyproject.toml into a script

---
+++ scripts/modify_pyproject_for_marimo_base.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Python build tools generally only allow building one project
+# per Python project. This modifies the pyproject.toml
+# to build marimo-base, a slimmed down marimo distribution with
+# no static artifacts.
+# 
+# Adapted from https://github.com/cvxpy/cvxpy/blob/297278e2a88db3c0084750052a16e60672074da3/.github/workflows/build.yml#L169C1-L180C1
+#
+# Mac has a different syntax for sed -i, this works across oses
+sed -i.bak -e 's/name = "marimo"/name = "marimo-base"/g' pyproject.toml
+# Remove static artifacts
+sed -i.bak '/artifacts = /d' pyproject.toml
+rm -rf pyproject.toml.bak
