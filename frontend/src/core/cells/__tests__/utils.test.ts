de282e6758aa310b4c93356dbb5f2ae750be2cae
Akshay Agrawal <akshay@marimo.io>
2024-10-12 15:38:09 -0700

    fix: showing hidden cells shouldn't change needsSave
    As a hack, ignore `hide_code` cell config in notebookNeedsSave

--- frontend/src/core/cells/__tests__/utils.test.ts
+++ frontend/src/core/cells/__tests__/utils.test.ts
@@ -1,9 +1,15 @@
 /* Copyright 2024 Marimo. All rights reserved. */
 import { describe, it, expect } from "vitest";
-import { getCellConfigs, type NotebookState } from "@/core/cells/cells";
+import {
+  getCellConfigs,
+  type LastSavedNotebook,
+  type NotebookState,
+} from "@/core/cells/cells";
 import { CellId } from "@/core/cells/ids";
 import { MultiColumn } from "@/utils/id-tree";
 import type { CellData, CellRuntimeState } from "@/core/cells/types";
+import { notebookNeedsSave } from "@/core/cells/utils.ts";
+import type { LayoutState } from "@/core/layout/layout";
 
 describe("getCellConfigs", () => {
   it("should return correct cell configs with column indices", () => {
@@ -111,5 +117,69 @@ describe("getCellConfigs", () => {
     const result = getCellConfigs(mockState);
 
     expect(result).toEqual([]);
+  });
+});
+
+describe("notebookNeedsSave", () => {
+  it("should ignore hide_code", () => {
+    // Create a mock NotebookState
+    const cellId1 = CellId.create();
+    const mockState: NotebookState = {
+      cellIds: MultiColumn.from([[cellId1]]),
+      cellData: {
+        [cellId1]: {
+          id: cellId1,
+          name: "hello",
+          code: "hello",
+          config: { hide_code: false, disabled: false },
+        } as CellData,
+      },
+      cellRuntime: {},
+      cellHandles: {},
+      history: [],
+      scrollKey: null,
+      cellLogs: [],
+    };
+
+    const mockLayoutState: LayoutState = {
+      selectedLayout: "vertical",
+      layoutData: {},
+    };
+
+    // These is the same as mockState
+    let mockLastSavedState: LastSavedNotebook = {
+      codes: ["hello"],
+      configs: [{ hide_code: false, disabled: false }],
+      names: ["hello"],
+      layout: mockLayoutState,
+    };
+
+    expect(
+      notebookNeedsSave(mockState, mockLayoutState, mockLastSavedState),
+    ).toBe(false);
+
+    // hide_code should be ignored
+    mockLastSavedState = {
+      codes: ["hello"],
+      configs: [{ hide_code: true, disabled: false }],
+      names: ["hello"],
+      layout: mockLayoutState,
+    };
+
+    expect(
+      notebookNeedsSave(mockState, mockLayoutState, mockLastSavedState),
+    ).toBe(false);
+
+    // disabled should not be ignored
+    mockLastSavedState = {
+      codes: ["hello"],
+      configs: [{ hide_code: true, disabled: true }],
+      names: ["hello"],
+      layout: mockLayoutState,
+    };
+
+    expect(
+      notebookNeedsSave(mockState, mockLayoutState, mockLastSavedState),
+    ).toBe(true);
   });
 });
