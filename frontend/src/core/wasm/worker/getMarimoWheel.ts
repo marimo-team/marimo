387f239af7fa20b3d695fbe3a5811be07e8cca64
Myles Scolnick <myles@marimo.io>
2024-10-14 14:17:40 -0400

    perf: move to marimo base (#2625)

--- frontend/src/core/wasm/worker/getMarimoWheel.ts
+++ frontend/src/core/wasm/worker/getMarimoWheel.ts
@@ -1,7 +1,7 @@
 /* Copyright 2024 Marimo. All rights reserved. */
 export function getMarimoWheel(version: string) {
   if (!version) {
-    return "marimo >= 0.3.0";
+    return "marimo-base>=0.9.0";
   }
   if (version === "local") {
     return `http://localhost:8000/dist/marimo-${
@@ -9,7 +9,7 @@ export function getMarimoWheel(version: string) {
     }-py3-none-any.whl`;
   }
   if (version === "latest") {
-    return "marimo";
+    return "marimo-base";
   }
-  return `marimo==${version}`;
+  return `marimo-base==${version}`;
 }
