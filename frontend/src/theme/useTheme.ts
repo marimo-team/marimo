c843aea4c014b2c942190f23e139900bba04d752
Myles Scolnick <myles@marimo.io>
2024-10-22 14:11:11 -0400

    fix: simplify islands color-schema

--- frontend/src/theme/useTheme.ts
+++ frontend/src/theme/useTheme.ts
@@ -10,27 +10,10 @@ export type ResolvedTheme = "light" | "dark";
 export const THEMES: Theme[] = ["light", "dark", "system"];
 
 const themeAtom = atom((get) => {
-  // If it is islands, try a few ways to infer if it is dark mode.
   if (isIslands()) {
-    // If it has a dark mode class on the body, use dark mode.
-    if (
-      document.body.classList.contains("dark-mode") ||
-      document.body.classList.contains("dark")
-    ) {
-      return "dark";
-    }
-    // If it has data-theme=dark or data-mode=dark on the body, use dark mode.
-    if (
-      document.body.dataset.theme === "dark" ||
-      document.body.dataset.mode === "dark" ||
-      document.body.dataset.vscodeThemeKind === "vscode-dark" ||
-      document.body.dataset.vscodeThemeKind === "vscode-high-contrast"
-    ) {
-      return "dark";
-    }
-    // We don't want to infer from the system theme,
-    // since the island consumer may not support dark mode.
-    return "light";
+    // If it is islands, we use the system theme,
+    // which infers from color-scheme: light or dark.
+    return "system";
   }
 
   return get(userConfigAtom).display.theme;
