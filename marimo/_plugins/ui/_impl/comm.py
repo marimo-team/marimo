165723a76998962fd8d013881600791fcab8edeb
Philipp Rudiger <prudiger@anaconda.com>
2024-10-24 21:40:02 +0200

    Apply suggestions from code review

--- marimo/_plugins/ui/_impl/comm.py
+++ marimo/_plugins/ui/_impl/comm.py
@@ -151,6 +151,7 @@ class MarimoComm:
 
         while self._publish_message_buffer:
             data, _metadata, buffers = self._publish_message_buffer.pop(0)
+
             SendUIElementMessage(
                 ui_element=self.ui_element_id,
                 message=data,
