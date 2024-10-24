31d20eb09030d2bf3c71867e5239f093f75e238e
metaboulie <eugeneheiner14@gmail.com>
2024-10-24 21:05:57 +0800

    update example for marimo dictionary

--- examples/ui/arrays_and_dicts.py
+++ examples/ui/arrays_and_dicts.py
@@ -7,7 +7,7 @@
 
 import marimo
 
-__generated_with = "0.8.19"
+__generated_with = "0.9.14"
 app = marimo.App()
 
 
@@ -76,6 +76,55 @@ def __(mo):
 def __(array, dictionary, mo):
     mo.hstack([array.value, dictionary.value], justify="space-around")
     return
+
+
+@app.cell
+def __(mo):
+    mo.md(
+        r"""
+        Fundamental difference between marimo dict and standard python dict.
+
+        The UI elements in marimo dict are clones of the original: interacting with the marimo dictionary will not update the original elements, and vice versa.
+
+        This allows you to reuse multiple UI components (e.g., sliders, text inputs, or date pickers) independently, ensuring they don’t interfere with each other when managed through marimo dict.
+
+        However, if you use a regular python dict, all references remain synchronized, and changes to one element propagate across all linked instances.
+        """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def __(create, mo):
+    create
+
+    slider = mo.ui.slider(1, 10, show_value=True)
+    text = mo.ui.text()
+    date = mo.ui.date()
+
+    mo_d = mo.ui.dictionary(
+        {
+            "slider": slider,
+            "text": text,
+            "date": date,
+        }
+    )
+
+    py_d = {
+        "slider": slider,
+        "text": text,
+        "date": date,
+    }
+
+    mo.hstack(
+        [
+            mo.vstack(["marimo dict", mo_d]),
+            mo.vstack(["original elements", mo.vstack([slider, text, date])]),
+            mo.vstack(["python dict", py_d]),
+        ],
+        justify="space-around",
+    )
+    return date, mo_d, py_d, slider, text
 
 
 @app.cell
