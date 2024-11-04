4806d0fe200404dd23a093dbfc2cba1188cce170
Myles Scolnick <myles@marimo.io>
2024-11-04 10:01:21 -0500

    fix: include index.html in marimo-base for html export

---
+++ scripts/validate_base_wheel_size.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wheel_file=$(ls dist/*.whl)
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # macOS
+  size=$(stat -f%z "$wheel_file")
+else
+  # Linux
+  size=$(stat -c%s "$wheel_file")
+fi
+
+if [ $size -gt 2097152 ]; then
+  echo "Wheel file is larger than 2mb"
+  exit 1
+fi
+
+echo "Wheel file is $size bytes"
