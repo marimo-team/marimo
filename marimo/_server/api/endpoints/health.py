9410f57dc5b7ffc77fbfdf42b97a235082ae4024
Myles Scolnick <myles@marimo.io>
2024-11-11 09:58:58 -0500

    docs: add docs for specifying python version while using --sandbox

--- marimo/_server/api/endpoints/health.py
+++ marimo/_server/api/endpoints/health.py
@@ -10,7 +10,11 @@ from starlette.responses import JSONResponse, PlainTextResponse
 from marimo import __version__, _loggers
 from marimo._server.api.deps import AppState
 from marimo._server.router import APIRouter
-from marimo._utils.health import get_node_version, get_required_modules_list
+from marimo._utils.health import (
+    get_node_version,
+    get_python_version,
+    get_required_modules_list,
+)
 
 if TYPE_CHECKING:
     from starlette.requests import Request
@@ -76,6 +80,7 @@ async def status(request: Request) -> JSONResponse:
             "mode": app_state.mode,
             "sessions": len(app_state.session_manager.sessions),
             "version": __version__,
+            "python_version": get_python_version(),
             "requirements": get_required_modules_list(),
             "node_version": get_node_version(),
             "lsp_running": app_state.session_manager.lsp_server.is_running(),
