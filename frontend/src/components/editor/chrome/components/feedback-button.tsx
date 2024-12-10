9525b9a73faa844f3490c9a6fb70d68c11d2f18f
Akshay Agrawal <akshay@marimo.io>
2024-12-10 15:52:41 -0800

    edit

--- frontend/src/components/editor/chrome/components/feedback-button.tsx
+++ frontend/src/components/editor/chrome/components/feedback-button.tsx
@@ -96,8 +96,8 @@ const FeedbackModal: React.FC<{
               </li>
             </ul>
             <p className="my-2 prose">
-              We're excited you're here as we discover and build the future of
-              Python data tooling together. Thanks for being part of our
+              We're excited you're here as we build the future of
+              Python data tooling. Thanks for being part of our
               community!
             </p>
           </DialogDescription>
