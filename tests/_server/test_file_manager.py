5cf1b0b3d69c6d9111dc2ad6b4b1bc306416b67f
Myles Scolnick <myles@marimo.io>
2024-12-11 22:09:49 -0500

    fix

--- tests/_server/test_file_manager.py
+++ tests/_server/test_file_manager.py
@@ -239,7 +239,7 @@ def test_to_code(app_file_manager: AppFileManager) -> None:
             "",
             "",
             "@app.cell",
-            "def __f649():",
+            "def __f649a0():",
             "    import marimo as mo",
             "    return (mo,)",
             "",
