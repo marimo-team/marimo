266d895b5ef845fd251f65c828c0ac72a55f4143
Shahmir Varqha <sham9871@gmail.com>
2024-12-14 16:01:19 +0800

    add experimental tracing panel

---
+++ frontend/src/components/editor/chrome/panels/tracing-panel.tsx
@@ -0,0 +1,7 @@
+/* Copyright 2024 Marimo. All rights reserved. */
+import { Tracing } from "@/components/tracing/tracing";
+import React from "react";
+
+export const TracingPanel: React.FC = () => {
+  return <Tracing />;
+};
