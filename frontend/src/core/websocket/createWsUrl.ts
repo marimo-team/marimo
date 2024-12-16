0cc583f8c7dea3c09680be5ea88159e7567682f5
Myles Scolnick <myles@marimo.io>
2024-12-16 16:06:31 -0500

    fix: resolve relative URL for old browsers (#3183)
    Some older browsers don't support relative websocket urls so we need to
    resolve them ourselves
    
    Fixes #3167

--- frontend/src/core/websocket/createWsUrl.ts
+++ frontend/src/core/websocket/createWsUrl.ts
@@ -1,9 +1,21 @@
 /* Copyright 2024 Marimo. All rights reserved. */
+import { Strings } from "@/utils/strings";
 import { KnownQueryParams } from "../constants";
 
 export function createWsUrl(sessionId: string): string {
   const searchParams = new URLSearchParams(window.location.search);
   searchParams.set(KnownQueryParams.sessionId, sessionId);
 
-  return `ws?${searchParams.toString()}`;
+  return resolveToWsUrl(`ws?${searchParams.toString()}`);
+}
+
+export function resolveToWsUrl(relativeUrl: string): string {
+  if (relativeUrl.startsWith("ws:") || relativeUrl.startsWith("wss:")) {
+    return relativeUrl;
+  }
+  const baseUri = new URL(document.baseURI);
+  const protocol = baseUri.protocol === "https:" ? "wss:" : "ws:";
+  const host = baseUri.host;
+  const pathname = baseUri.pathname;
+  return `${protocol}//${host}${Strings.withoutTrailingSlash(pathname)}/${Strings.withoutLeadingSlash(relativeUrl)}`;
 }
