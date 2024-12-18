e43b257096241159267abda7d50d6859f07bdeba
Akshay Agrawal <akshay@marimo.io>
2024-12-17 19:03:57 -0800

    fix: toml parse error for recent notebooks (#3204)
    Fixes #3202 
    
    An uncaught exception was bringing down the server.

--- marimo/_server/recents.py
+++ marimo/_server/recents.py
@@ -6,6 +6,7 @@ import pathlib
 from dataclasses import dataclass, field
 from typing import List
 
+from marimo import _loggers
 from marimo._server.models.home import MarimoFile
 from marimo._utils.config.config import ConfigReader
 from marimo._utils.paths import pretty_path
@@ -16,7 +17,10 @@ class RecentFilesState:
     files: List[str] = field(default_factory=list)
 
 
+# TODO(akshayka): _IGNORED_FOLDERS doesn't cover Windows
 _IGNORED_FOLDERS = ("/tmp", "/var")
+
+LOGGER = _loggers.marimo_logger()
 
 
 def _is_tmp_file(filename: str) -> bool:
@@ -39,14 +43,29 @@ class RecentFilesManager:
         if _is_tmp_file(filename):
             return
 
-        state = self.config.read_toml(
-            RecentFilesState, fallback=RecentFilesState()
-        )
+        try:
+            state = self.config.read_toml(
+                RecentFilesState, fallback=RecentFilesState()
+            )
+        except Exception as e:
+            LOGGER.error("Failed to read recent notebook at %s", self.LOCATION)
+            LOGGER.error(str(e))
+            # On error we overwrite the corrupted recents file; nothing
+            # of significance is lost
+            state = RecentFilesState()
+
         if filename in state.files:
             state.files.remove(filename)
         state.files.insert(0, filename)
         state.files = state.files[: self.MAX_FILES]
-        self.config.write_toml(state)
+
+        try:
+            self.config.write_toml(state)
+        except Exception as e:
+            LOGGER.error(
+                "Failed to write recent notebook at %s", self.LOCATION
+            )
+            LOGGER.error(str(e))
 
     def rename(self, old_filename: str, new_filename: str) -> None:
         if not self.config:
