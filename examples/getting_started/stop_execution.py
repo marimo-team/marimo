65c8c4da84876e300b399b5b68dc0df9e30ac18a
Akshay Agrawal <akshay@marimo.io>
2024-12-19 21:03:45 -0800

    stop execution

---
+++ examples/getting_started/stop_execution.py
@@ -0,0 +1,29 @@
+import marimo
+
+__generated_with = "0.10.6"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return (mo,)
+
+
+@app.cell
+def _(mo):
+    button = mo.ui.run_button()
+    button
+    return (button,)
+
+
+@app.cell
+def _(button, mo):
+    mo.stop(not button.value, "Click the button to continue")
+
+    mo.md("# :tada:")
+    return
+
+
+if __name__ == "__main__":
+    app.run()
