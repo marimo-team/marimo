6fcde0aa87a8c1655bb9e1e967e1153f2d0ba58c
Myles Scolnick <myles@marimo.io>
2025-01-08 17:41:04 -0500

    improvement: upgrade radix (#3377)
    Upgrade radix. This failed last time (#1738) because we one of the
    components was capturing the Escape key (because the popover was always
    open)

--- frontend/src/components/ui/fullscreen.tsx
+++ frontend/src/components/ui/fullscreen.tsx
@@ -1,6 +1,5 @@
 /* Copyright 2024 Marimo. All rights reserved. */
 import { useEventListener } from "@/hooks/useEventListener";
-import type { PortalProps } from "@radix-ui/react-popover";
 import { useState } from "react";
 
 /**
@@ -20,9 +19,11 @@ export function useFullScreenElement() {
  * HOC wrapping a Portal component to use the
  * full screen element as the container if we are in full screen mode
  */
-export function withFullScreenAsRoot<T extends PortalProps>(
-  Component: React.ComponentType<PortalProps>,
-) {
+export function withFullScreenAsRoot<
+  T extends {
+    container?: Element | DocumentFragment | null;
+  },
+>(Component: React.ComponentType<T>) {
   const Comp = (props: T) => {
     const fullScreenElement = useFullScreenElement();
     if (!fullScreenElement) {
