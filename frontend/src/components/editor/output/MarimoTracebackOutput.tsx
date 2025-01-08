556d503876a4cf1d8b4c489f4311efcd58930f9d
Myles Scolnick <myles@marimo.io>
2025-01-07 20:41:06 -0500

    docs: update community link url (#3364)
    update links to https://community.marimo.io/

--- frontend/src/components/editor/output/MarimoTracebackOutput.tsx
+++ frontend/src/components/editor/output/MarimoTracebackOutput.tsx
@@ -128,7 +128,7 @@ export const MarimoTracebackOutput = ({
             <DropdownMenuItem asChild={true}>
               <a
                 target="_blank"
-                href={`https://marimo.hall.community/search?q=${encodeURIComponent(lastTracebackLine)}`}
+                href={`https://community.marimo.io/search?q=${encodeURIComponent(lastTracebackLine)}`}
                 rel="noreferrer"
               >
                 <HelpCircleIcon className="h-4 w-4 mr-2" />
