992aca38df769a5b406245a6e95b67c4d89b9947
Akshay Agrawal <akshay@marimo.io>
2025-01-08 20:45:55 -0800

    fix: datetime formatting for Pandas tables
    Always include H:M:S.f. When the time is excluded our frontend doesn't
    know how to interpret the timestamp.

--- marimo/_plugins/ui/_impl/tables/pandas_table.py
+++ marimo/_plugins/ui/_impl/tables/pandas_table.py
@@ -45,9 +45,14 @@ class PandasTableManagerFactory(TableManagerFactory):
                 self, format_mapping: Optional[FormatMapping] = None
             ) -> bytes:
                 has_headers = len(self.get_row_headers()) > 0
+                # Pandas omits H:M:S for datetimes when H:M:S is identically
+                # 0; this doesn't play well with our frontend table component,
+                # so we use an explicit date format.
                 return (
                     self.apply_formatting(format_mapping)
-                    ._original_data.to_csv(index=has_headers)
+                    ._original_data.to_csv(
+                        index=has_headers, date_format="%Y-%m-%d %H:%M:%S"
+                    )
                     .encode("utf-8")
                 )
 
