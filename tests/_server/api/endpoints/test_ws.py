451ee55879bff5d26daa0e8e53720569acd6cdb0
Myles Scolnick <myles@marimo.io>
2025-01-14 18:05:32 -0500

    fix test

--- tests/_server/api/endpoints/test_ws.py
+++ tests/_server/api/endpoints/test_ws.py
@@ -208,6 +208,7 @@ def test_fails_on_multiple_connections_with_same_file(
 
 async def test_file_watcher_calls_reload(client: TestClient) -> None:
     session_manager: SessionManager = get_session_manager(client)
+    session_manager.watch = True
     with client.websocket_connect("/ws?session_id=123") as websocket:
         data = websocket.receive_json()
         assert_kernel_ready_response(data)
@@ -217,13 +218,13 @@ async def test_file_watcher_calls_reload(client: TestClient) -> None:
         with open(filename, "a") as f:  # noqa: ASYNC101 ASYNC230
             f.write("\n# test")
             f.close()
-        assert session_manager.watcher_manager.watchers
-        await session_manager.watcher_manager.watchers[0].callback(
-            Path(filename)
-        )
+        assert session_manager.watcher_manager._watchers
+        watcher = list(session_manager.watcher_manager._watchers.values())[0]
+        await watcher.callback(Path(filename))
         data = websocket.receive_json()
         assert data == {"op": "reload", "data": {}}
         session_manager.mode = SessionMode.EDIT
+        session_manager.watch = False
     client.post("/api/kernel/shutdown", headers=HEADERS)
 
 
