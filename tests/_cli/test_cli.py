af75d8191ac74e196f18be84b8347c0eea464afe
Hal Wine <hal@dtor.com>
2025-01-17 08:02:47 -0800

    Only run win32 test on win32
    Not surprisingly, only python-on-windows knows how to deal with windows
    style paths. So, tell pytest to try elsewhere.
    
    Used "recommended" formatting style for parametrize arguments per
    https://doc.pytest.org/en/latest/how-to/skipping.html#skip-xfail-with-parametrize
    
    And, there are 2 "windows like" platforms per
    https://docs.python.org/3/library/sys.html#sys.platform

--- tests/_cli/test_cli.py
+++ tests/_cli/test_cli.py
@@ -800,21 +800,27 @@ def test_cli_run_sandbox_prompt_yes() -> None:
 
 # shell-completion has 1 input (value of $SHELL) & 3 outputs (return code, stdout, & stderr)
 # parameterize to give coverage. We use a boolean to specify if output on that stream should be present.
-shell_completion_test_results = [
-    # valid shell values, rc of 0, data only on stdout
-    ("bash", 0, True, False),
-    ("bash.exe", 0, True, False),
-    ("/usr/bin/zsh", 0, True, False),
-    (r"c:\spam\eggs\fish.exe", 0, True, False),
-    # invalid shell values, rc of 0, data only on stderr
-    # (N.B. rc will become 2 when Issue #3476 is fixed)
-    ("bogus", 0, False, True),
-]
-
-
 @pytest.mark.parametrize(
     "shell,rc,expect_stdout,expect_stderr".split(","),
-    shell_completion_test_results,
+    [
+        # valid shell values, rc of 0, data only on stdout
+        ("bash", 0, True, False),
+        ("bash.exe", 0, True, False),
+        ("/usr/bin/zsh", 0, True, False),
+        pytest.param(
+            r"c:\spam\eggs\fish.exe",
+            0,
+            True,
+            False,
+            marks=pytest.mark.skipif(
+                not sys.platform.startswith(("win32", "cygwin")),
+                reason="win32 only",
+            ),
+        ),
+        # invalid shell values, rc of 0, data only on stderr
+        # (N.B. rc will become 2 when Issue #3476 is fixed)
+        ("bogus", 0, False, True),
+    ],
 )
 def test_shell_completion(
     shell: str, rc: int, expect_stdout: bool, expect_stderr: bool
