3586ceb7fb3d7b7d0585d7c640723a472e3e2eae
Myles Scolnick <myles@marimo.io>
2025-01-20 16:02:51 -0800

    fix: update known ai models

--- frontend/src/components/app-config/constants.ts
+++ frontend/src/components/app-config/constants.ts
@@ -1,23 +1,26 @@
 /* Copyright 2024 Marimo. All rights reserved. */
 export const KNOWN_AI_MODELS = [
-  // OpenAI
-  "gpt-4o",
-  "gpt-4-turbo",
-  "gpt-4",
-  "gpt-3.5-turbo",
-
   // Anthropic
-  "claude-3-opus-20240229",
-  "claude-3-sonnet-20240229",
-  "claude-3-haiku-20240307",
-  "claude-3-5-sonnet-20240620",
+  "claude-3-5-sonnet-20241022",
+  "claude-3-opus",
+  "claude-3.5-haiku",
+  "claude-3.5-sonnet",
+
+  // DeepSeek
+  "deepseek-v3",
 
   // Google
-  "gemini-pro",
-  "gemini-1.5-pro-001",
-  "gemini-1.5-flash-001",
-  "gemini-1.5-pro-001",
-  "gemini-1.5-flash-002",
-  "gemini-1.5-pro-002",
-  "gemini-1.5-flash-002",
+  "gemini-2.0-flash-exp",
+  "gemini-2.0-flash-thinking-exp",
+  "gemini-exp-1206",
+
+  // OpenAI
+  "gpt-3.5-turbo",
+  "gpt-4",
+  "gpt-4-turbo-2024-04-09",
+  "gpt-4o",
+  "gpt-4o-mini",
+  "o1",
+  "o1-mini",
+  "o1-preview",
 ] as const;
