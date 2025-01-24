1a3744706afd07032b38d98891c1e3c9f369b8e5
Myles Scolnick <myles@marimo.io>
2025-01-24 10:18:39 -0800

    fix: altair timezone rendering

---
+++ marimo/_smoke_tests/issues/2871_timezones.py
@@ -0,0 +1,45 @@
+import marimo
+
+__generated_with = "0.10.16"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import polars as pl
+    from datetime import datetime, date
+
+    d = datetime(2010, 10, 7, 13, 15)
+
+    # It should appear as 2010-10-07T13:15:00.000
+    pl.DataFrame({"timestamp": [d], "date": [d.date()]})
+    return d, date, datetime, pl
+
+
+@app.cell
+def _(d):
+    # It should appear as 2010-10-07T13:15:00.000
+    [d, d.date()]
+    return
+
+
+@app.cell
+def _(date, pl):
+    import altair as alt
+
+    data = pl.DataFrame(
+        {
+            "Date": [
+                date(2021, 1, 1),
+                date(2021, 1, 2),
+                date(2021, 1, 3),
+            ],
+            "Value": [23, 45, 67],
+        }
+    )
+    alt.Chart(data).mark_line().encode(x="Date:T", y="Value:Q").interactive()
+    return alt, data
+
+
+if __name__ == "__main__":
+    app.run()
