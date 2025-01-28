6f914a6c76af60c23788f6f531ebd06621331924
Srihari Thyagarajan <57552973+Haleshot@users.noreply.github.com>
2025-01-28 21:47:08 +0530

    Add DeepSeek chatbot example (#3598)
    ## 📝 Summary
    
    Add an example for chatting with deepseek-reasoner using `mo.ui.chat`
    
    ## 🔍 Description of Changes
    
    Used `mo.ui.chat`; more specifically `mo.ai.llm.openai` and override the
    `base_url` since it's OpenAI API compatible. Specified the model along
    with custom prompts to use.
    
    TODO:
    - [ ] Verify working with an API locally (currently, the [services are
    down](https://chat.deepseek.com/503/) due to high usage). Cannot obtain
    an API key.
    
    ## 📋 Checklist
    
    - [x] I have read the [contributor
    guidelines](https://github.com/marimo-team/marimo/blob/main/CONTRIBUTING.md).
    - [ ] For large changes, or changes that affect the public API: this
    change was discussed or approved through an issue, on
    [Discord](https://marimo.io/discord?ref=pr), or the community
    [discussions](https://github.com/marimo-team/marimo/discussions) (Please
    provide a link if applicable).
    - [ ] I have added tests for the changes made.
    - [ ] I have run the code and verified that it works as expected.
    
    ## 📜 Reviewers
    
    <!--
    Tag potential reviewers from the community or maintainers who might be
    interested in reviewing this pull request.
    
    Your PR will be reviewed more quickly if you can figure out the right
    person to tag with @ -->
    
    @akshayka OR @mscolnick

--- examples/ai/chat/deepseek_example.py
+++ examples/ai/chat/deepseek_example.py
@@ -2,79 +2,79 @@
 # requires-python = ">=3.11"
 # dependencies = [
 #     "marimo",
-#     "groq==0.11.0",
+#     "openai==1.60.2",
 # ]
 # ///
 
 import marimo
 
-__generated_with = "0.9.14"
+__generated_with = "0.10.17"
 app = marimo.App(width="medium")
 
 
 @app.cell
-def __():
+def _():
     import marimo as mo
     return (mo,)
 
 
 @app.cell(hide_code=True)
-def __(mo):
+def _(mo):
     mo.md(
         r"""
-        # Using Groq
+        # Using DeepSeek
 
-        This example shows how to use [`mo.ui.chat`](https://docs.marimo.io/api/inputs/chat.html#marimo.ui.chat) to make a chatbot backed by [Groq](https://groq.com/).
+        This example shows how to use [`mo.ui.chat`](https://docs.marimo.io/api/inputs/chat/?h=mo.ui.chat) to make a chatbot backed by [Deepseek](https://deepseek.com/).
         """
     )
     return
 
 
 @app.cell(hide_code=True)
-def __(mo):
-    # Hyperlinking the groq as it is mentioned here - https://console.groq.com/docs/badge
+def _(mo):
     mo.md(
         r"""
-        <a href="https://groq.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://api-docs.deepseek.com/" target="_blank" rel="noopener noreferrer">
           <img
-            src="https://groq.com/wp-content/uploads/2024/03/PBG-mark1-color.svg"
-            alt="Powered by Groq for fast inference."
-            width="80" height="800"
+            src="https://chat.deepseek.com/deepseek-chat.jpeg"
+            alt="Powered by deepseek"
+            width="450"
           />
         </a>
         """
-    ).right()
+    ).center()
     return
 
 
 @app.cell
-def __(mo):
+def _(mo):
     import os
 
-    os_key = os.environ.get("GROQ_AI_API_KEY")
-    input_key = mo.ui.text(label="Groq AI API key", kind="password")
+    os_key = os.environ.get("DEEPSEEK_API_KEY")
+    input_key = mo.ui.text(label="Deepseek API key", kind="password")
     input_key if not os_key else None
     return input_key, os, os_key
 
 
 @app.cell
-def __(input_key, mo, os_key):
+def _(input_key, mo, os_key):
     key = os_key or input_key.value
 
     mo.stop(
         not key,
-        mo.md("Please provide your Groq AI API key in the input field."),
+        mo.md("Please provide your Deepseek AI API key in the input field."),
     )
     return (key,)
 
 
 @app.cell
-def __(key, mo):
+def _(key, mo):
     chatbot = mo.ui.chat(
-       mo.ai.llm.groq(
-           model="llama-3.1-70b-versatile",
+       mo.ai.llm.openai(
+           model="deepseek-reasoner",
            system_message="You are a helpful assistant.",
            api_key=key,
+           base_url="https://api.deepseek.com",
        ),
         prompts=[
             "Hello",
@@ -87,13 +87,13 @@ def __(key, mo):
 
 
 @app.cell(hide_code=True)
-def __(mo):
+def _(mo):
     mo.md("""Access the chatbot's historical messages with [`chatbot.value`](https://docs.marimo.io/api/inputs/chat.html#accessing-chat-history).""")
     return
 
 
 @app.cell
-def __(chatbot):
+def _(chatbot):
     # chatbot.value is the list of chat messages
     chatbot.value
     return
