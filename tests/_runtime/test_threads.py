30fb7aa85001cd31f7be75e6124750c70ecdde4c
Akshay Agrawal <akshay@marimo.io>
2025-01-29 16:46:43 -0800

    test output.append

--- tests/_runtime/test_threads.py
+++ tests/_runtime/test_threads.py
@@ -58,3 +58,42 @@ async def test_thread_has_own_stream(
     assert k.globals["thread_ctx"] != k.globals["ctx_main"]
     stream = k.globals["thread_ctx"].stream
     assert stream.cell_id == k.globals["cell_id"]
+
+
+async def test_thread_output_append(
+    k: Kernel, exec_req: ExecReqProvider
+) -> None:
+    """Test that a thread starts and runs."""
+
+    await k.run(
+        [
+            exec_req.get("import marimo as mo"),
+            exec_req.get(
+                """
+                stream = mo._runtime.context.get_context().stream
+                thread_stream = None
+                def target():
+                    global thread_stream
+                    import marimo as mo
+                    mo.output.append("hello")
+                    mo.output.append("world")
+                    thread_stream = mo._runtime.context.get_context().stream
+                t = mo.Thread(target=target); t.start(); t.join()
+                """
+            ),
+        ]
+    )
+
+    # thread run should be basically instantaneous, but sleep just in case ...
+    assert not k.errors
+    time.sleep(0.01)  # noqa: ASYNC251
+    # The main thread should not have any output, but the new thread should
+    stream = k.globals["stream"]
+    for m in stream.messages:
+        if m[0] == "cell-op" and m[1]["output"] is not None:
+            assert "hello" not in m[1]["output"]["data"]
+            assert "world" not in m[1]["output"]["data"]
+    thread_stream = k.globals["thread_stream"]
+    assert len(thread_stream.messages) == 2
+    assert "hello" in thread_stream.messages[0][1]["output"]["data"]
+    assert "world" in thread_stream.messages[1][1]["output"]["data"]
