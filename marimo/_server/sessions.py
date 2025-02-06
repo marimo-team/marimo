07c2e8973627734473b2cca2e53d0c83e3e5f08a
Myles Scolnick <myles@marimo.io>
2025-02-06 09:21:59 -0500

    fix

--- marimo/_server/sessions.py
+++ marimo/_server/sessions.py
@@ -204,6 +204,7 @@ class KernelManager:
                     self.queue_manager.control_queue,
                     self.queue_manager.set_ui_element_queue,
                     self.queue_manager.completion_queue,
+                    self.queue_manager.mcp_evaluation_queue,
                     self.queue_manager.input_queue,
                     # stream queue unused
                     None,
@@ -249,6 +250,7 @@ class KernelManager:
                     self.queue_manager.control_queue,
                     self.queue_manager.set_ui_element_queue,
                     self.queue_manager.completion_queue,
+                    self.queue_manager.mcp_evaluation_queue,
                     self.queue_manager.input_queue,
                     self.queue_manager.stream_queue,
                     # IPC not used in run mode
