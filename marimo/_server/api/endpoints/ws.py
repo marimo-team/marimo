6ea5301042d26e497fa415637a800a67fae89518
Myles Scolnick <myles@marimo.io>
2025-02-18 16:58:52 -0500

    fixes

--- marimo/_server/api/endpoints/ws.py
+++ marimo/_server/api/endpoints/ws.py
@@ -291,7 +291,7 @@ class WebsocketHandler(SessionConsumer):
         self.message_queue: asyncio.Queue[KernelMessage]
         self.ws_future: asyncio.Future[tuple[None, None]] | None = None
 
-        super().__init__(consumer_id=ConsumerId(str(session_id)))
+        super().__init__(consumer_id=ConsumerId(session_id))
 
     def _write_kernel_ready(
         self,
@@ -578,7 +578,7 @@ class WebsocketHandler(SessionConsumer):
 
             # 4. Handle resume
             resumable_session = mgr.maybe_resume_session(
-                SessionId(session_id), self.file_key
+                session_id, self.file_key
             )
             if resumable_session is not None:
                 LOGGER.debug("Resuming session %s", session_id)
