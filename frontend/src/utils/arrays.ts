7996326002c1cd775648bfd55e5530b692048b5d
Myles Scolnick <myles@marimo.io>
2025-02-25 18:36:21 -0500

    change array

--- frontend/src/utils/arrays.ts
+++ frontend/src/utils/arrays.ts
@@ -54,7 +54,7 @@ export function arrayShallowEquals<T>(a: T[], b: T[]): boolean {
 
 export const Arrays = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  EMPTY: Object.freeze([] as any[]),
+  EMPTY: [] as any,
   zip: <T, U>(a: T[], b: U[]): Array<[T, U]> => {
     invariant(a.length === b.length, "Arrays must be the same length");
     return a.map((item, i) => [item, b[i]]);
