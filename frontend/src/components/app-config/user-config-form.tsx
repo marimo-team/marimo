e6b02f229381d761f9fcc8fc48bea185f07d8a0b
Akshay Agrawal <akshay@marimo.io>
2025-02-28 10:03:00 -0800

    Disable RTC experimental flag
    Disables the RTC experimental flag, which often leads to
    code duplication.
    
    * Users can no longer opt-in to RTC on the frontend.
    * Backend disables rtc, regardless of user's stored config.

--- frontend/src/components/app-config/user-config-form.tsx
+++ frontend/src/components/app-config/user-config-form.tsx
@@ -1092,34 +1092,6 @@ export const UserConfigForm: React.FC = () => {
                 </div>
               )}
             />
-            {!isWasm() && (
-              <FormField
-                control={form.control}
-                name="experimental.rtc"
-                render={({ field }) => (
-                  <div className="flex flex-col gap-y-1">
-                    <FormItem className={formItemClasses}>
-                      <FormLabel className="font-normal">
-                        Real-time Collaboration
-                      </FormLabel>
-                      <FormControl>
-                        <Checkbox
-                          data-testid="rtc-checkbox"
-                          checked={field.value === true}
-                          onCheckedChange={field.onChange}
-                        />
-                      </FormControl>
-                    </FormItem>
-                    <FormDescription>
-                      Enable experimental real-time collaboration to allow
-                      editing cell inputs by multiple users. This experimental
-                      feature has known issues, including duplication of code.
-                      Requires refreshing the page to take effect.
-                    </FormDescription>{" "}
-                  </div>
-                )}
-              />
-            )}
           </SettingGroup>
         );
     }
