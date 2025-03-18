2501083a255663fe01b965a2bd85c3f150b0f2e1
Myles Scolnick <myles@marimo.io>
2025-03-18 08:16:52 -0400

    fix: WASM app with print() finish loading

--- frontend/src/core/wasm/state.ts
+++ frontend/src/core/wasm/state.ts
@@ -7,7 +7,15 @@ export const wasmInitializationAtom = atom<string>("Initializing...");
 
 export const hasAnyOutputAtom = atom<boolean>((get) => {
   const notebook = get(notebookAtom);
-  return Object.values(notebook.cellRuntime).some(
+  const runtimeStates = Object.values(notebook.cellRuntime);
+  // First check if there is any output
+  const hasOutput = runtimeStates.some(
     (runtime) => !isOutputEmpty(runtime.output),
   );
+  if (hasOutput) {
+    return true;
+  }
+  // If there is no output, check if they have all run to completion
+  // in case this notebook doesn't contain outputs
+  return runtimeStates.every((runtime) => runtime.status === "idle");
 });
