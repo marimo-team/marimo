29036ff9779eae21c267b80d6732944287feffe2
Myles Scolnick <myles@marimo.io>
2025-03-25 18:10:43 -0400

    lsp ff

--- frontend/src/components/app-config/user-config-form.tsx
+++ frontend/src/components/app-config/user-config-form.tsx
@@ -1229,6 +1229,39 @@ export const UserConfigForm: React.FC = () => {
             </p>
             <FormField
               control={form.control}
+              name="experimental.lsp"
+              render={({ field }) => (
+                <div className="flex flex-col gap-y-1">
+                  <FormItem className={formItemClasses}>
+                    <FormLabel className="font-normal">
+                      LSP (Language Server Protocol)
+                    </FormLabel>
+                    <FormControl>
+                      <Checkbox
+                        data-testid="inline-ai-checkbox"
+                        checked={field.value === true}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                  </FormItem>
+                  <FormDescription>
+                    Enable experimental LSP support. You will need to have
+                    `marimo[lsp]` installed in your environment to use this. See{" "}
+                    <a
+                      className="text-link hover:underline"
+                      href="https://docs.marimo.io/guides/editor_features/language_server/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      docs
+                    </a>{" "}
+                    for more info.
+                  </FormDescription>
+                </div>
+              )}
+            />
+            <FormField
+              control={form.control}
               name="experimental.inline_ai_tooltip"
               render={({ field }) => (
                 <div className="flex flex-col gap-y-1">
