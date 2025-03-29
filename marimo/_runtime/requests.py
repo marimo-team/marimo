055ffd4e97dfaa4332f1aea81f54368034c22be3
Akshay Agrawal <akshay@marimo.io>
2025-03-28 23:01:31 -0700

    Union not | for dataclass parsing

--- marimo/_runtime/requests.py
+++ marimo/_runtime/requests.py
@@ -233,7 +233,7 @@ class AppMetadata:
 
     query_params: SerializedQueryParams
     cli_args: SerializedCLIArgs
-    argv: list[str] | None = None
+    argv: Union[list[str], None] = None
 
     filename: Optional[str] = None
 
