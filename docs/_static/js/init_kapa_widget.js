d1fed53b787d493ddecf3c918f44c794cd203fc6
Myles Scolnick <myles@marimo.io>
2025-04-03 18:19:40 -0400

    minor updates

--- docs/_static/js/init_kapa_widget.js
+++ docs/_static/js/init_kapa_widget.js
@@ -5,9 +5,13 @@ document.addEventListener("DOMContentLoaded", () => {
     "data-website-id",
     "d5df1a3e-916b-44ad-a725-e80e490570b2",
   );
+  script.setAttribute("data-modal-title", "Ask marimo");
   script.setAttribute("data-project-name", "marimo");
-  script.setAttribute("data-project-color", "#1c7362");
-  script.setAttribute("data-project-logo", "https://marimo.io/logo.png");
+  script.setAttribute("data-project-color", "rgba(28, 115, 98, 0.6)");
+  script.setAttribute(
+    "data-project-logo",
+    "https://marimo.io/favicon-32x32.png",
+  );
   script.setAttribute(
     "data-modal-disclaimer",
     "This is a custom LLM for marimo with access to all [documentation](https://docs.marimo.io) and the [API reference.](https://docs.marimo.io/api/).",
@@ -16,6 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "data-modal-example-questions",
     "How is marimo different from Jupyter?,How can I run my marimo notebook as a script?",
   );
+  script.setAttribute("data-user-analytics-fingerprint-enabled", "true");
   script.async = true;
   document.head.appendChild(script);
 });
