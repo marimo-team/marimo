af73c75939c92d858ab04a397c557354101a3e8b
Myles Scolnick <myles@marimo.io>
2025-04-03 18:03:03 -0400

    docs: kapa widget

---
+++ docs/_static/js/init_kapa_widget.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const script = document.createElement("script");
+  script.src = "https://widget.kapa.ai/kapa-widget.bundle.js";
+  script.setAttribute(
+    "data-website-id",
+    "d5df1a3e-916b-44ad-a725-e80e490570b2",
+  );
+  script.setAttribute("data-project-name", "marimo");
+  script.setAttribute("data-project-color", "#1c7362");
+  script.setAttribute("data-project-logo", "https://marimo.io/logo.png");
+  script.async = true;
+  document.head.appendChild(script);
+});
