b0326331a169f61103e80032a4a1d81f37da8dee
Myles Scolnick <myles@marimo.io>
2025-04-07 16:44:29 -0700

    comment

--- frontend/src/core/codemirror/completion/Autocompleter.ts
+++ frontend/src/core/codemirror/completion/Autocompleter.ts
@@ -42,6 +42,7 @@ export const AUTOCOMPLETER = new DeferredRequestRegistry<
   // because they may be used for tooltips or live documentation.
 );
 
+// Boost params and properties so they appear first
 const BOOSTS: Record<CompletionOption["type"], number> = {
   param: 3,
   property: 2,
@@ -62,8 +63,6 @@ export const Autocompleter = {
         return {
           label: option.name,
           type: option.type,
-          // Boost params so they appear first
-          // This appears inside parentheses
           boost: BOOSTS[option.type] ?? 1,
           info: () => constructCompletionInfoNode(option.completion_info),
         };
