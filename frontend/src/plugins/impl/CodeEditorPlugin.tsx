c58a9fb0f2ad39646b5a6b2b0c503d6279a7c0c1
nojaf <florian.verdonck@outlook.com>
2025-04-10 11:38:02 +0200

    Add debounce option to marimo-code-editor

--- frontend/src/plugins/impl/CodeEditorPlugin.tsx
+++ frontend/src/plugins/impl/CodeEditorPlugin.tsx
@@ -1,11 +1,14 @@
 /* Copyright 2024 Marimo. All rights reserved. */
 import { z } from "zod";
 import type { IPlugin, IPluginProps, Setter } from "../types";
+import * as events from "@uiw/codemirror-extensions-events";
 
 import { Labeled } from "./common/labeled";
 import { type Theme, useTheme } from "@/theme/useTheme";
 import { LazyAnyLanguageCodeMirror } from "./code/LazyAnyLanguageCodeMirror";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { useCallback, useState } from "react";
+import { useDebounceControlledState } from "@/hooks/useDebounce";
 
 type T = string;
 
@@ -18,6 +21,7 @@ interface Data {
   minHeight?: number;
   maxHeight?: number;
   showCopyButton?: boolean;
+  debounce?: boolean | number;
 }
 
 export class CodeEditorPlugin implements IPlugin<T, Data> {
@@ -33,6 +37,7 @@ export class CodeEditorPlugin implements IPlugin<T, Data> {
     minHeight: z.number().optional(),
     maxHeight: z.number().optional(),
     showCopyButton: z.boolean().optional(),
+    debounce: z.optional(z.union([z.boolean(), z.number()])),
   });
 
   render(props: IPluginProps<T, Data>): JSX.Element {
@@ -57,6 +62,36 @@ const CodeEditorComponent = (props: CodeEditorComponentProps) => {
   const minHeight = props.minHeight ? `${props.minHeight}px` : "70px";
   const maxHeight = props.maxHeight ? `${props.maxHeight}px` : undefined;
 
+  const [localValue, setLocalValue] = useState(props.value);
+  const { onChange: onChangeDebounce } = useDebounceControlledState<string>({
+    initialValue: props.value,
+    delay: Number.isFinite(props.debounce) ? (props.debounce as number) : 0,
+    onChange: props.setValue,
+  });
+
+  const handleChange = useCallback(
+    (newValue: string) => {
+      setLocalValue((_) => newValue);
+      if (typeof props.debounce === "number") {
+        onChangeDebounce(newValue);
+      } else if (props.debounce !== true) {
+        props.setValue(newValue);
+      }
+    },
+    [onChangeDebounce, props.setValue],
+  );
+
+  const extensions =
+    props.debounce === true
+      ? [
+          events.content({
+            blur: () => {
+              props.setValue(localValue);
+            },
+          }),
+        ]
+      : [];
+
   return (
     <TooltipProvider>
       <Labeled label={props.label} align="top" fullWidth={true}>
@@ -67,10 +102,11 @@ const CodeEditorComponent = (props: CodeEditorComponentProps) => {
           maxHeight={maxHeight}
           placeholder={props.placeholder}
           editable={!props.disabled}
-          value={props.value}
+          value={localValue}
           language={props.language}
-          onChange={props.setValue}
+          onChange={handleChange}
           showCopyButton={props.showCopyButton}
+          extensions={extensions}
         />
       </Labeled>
     </TooltipProvider>
