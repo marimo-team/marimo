725a30bf09b6082def31e3ee7b5438a402ea8d91
Shahmir Varqha <sham9871@gmail.com>
2025-04-11 17:24:57 +0800

    add back comment

--- marimo/_sql/sql.py
+++ marimo/_sql/sql.py
@@ -118,6 +118,8 @@ def sql(
             # Also preload the first page of data without user confirmation.
             t = table.table.lazy(df, preload=True)
         else:
+            # df may be a cursor result from an SQL Engine
+            # In this case, we need to convert it to a DataFrame
             display_df = df
             if SQLAlchemyEngine.is_cursor_result(df):
                 result = SQLAlchemyEngine.get_cursor_metadata(df)
