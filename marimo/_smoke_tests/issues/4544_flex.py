c6b092b55fe68de447d8a53c360e56e59ed6d71c
Myles Scolnick <myles@marimo.io>
2025-04-15 15:13:52 -0400

    smoke test

--- marimo/_smoke_tests/issues/4544_flex.py
+++ marimo/_smoke_tests/issues/4544_flex.py
@@ -5,11 +5,16 @@ import marimo
 __generated_with = "0.12.9"
 app = marimo.App(width="medium")
 
-
 @app.cell
 def _():
     import marimo as mo
     return (mo,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""## Buttons""")
+    return
 
 
 @app.cell
@@ -58,6 +63,58 @@ def _(different_height_items, mo):
 @app.cell
 def _(different_height_items, mo):
     mo.hstack(different_height_items)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""## Sliders""")
+    return
+
+
+@app.cell(hide_code=True)
+def _(full_width):
+    full_width
+    return
+
+
+@app.cell
+def _(full_width, mo):
+    range_slider = mo.ui.range_slider(
+        start=1, stop=10, step=2, value=[2, 6], full_width=full_width.value
+    )
+    range_slider
+    return (range_slider,)
+
+
+@app.cell
+def _(mo, range_slider):
+    mo.hstack([range_slider])
+    return
+
+
+@app.cell
+def _(mo, range_slider):
+    mo.vstack([range_slider])
+    return
+
+
+@app.cell
+def _(full_width, mo):
+    slider = mo.ui.slider(start=1, stop=10, step=2, full_width=full_width.value)
+    slider
+    return (slider,)
+
+
+@app.cell
+def _(mo, slider):
+    mo.hstack([slider])
+    return
+
+
+@app.cell
+def _(mo, slider):
+    mo.vstack([slider])
     return
 
 
