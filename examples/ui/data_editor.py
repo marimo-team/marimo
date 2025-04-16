d42cd50eb807b85453a61aee622857a533deddff
Akshay Agrawal <akshay@marimo.io>
2025-04-15 21:37:05 -0700

    docs: fix data editor example (#4552)
    Read dataframe from disk in same cell that creates the data_editor, to
    avoid spurious overwriting.

--- examples/ui/data_editor.py
+++ examples/ui/data_editor.py
@@ -1,3 +1,5 @@
+
+
 import marimo
 
 __generated_with = "0.12.9"
@@ -23,36 +25,29 @@ def _():
 
 
 @app.cell
-def _(DATA_FILE, os):
+def _(DATA_FILE, mo, os):
+    import polars as pl
+
     if not os.path.exists(DATA_FILE):
         from vega_datasets import data
 
         data.cars().to_csv(DATA_FILE)
 
-
-    import polars as pl
-
-    df = pl.read_csv(DATA_FILE)
-    return data, df, pl
-
-
-@app.cell
-def _(df, mo):
-    editor = mo.ui.data_editor(df).form(bordered=False)
+    editor = mo.ui.data_editor(pl.read_csv(DATA_FILE)).form(bordered=False)
     editor
     return (editor,)
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        "The following cell writes the updated dataframe to disk when the submit button is clicked."
-    )
+    mo.md("""The following cell writes the updated dataframe to disk when the submit button is clicked.""")
     return
 
 
 @app.cell
-def _(DATA_FILE, editor):
+def _(DATA_FILE, editor, mo):
+    mo.stop(editor.value is None, mo.md("Submit your changes."))
+
     editor.value.write_csv(DATA_FILE)
     return
 
