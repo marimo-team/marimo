5c663ca2bfff0f7f28d8a94de3cbc7cbcdb5d80d
Breno Araujo <breno.c.araujo@gmail.com>
2025-04-22 08:09:35 +0200

    Remove spaces from empty lines

--- marimo/_output/formatters/tqdm_formatters.py
+++ marimo/_output/formatters/tqdm_formatters.py
@@ -29,10 +29,9 @@ class ProgressBarTqdmPatch(progress_bar):
             title=desc or "Loading...",
             total=total,
         )
-        
+
     def update(self, n: int = 1) -> None:
         """Update the progress bar by incrementing it by n.
-        
         Args:
             n (int, optional): Number of iterations to increment by. Defaults to 1.
         """
