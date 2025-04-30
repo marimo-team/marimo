62dd3a71211bb61a8db23eb96ea5082aa099ecb7
dylan <dylan@marimo.io>
2025-04-29 18:18:42 -0700

    nits

--- tests/_server/export/test_utils.py
+++ tests/_server/export/test_utils.py
@@ -3,7 +3,6 @@ from marimo._server.export.utils import get_markdown_from_cell
 
 
 def test_extract_markdown_base():
-    # Test with a simple markdown string
     empty_markdown_str = "mo.md('hello')"
     markdown = get_markdown_from_cell(
         compile_cell(empty_markdown_str, "id"), empty_markdown_str
@@ -12,7 +11,6 @@ def test_extract_markdown_base():
 
 
 def test_extract_markdown_empty():
-    # Test with a simple markdown string
     empty_markdown_str = "mo.md()"
     markdown = get_markdown_from_cell(
         compile_cell(empty_markdown_str, "id"), empty_markdown_str
@@ -21,9 +19,8 @@ def test_extract_markdown_empty():
 
 
 def test_extract_markdown_broken():
-    # Test with a simple markdown string
     empty_markdown_str = "mo.md()"
-    # This can occut because the cell isn't recompiled at this point.
+    # This can occur because the cell isn't recompiled at this point.
     markdown = get_markdown_from_cell(
         compile_cell(empty_markdown_str, "id"), "mo.md(f'{broken(}')"
     )
