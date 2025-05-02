857f034732be3436932a9a3edd8c9eb131350fc6
Shahmir Varqha <sham9871@gmail.com>
2025-05-02 21:57:57 +0800

    add atoms

--- frontend/src/components/data-table/selection-panel/panel-atoms.ts
+++ frontend/src/components/data-table/selection-panel/panel-atoms.ts
@@ -1,6 +1,6 @@
 /* Copyright 2024 Marimo. All rights reserved. */
 import { atom } from "jotai";
-import type { TableState } from "@tanstack/react-table";
+import type { Row } from "@tanstack/react-table";
 import type { CellId } from "@/core/cells/ids";
 
 export const selectionPanelOpenAtom = atom(false);
@@ -8,4 +8,10 @@ export const selectionPanelOpenAtom = atom(false);
 // Keeps track of the currently focused or selected cell
 export const currentlyFocusedCellAtom = atom<CellId>();
 
-export const tableDataAtom = atom<Record<CellId, TableState>>();
+// TODO: We can only get rows from current table page
+export interface TableData {
+  rows: Array<Row<unknown>>;
+}
+
+// TODO: We should probably delete old or deleted cells from this atom. Or limit to most recent cells
+export const tableDataAtom = atom<Partial<Record<CellId, TableData>>>({});
