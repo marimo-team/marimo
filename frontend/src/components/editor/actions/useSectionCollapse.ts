fa5b74fce0af8054db6cdb7a65ad1a9284eb9eb9
Joaquin Coromina <75667013+bjoaquinc@users.noreply.github.com>
2025-05-04 20:30:06 -0400

    Updated useSectionCollapse to use collapseAllCells() and expandAllCells()

--- frontend/src/components/editor/actions/useSectionCollapse.ts
+++ frontend/src/components/editor/actions/useSectionCollapse.ts
@@ -1,7 +1,5 @@
 /* Copyright 2024 Marimo. All rights reserved. */
 import { useCellActions } from "@/core/cells/cells";
-import { getNotebook } from "@/core/cells/cells";
-import { canCollapseOutline } from "@/core/dom/outline";
 import useEvent from "react-use-event-hook";
 
 /**
@@ -9,30 +7,10 @@ import useEvent from "react-use-event-hook";
  */
 
 export const useSectionCollapse = () => {
-  const { collapseCell, expandCell } = useCellActions();
-
-  const processAllSections = async (action: "collapse" | "expand") => {
-    const notebook = getNotebook();
-    const cellIds = notebook.cellIds.inOrderIds;
-
-    // Find all markdown cells that aren't already hidden
-    for (const cellId of cellIds) {
-      const outline = notebook.cellRuntime[cellId].outline;
-      // Check if the cell is a markdown cell with a TOC outline
-      if (!outline) {
-        continue;
-      }
-      // Check if the cell is a collapsible header
-      if (!canCollapseOutline(outline)) {
-        continue;
-      }
-      // Collapse or expand the cell based on the action
-      action === "collapse" ? collapseCell({ cellId }) : expandCell({ cellId });
-    }
-  };
+  const { collapseAllCells, expandAllCells } = useCellActions();
 
   return {
-    collapseAllSections: useEvent(() => processAllSections("collapse")),
-    expandAllSections: useEvent(() => processAllSections("expand")),
+    collapseAllSections: useEvent(() => collapseAllCells()),
+    expandAllSections: useEvent(() => expandAllCells()),
   };
 };
