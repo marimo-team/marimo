83b317c5ca5eadcf0d1e93da89edb481f7ede018
Myles Scolnick <myles@marimo.io>
2025-05-05 14:45:54 -0400

    swap tomlkit

--- marimo/_utils/toml.py
+++ marimo/_utils/toml.py
@@ -12,42 +12,41 @@ if TYPE_CHECKING:
 def read_toml(file_path: Union[str, Path]) -> dict[str, Any]:
     """Read and parse a TOML file."""
 
-    # Prefer tomllib over tomlkit for reading since it is built-in and works with pyodide
     # tomllib is only available in python 3.11+
-    if DependencyManager.tomllib.has():
-        import tomllib
-
-        with open(file_path, "rb") as file:
-            return tomllib.load(file)
-    else:
+    if DependencyManager.tomlkit.has():
         import tomlkit
 
         with open(file_path, "rb") as file:
             return tomlkit.load(file)
+    else:
+        import tomllib
+
+        with open(file_path, "rb") as file:
+            return tomllib.load(file)
 
 
 def read_toml_string(s: str) -> dict[str, Any]:
     """Read and parse a TOML string."""
 
-    # Prefer tomllib over tomlkit for reading since it is built-in and works with pyodide
     # tomllib is only available in python 3.11+
-    if DependencyManager.tomllib.has():
-        import tomllib
-
-        return tomllib.loads(s)
-    else:
+    if DependencyManager.tomlkit.has():
         import tomlkit
 
         return tomlkit.loads(s)
+    else:
+        import tomllib
+
+        return tomllib.loads(s)
 
 
 def is_toml_error(e: Exception) -> bool:
     """Check if an exception is a TOML error."""
-    if DependencyManager.tomllib.has():
-        import tomllib
 
-        return isinstance(e, tomllib.TOMLDecodeError)
-    else:
+    if DependencyManager.tomlkit.has():
         import tomlkit
 
         return isinstance(e, tomlkit.exceptions.TOMLKitError)
+    else:
+        import tomllib
+
+        return isinstance(e, tomllib.TOMLDecodeError)
