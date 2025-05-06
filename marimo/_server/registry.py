56f2e6e2a11e6beb9685d2ad23216e6a96e6230e
Myles Scolnick <myles@marimo.io>
2025-05-06 12:23:19 -0400

    fix import

--- marimo/_server/registry.py
+++ marimo/_server/registry.py
@@ -1,16 +1,17 @@
 from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from starlette.applications import Starlette  # noqa: TC004
-    from starlette.middleware import Middleware  # noqa: TC004
-    from starlette.types import Lifespan  # noqa: TC004
-
 from marimo._entrypoints.registry import EntryPointRegistry
 
-MIDDLEWARE_REGISTRY = EntryPointRegistry[Middleware](
+if TYPE_CHECKING:
+    from starlette.applications import Starlette
+    from starlette.middleware import Middleware
+    from starlette.types import Lifespan
+
+
+MIDDLEWARE_REGISTRY: EntryPointRegistry["Middleware"] = EntryPointRegistry(
     entry_point_group="marimo.server.asgi.middleware"
 )
 
-LIFESPAN_REGISTRY = EntryPointRegistry[Lifespan[Starlette]](
-    entry_point_group="marimo.server.asgi.lifespan"
+LIFESPAN_REGISTRY: EntryPointRegistry["Lifespan[Starlette]"] = (
+    EntryPointRegistry(entry_point_group="marimo.server.asgi.lifespan")
 )
