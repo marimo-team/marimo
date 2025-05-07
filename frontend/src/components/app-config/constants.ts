6587fc33406e1338d58592278a4ee73007972fbb
Myles Scolnick <myles@marimo.io>
2025-05-07 14:05:16 -0400

    fix: update common llm models (#4838)
    Just some name updates

--- frontend/src/components/app-config/constants.ts
+++ frontend/src/components/app-config/constants.ts
@@ -1,19 +1,20 @@
 /* Copyright 2024 Marimo. All rights reserved. */
 export const KNOWN_AI_MODELS = [
   // Anthropic
-  "claude-3-7-sonnet-20250219",
-  "claude-3-5-sonnet-20241022",
-  "claude-3-opus",
-  "claude-3.5-haiku",
-  "claude-3.5-sonnet",
+  "claude-3-7-sonnet-latest",
+  "claude-3-5-sonnet-latest",
+  "claude-3-5-haiku-latest)",
+  "claude-3-opus-latest",
 
   // DeepSeek
   "deepseek-v3",
+  "deepseek-r1",
 
   // Google
-  "gemini-2.0-flash-exp",
-  "gemini-2.0-flash-thinking-exp",
-  "gemini-exp-1206",
+  "gemini-2.5-pro",
+  "gemini-2.5-flash",
+  "gemini-2.0-flash-lite",
+  "gemini-2.0-flash",
 
   // OpenAI
   "gpt-3.5-turbo",
@@ -21,7 +22,9 @@ export const KNOWN_AI_MODELS = [
   "gpt-4-turbo-2024-04-09",
   "gpt-4o",
   "gpt-4o-mini",
+  "gpt-4.5",
   "o1",
   "o1-mini",
   "o1-preview",
+  "o3-mini",
 ] as const;
