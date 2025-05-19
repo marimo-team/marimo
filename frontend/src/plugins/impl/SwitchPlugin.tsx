50c70775c62967c3d7e2cd489b27978144beaa23
Joaquin Coromina <75667013+bjoaquinc@users.noreply.github.com>
2025-05-19 13:18:50 -0400

    feat: added disabled prop to switch (#4973)
    ## 📝 Summary
    
    <!--
    Provide a concise summary of what this pull request is addressing.
    
    If this PR fixes any issues, list them here by number (e.g., Fixes
    #123).
    -->
    Added disabled to switch
    
    <img width="534" alt="Screenshot 2025-05-18 at 10 08 38 PM"
    src="https://github.com/user-attachments/assets/7f6aa23f-3d5a-494c-88a4-40f294965cd7"
    />
    
    
    ## 🔍 Description of Changes
    
    <!--
    Detail the specific changes made in this pull request. Explain the
    problem addressed and how it was resolved. If applicable, provide before
    and after comparisons, screenshots, or any relevant details to help
    reviewers understand the changes easily.
    -->
    
    - Added disabled property and cleaned code in SwitchPlugin.tsx
    - Mapped disabled in python class with definition
    
    ## 📋 Checklist
    
    - [x] I have read the [contributor
    guidelines](https://github.com/marimo-team/marimo/blob/main/CONTRIBUTING.md).
    - [x] For large changes, or changes that affect the public API: this
    change was discussed or approved through an issue, on
    [Discord](https://marimo.io/discord?ref=pr), or the community
    [discussions](https://github.com/marimo-team/marimo/discussions) (Please
    provide a link if applicable).
    - [x] I have added tests for the changes made.
    - [x] I have run the code and verified that it works as expected.
    
    ## 📜 Reviewers
    
    <!--
    Tag potential reviewers from the community or maintainers who might be
    interested in reviewing this pull request.
    
    Your PR will be reviewed more quickly if you can figure out the right
    person to tag with @ -->
    
    @mscolnick

--- frontend/src/plugins/impl/SwitchPlugin.tsx
+++ frontend/src/plugins/impl/SwitchPlugin.tsx
@@ -6,17 +6,22 @@ import { Switch } from "@/components/ui/switch";
 import type { IPlugin, IPluginProps } from "@/plugins/types";
 import { Labeled } from "./common/labeled";
 
-export class SwitchPlugin
-  implements IPlugin<boolean, { label: string | null }>
-{
+type T = boolean;
+interface Data {
+  label: string | null;
+  disabled?: boolean;
+}
+
+export class SwitchPlugin implements IPlugin<T, Data> {
   tagName = "marimo-switch";
 
   validator = z.object({
     initialValue: z.boolean(),
     label: z.string().nullable(),
+    disabled: z.boolean().optional(),
   });
 
-  render(props: IPluginProps<boolean, { label: string | null }>): JSX.Element {
+  render(props: IPluginProps<T, Data>): JSX.Element {
     return <SwitchComponent {...props} />;
   }
 }
@@ -25,7 +30,7 @@ const SwitchComponent = ({
   value,
   setValue,
   data,
-}: IPluginProps<boolean, { label: string | null }>): JSX.Element => {
+}: IPluginProps<T, Data>): JSX.Element => {
   const id = useId();
 
   return (
@@ -36,6 +41,7 @@ const SwitchComponent = ({
         onCheckedChange={setValue}
         id={id}
         className="data-[state=unchecked]:hover:bg-input/80 mb-0"
+        disabled={data.disabled}
       />
     </Labeled>
   );
