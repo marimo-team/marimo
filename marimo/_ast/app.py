638a3677bbb46eff6f08f1482af040757ac94909
dylan <dylan@marimo.io>
2025-05-20 15:07:41 -0700

    fix: allow some scope block

--- marimo/_ast/app.py
+++ marimo/_ast/app.py
@@ -125,6 +125,13 @@ class _SetupContext:
             )
         self._frame = with_frame
 
+        previous = {**with_frame.f_locals}
+        # A reference to the key app must be maintained in the frame.
+        # This may be a python quirk, so just remove refs to explicit defs
+        for var in self._cell.defs:
+            if var in previous:
+                del self._frame.f_locals[var]
+
     def __exit__(
         self,
         exception: Optional[type[BaseException]],
