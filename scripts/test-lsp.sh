9ab72e441f7993b550cba1895baf033da4135762
dylan <dylan@marimo.io>
2025-05-23 15:20:29 -0700

    fix: missing file

---
+++ scripts/test-lsp.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+node marimo/_lsp/index.cjs --help > /dev/null
+if [ $? -ne 0 ]; then
+    echo "LSP binary failed to start"
+    exit 1
+fi
+echo "LSP binary started and killed successfully"
