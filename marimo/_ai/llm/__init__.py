a28d8c37b3226a86803bab67b044d81e68bc8aa9
Andrea Ghensi <aghensi@systra.com>
2025-05-27 18:09:26 +0200

    fix: missing import

--- marimo/_ai/llm/__init__.py
+++ marimo/_ai/llm/__init__.py
@@ -1,4 +1,11 @@
 # Copyright 2025 Marimo. All rights reserved.
-from marimo._ai.llm._impl import anthropic, bedrock, google, groq, openai
+from marimo._ai.llm._impl import (
+    anthropic,
+    azure,
+    bedrock,
+    google,
+    groq,
+    openai,
+)
 
 __all__ = ["openai", "anthropic", "google", "groq", "bedrock", "azure"]
