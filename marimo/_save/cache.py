f85f365d908b6bfab91af94ac44083c2d1aa2168
dylan <dylan@marimo.io>
2025-05-28 17:06:49 -0700

    fix: Fewer cache misses

--- marimo/_save/cache.py
+++ marimo/_save/cache.py
@@ -68,16 +68,16 @@ class UIElementStub:
     def __init__(self, element: UIElement[S, T]) -> None:
         self.args = element._args
         self.cls = element.__class__
-        # Only hashable attributes are stored.
-        sample = self.cls.__new__(self.cls).__dict__
+        # Ideally only hashable attributes are stored on the subclass level.
+        defaults = set(self.cls.__new__(self.cls).__dict__.keys())
+        defaults |= {"_ctx"}
         self.data = {
             k: v
             for k, v in element.__dict__.items()
-            if hasattr(v, "__hash__") and k not in sample
+            if hasattr(v, "__hash__") and k not in defaults
         }
 
     def load(self) -> UIElement[S, T]:
-        # UIElement cannot be restored directly, so we return a stub.
         return self.cls.from_args(self.data, self.args)  # type: ignore
 
 
