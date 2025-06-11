cba7f89e756adcc31d5c362a9e026be7f0ff8627
Shahmir Varqha <sham9871@gmail.com>
2025-06-12 00:28:56 +0800

    update playwright

--- frontend/e2e-tests/py/layout_grid.py
+++ frontend/e2e-tests/py/layout_grid.py
@@ -1,42 +1,42 @@
 import marimo
 
-__generated_with = "0.9.14"
+__generated_with = "0.13.15"
 app = marimo.App(layout_file="layouts/layout_grid.grid.json")
 
 
 @app.cell
-def __(mo):
+def _(mo):
     mo.md("""# Grid Layout""")
     return
 
 
 @app.cell
-def __(mo, search):
-    mo.md(f"Searching {search.value}")
+def _(mo, search):
+    mo.md(f"""Searching {search.value}""")
     return
 
 
 @app.cell
-def __(mo):
+def _(mo):
     search = mo.ui.text(label="Search")
     search
     return (search,)
 
 
 @app.cell
-def __(mo):
+def _(mo):
     mo.md("""text 1""")
     return
 
 
 @app.cell
-def __(mo):
+def _(mo):
     mo.md("""text 2""")
     return
 
 
 @app.cell
-def __():
+def _():
     import marimo as mo
     return (mo,)
 
