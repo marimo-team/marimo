a99b2fb6b1913b08313fa79e93bfec07a2cee216
Shahmir Varqha <sham9871@gmail.com>
2025-06-20 01:14:16 +0800

    wip

--- frontend/src/components/editor/code/readonly-python-code.tsx
+++ frontend/src/components/editor/code/readonly-python-code.tsx
@@ -5,8 +5,9 @@ import CodeMirror, {
   EditorView,
   type ReactCodeMirrorProps,
 } from "@uiw/react-codemirror";
-import { CopyIcon, EyeIcon, EyeOffIcon } from "lucide-react";
+import { CopyIcon, EyeIcon, EyeOffIcon, PlusIcon } from "lucide-react";
 import { memo, useState } from "react";
+import { useAddCodeToNewCell } from "@/components/editor/cell/useAddCell";
 import { Button } from "@/components/ui/button";
 import { Tooltip } from "@/components/ui/tooltip";
 import { toast } from "@/components/ui/use-toast";
@@ -22,12 +23,25 @@ const pythonExtensions = [
 ];
 const sqlExtensions = [sql(), EditorView.lineWrapping];
 
+/**
+ * A readonly code component that can be used to display code in a readonly state.
+ *
+ * @param props.className - The class name to apply to the component.
+ * @param props.code - The code to display.
+ * @param props.initiallyHideCode - Whether to initially hide the code.
+ * @param props.disableHideCode - Whether to disable the hide code button.
+ * @param props.addNewCell - Whether to add a new cell button, when clicked will add a new cell in the next cell / end of file
+ * @param props.language - The language of the code. Default is "python".
+ * @param props.rest - The rest of the props to pass to the CodeMirror component.
+ */
 export const ReadonlyCode = memo(
   (
     props: {
       className?: string;
       code: string;
       initiallyHideCode?: boolean;
+      disableHideCode?: boolean;
+      addNewCell?: boolean;
       language?: "python" | "sql";
     } & ReactCodeMirrorProps,
   ) => {
@@ -36,10 +50,13 @@ export const ReadonlyCode = memo(
       code,
       className,
       initiallyHideCode,
+      disableHideCode,
+      addNewCell,
       language = "python",
       ...rest
     } = props;
     const [hideCode, setHideCode] = useState(initiallyHideCode);
+    const showHideCodeIndicators = !disableHideCode && !hideCode;
 
     return (
       <div
@@ -48,19 +65,19 @@ export const ReadonlyCode = memo(
           className,
         )}
       >
-        {hideCode && (
+        {showHideCodeIndicators && (
           <HideCodeButton
             tooltip="Show code"
             onClick={() => setHideCode(false)}
           />
         )}
-        {!hideCode && (
-          <div className="absolute top-0 right-0 my-1 mx-2 z-10 hover-action flex gap-2">
-            <CopyButton text={code} />
-
+        <div className="absolute top-0 right-0 my-1 mx-2 z-10 hover-action flex gap-2">
+          <CopyButton text={code} />
+          {addNewCell && <AddNewCellButton code={code} />}
+          {showHideCodeIndicators && (
             <EyeCloseButton onClick={() => setHideCode(true)} />
-          </div>
-        )}
+          )}
+        </div>
         <CodeMirror
           {...rest}
           className={cn("cm", hideCode && "opacity-20 h-8 overflow-hidden")}
@@ -118,5 +135,26 @@ export const HideCodeButton = (props: {
         <EyeIcon className="hover-action w-5 h-5 text-muted-foreground cursor-pointer absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-80 hover:opacity-100 z-20" />
       </Tooltip>
     </div>
+  );
+};
+
+const AddNewCellButton = (props: { code: string }) => {
+  const addCodeToNewCell = useAddCodeToNewCell();
+
+  const handleClick = () => {
+    addCodeToNewCell(props.code);
+  };
+
+  return (
+    <Tooltip content="Add new cell" usePortal={false}>
+      <Button
+        onClick={handleClick}
+        size="xs"
+        className="py-0"
+        variant="secondary"
+      >
+        <PlusIcon size={14} strokeWidth={1.5} />
+      </Button>
+    </Tooltip>
   );
 };
