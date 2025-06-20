e64472aafe1f4d91e99ef33ca09e1b5b489c1300
Akshay Agrawal <akshay@marimo.io>
2025-06-20 12:53:23 -0700

    print for debugging

--- scripts/test-lsp.sh
+++ scripts/test-lsp.sh
@@ -6,11 +6,14 @@ list_directories() {
 }
 
 
+list_directories
+
 if [ -d "marimo/_lsp/copilot/dist" ]; then
     echo "Error: marimo/_lsp/copilot should not have a dist/ directory"
     list_directories
     exit 1
 fi
+echo "Copilot directory structure looks correct."
 
 
 node marimo/_lsp/index.cjs --help > /dev/null
