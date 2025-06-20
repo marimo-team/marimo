b4a84b92faf9f2adc48aa3a3cbabb097fe90321a
Akshay Agrawal <akshay@marimo.io>
2025-06-20 10:40:57 -0700

    style: runtime connecting indicator
    A more subtle, but still quite visible, connecting alert. The previous
    connecting alert clipped over the first cell / output, and distracted
    from looking at the notebook.

--- frontend/src/components/editor/alerts/connecting-alert.tsx
+++ frontend/src/components/editor/alerts/connecting-alert.tsx
@@ -1,9 +1,10 @@
 /* Copyright 2024 Marimo. All rights reserved. */
 
 import { useAtomValue } from "jotai";
-import { Spinner } from "@/components/icons/spinner";
+import { LoadingEllipsis } from "@/components/icons/loading-ellipsis";
 import { isConnectingAtom } from "@/core/network/connection";
-import { FloatingAlert } from "./floating-alert";
+import { DelayMount } from "@/components/utils/delay-mount";
+import { Tooltip } from "../../ui/tooltip";
 
 const DELAY_MS = 1000; // 1 second
 
@@ -11,11 +12,16 @@ export const ConnectingAlert: React.FC = () => {
   const isConnecting = useAtomValue(isConnectingAtom);
 
   return (
-    <FloatingAlert title="Connecting" show={isConnecting} delayMs={DELAY_MS}>
-      <div className="flex items-center gap-2">
-        <Spinner className="h-4 w-4" />
-        <p>Connecting to a marimo runtime ...</p>
-      </div>
-    </FloatingAlert>
+    isConnecting && (
+      <DelayMount milliseconds={DELAY_MS}>
+        <div className="absolute top-4 m-0 flex items-center min-h-[28px] left-1/2 transform -translate-x-1/2 z-[200] ">
+          <Tooltip content="Connecting to a marimo runtime">
+            <div className="flex items-center">
+              <LoadingEllipsis size={5} className="text-yellow-500" />
+            </div>
+          </Tooltip>
+        </div>
+      </DelayMount>
+    )
   );
 };
