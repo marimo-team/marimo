acdbd6c8c4c4b807e84fef27615a3e43bf831e29
dylan <dylan@marimo.io>
2025-06-23 10:37:43 -0700

    debug: info -> debug

--- marimo/_server/session/serialize.py
+++ marimo/_server/session/serialize.py
@@ -82,7 +82,8 @@ def serialize_session_view(
         if view.cell_ids is not None:
             cell_ids = view.cell_ids.cell_ids
         else:
-            LOGGER.warning(
+            # TODO: Reconcile HTML, and json exports. Something seems a bit off.
+            LOGGER.debug(
                 "When serializing session view, the notebook-order of cells was "
                 "not known. This may cause issues when attempting to "
                 "reconstruct the notebook state from the serialized session "
