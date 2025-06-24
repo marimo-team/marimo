cce318f62ebd83b236dbd406a9a5eed5ae653343
Shahmir Varqha <Sham9871@gmail.com>
2025-06-24 23:17:10 +0800

    fix: set cell editor height and overflow to hidden when hidden (#5391)
    ## 📝 Summary
    
    <!--
    Provide a concise summary of what this pull request is addressing.
    
    If this PR fixes any issues, list them here by number (e.g., Fixes
    #123).
    -->
    Fixes #5367 . Targets the inner div and corrects the css. This issue
    only affects safari (the parent height doesn't seem to be respected).
    
    ## 🔍 Description of Changes
    
    <!--
    Detail the specific changes made in this pull request. Explain the
    problem addressed and how it was resolved. If applicable, provide before
    and after comparisons, screenshots, or any relevant details to help
    reviewers understand the changes easily.
    -->
    
    ## 📋 Checklist
    
    - [x] I have read the [contributor
    guidelines](https://github.com/marimo-team/marimo/blob/main/CONTRIBUTING.md).
    - [ ] For large changes, or changes that affect the public API: this
    change was discussed or approved through an issue, on
    [Discord](https://marimo.io/discord?ref=pr), or the community
    [discussions](https://github.com/marimo-team/marimo/discussions) (Please
    provide a link if applicable).
    - [ ] I have added tests for the changes made.
    - [x] I have run the code and verified that it works as expected.
    
    ## 📜 Reviewers
    
    <!--
    Tag potential reviewers from the community or maintainers who might be
    interested in reviewing this pull request.
    
    Your PR will be reviewed more quickly if you can figure out the right
    person to tag with @ -->
    
    @akshayka

--- frontend/src/components/editor/cell/code/cell-editor.tsx
+++ frontend/src/components/editor/cell/code/cell-editor.tsx
@@ -373,7 +373,8 @@ const CellEditorInternal = ({
   if (isMarkdown && hidden && hasOutput) {
     editorClassName = "h-0 overflow-hidden";
   } else if (hidden) {
-    editorClassName = "opacity-20 h-8 overflow-hidden";
+    editorClassName =
+      "opacity-20 h-8 [&>div.cm-editor]:h-full [&>div.cm-editor]:overflow-hidden";
   }
 
   return (
