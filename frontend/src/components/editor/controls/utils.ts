435d4de04af5e3c4c8b615e668bc5ba7f3cd0a34
Shahmir Varqha <sham9871@gmail.com>
2025-07-02 23:12:37 +0800

    small cleanup

--- frontend/src/components/editor/controls/utils.ts
+++ frontend/src/components/editor/controls/utils.ts
@@ -1,10 +1,10 @@
 /* Copyright 2024 Marimo. All rights reserved. */
 
-export function isCopyKey(e: React.KeyboardEvent<HTMLElement>) {
+export function copyShortcutPressed(e: React.KeyboardEvent<HTMLElement>) {
   return isModifierKey(e) && e.key === "c";
 }
 
-export function isPasteKey(e: React.KeyboardEvent<HTMLElement>) {
+export function pasteShortcutPressed(e: React.KeyboardEvent<HTMLElement>) {
   return isModifierKey(e) && e.key === "v";
 }
 
