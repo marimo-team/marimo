4480e1485aa41546b8f553a7e61da1e3ee4f8dcb
Shahmir Varqha <sham9871@gmail.com>
2025-07-04 18:35:26 +0800

    auto sizing, copy support

--- frontend/src/plugins/impl/data-editor/utils.ts
+++ frontend/src/plugins/impl/data-editor/utils.ts
@@ -11,6 +11,7 @@ import type { DataType } from "@/core/kernel/messages";
 import { logNever } from "@/utils/assertNever";
 import { copyToClipboard } from "@/utils/copy";
 
+// Unused function, but keeping it temporarily if we switch to controlling selection state
 export function copyCells(
   selection: GridSelection,
   getCellContent: (cell: [number, number]) => GridCell,
@@ -50,41 +51,6 @@ export function copyCells(
 
   const text = getTabSeparatedValues(cellsToCopy);
   copyToClipboard(text);
-}
-
-const MIN_WIDTHS: Record<DataType, number> = {
-  boolean: 40,
-  string: 80,
-  number: 70,
-  integer: 70,
-  date: 100,
-  datetime: 140,
-  time: 80,
-  unknown: 80,
-};
-
-const ICON_OFFSET_LENGTH = 30;
-export function getColumnWidth<T>(
-  fieldType: DataType,
-  values: T[],
-  columnTitle: string,
-): number {
-  if (fieldType === "boolean") {
-    return 200;
-  }
-
-  const minWidth = MIN_WIDTHS[fieldType];
-
-  const lengths = [
-    columnTitle.length,
-    ...values.map((value) => String(value).length),
-  ];
-
-  // 8px per character
-  const calculatedWidth = Math.max(...lengths) * 8 + ICON_OFFSET_LENGTH;
-
-  // Return the larger of minimum width or calculated width, capped at 600px
-  return Math.min(Math.max(calculatedWidth, minWidth), 600);
 }
 
 export function getColumnKind(fieldType: DataType): GridCellKind {
