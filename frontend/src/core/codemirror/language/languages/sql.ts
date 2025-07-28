74214b89a48d0c175c0c7260d43c1226ce0ad166
Myles Scolnick <myles@marimo.io>
2025-07-28 09:28:06 -0400

    fix: dynamic SQL keyword completion based on dialect (#5775)
    This makes the keyword completion dynamic, using the dialect for the
    selected connection. Previously it was just always using the standard
    SQL dialect.
    
    This also fixes caching - it was always "missing" the cache (it always
    created a new object which gets passed to a Map and check for object
    equality)
    This also adds a few tests.

--- frontend/src/core/codemirror/language/languages/sql.ts
+++ frontend/src/core/codemirror/language/languages/sql.ts
@@ -16,7 +16,7 @@ import {
   schemaCompletionSource,
   sql,
 } from "@codemirror/lang-sql";
-import type { Extension } from "@codemirror/state";
+import type { EditorState, Extension } from "@codemirror/state";
 import type { SyntaxNode, TreeCursor } from "@lezer/common";
 import { parser } from "@lezer/python";
 import dedent from "string-dedent";
@@ -185,34 +185,20 @@ export class SQLLanguageAdapter
   }
 
   getExtension(): Extension[] {
-    const keywordCompletion = keywordCompletionSource(StandardSQL);
     return [
-      sql({
-        dialect: StandardSQL,
-      }),
+      sql({}),
       autocompletion({
         // We remove the default keymap because we use our own which
         // handles the Escape key correctly in Vim
         defaultKeymap: false,
         activateOnTyping: true,
         override: [
+          // Completions for schema
           tablesCompletionSource(),
           // Complete for variables in SQL {} blocks
           variableCompletionSource,
-          (ctx) => {
-            // We want to ignore keyword completions on something like
-            // `WHERE my_table.col`
-            //                    ^cursor
-            const textBefore = ctx.matchBefore(/\.\w*/);
-            if (textBefore) {
-              // If there is a match, we are typing after a dot,
-              // so we don't want to trigger SQL keyword completion
-              return null;
-            }
-
-            const result = keywordCompletion(ctx);
-            return result;
-          },
+          // Completions for dialect keywords
+          customKeywordCompletionSource(),
         ],
       }),
     ];
@@ -221,135 +207,185 @@ export class SQLLanguageAdapter
 
 type TableToCols = Record<string, string[]>;
 type Schemas = Record<string, TableToCols>;
+type CachedSchema = Pick<SQLConfig, "schema" | "defaultSchema"> & {
+  shouldAddLocalTables: boolean;
+};
 
 export class SQLCompletionStore {
-  private cache = new LRUCache<[DataSourceConnection, TableToCols], SQLConfig>(
-    10,
-  );
+  private cache: LRUCache<DataSourceConnection, CachedSchema>;
+
+  constructor() {
+    this.cache = new LRUCache(10, {
+      create: (connection) => this.getConnectionSchema(connection),
+    });
+  }
+
+  private getConnection(
+    connectionName: ConnectionName,
+  ): DataSourceConnection | undefined {
+    const dataConnectionsMap = store.get(dataConnectionsMapAtom);
+    return dataConnectionsMap.get(connectionName);
+  }
+
+  private getConnectionSchema(connection: DataSourceConnection): CachedSchema {
+    const schemaMap: Record<string, TableToCols> = {};
+    const databaseMap: Record<string, Schemas> = {};
+
+    // When there is only one database, it is the default
+    const defaultDb = connection.databases.find(
+      (db) =>
+        db.name === connection.default_database ||
+        connection.databases.length === 1,
+    );
+
+    const dbToVerify = defaultDb ?? connection.databases[0];
+    const isSchemalessDb =
+      dbToVerify?.schemas.some((schema) => isSchemaless(schema.name)) ?? false;
+
+    // For schemaless databases, treat databases as schemas
+    if (isSchemalessDb) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const dbToTablesMap: Record<string, any> = {};
+
+      for (const db of connection.databases) {
+        const isDefaultDb = db.name === defaultDb?.name;
+
+        for (const schema of db.schemas) {
+          for (const table of schema.tables) {
+            const columns = table.columns.map((col) => col.name);
+
+            if (isDefaultDb) {
+              // For default database, add tables directly to top level
+              dbToTablesMap[table.name] = columns;
+            } else {
+              // Otherwise nest under database name
+              dbToTablesMap[db.name] = dbToTablesMap[db.name] || {};
+              dbToTablesMap[db.name][table.name] = columns;
+            }
+          }
+        }
+      }
+
+      return {
+        shouldAddLocalTables: false,
+        schema: dbToTablesMap,
+        defaultSchema: defaultDb?.name,
+      };
+    }
+
+    // For default db, we can use the schema name directly
+    for (const schema of defaultDb?.schemas ?? []) {
+      schemaMap[schema.name] = {};
+      for (const table of schema.tables) {
+        const columns = table.columns.map((col) => col.name);
+        schemaMap[schema.name][table.name] = columns;
+      }
+    }
+
+    // Otherwise, we need to use the fully qualified name
+    for (const database of connection.databases) {
+      if (database.name === defaultDb?.name) {
+        continue;
+      }
+      databaseMap[database.name] = {};
+
+      for (const schema of database.schemas) {
+        databaseMap[database.name][schema.name] = {};
+
+        for (const table of schema.tables) {
+          const columns = table.columns.map((col) => col.name);
+          databaseMap[database.name][schema.name][table.name] = columns;
+        }
+      }
+    }
+
+    return {
+      shouldAddLocalTables: true,
+      schema: { ...databaseMap, ...schemaMap },
+      defaultSchema: connection.default_schema ?? undefined,
+    };
+  }
+
+  /**
+   * Get the dialect for a connection.
+   * If the connection is not found, return the standard SQL dialect.
+   */
+  getDialect(connectionName: ConnectionName): SQLDialect {
+    const connection = this.getConnection(connectionName);
+    if (!connection) {
+      return StandardSQL;
+    }
+    return guessDialect(connection) ?? StandardSQL;
+  }
 
   getCompletionSource(connectionName: ConnectionName): SQLConfig | null {
-    const dataConnectionsMap = store.get(dataConnectionsMapAtom);
-    const connection = dataConnectionsMap.get(connectionName);
+    const connection = this.getConnection(connectionName);
     if (!connection) {
       return null;
     }
 
-    const localTables = store.get(datasetTablesAtom);
-
-    // If there is a conflict with connection tables,
-    // the engine will prioritize the connection tables without special handling
-    const tablesMap: TableToCols = {};
-    for (const table of localTables) {
-      const tableColumns = table.columns.map((col) => col.name);
-      tablesMap[table.name] = tableColumns;
-    }
-
-    const cacheKey: [DataSourceConnection, TableToCols] = [
-      connection,
-      tablesMap,
-    ];
-
-    let cacheConfig: SQLConfig | undefined = this.cache.get(cacheKey);
-    if (!cacheConfig) {
-      const schemaMap: Record<string, TableToCols> = {};
-      const databaseMap: Record<string, Schemas> = {};
-
-      const baseConfig: SQLConfig = {
-        dialect: guessDialect(connection),
-        schema: schemaMap,
-        defaultSchema: connection.default_schema ?? undefined,
-        defaultTable: getSingleTable(connection),
-      };
-
-      // When there is only one database, it is the default
-      const defaultDb = connection.databases.find(
-        (db) =>
-          db.name === connection.default_database ||
-          connection.databases.length === 1,
-      );
-
-      const dbToVerify = defaultDb ?? connection.databases[0];
-      const isSchemalessDb =
-        dbToVerify?.schemas.some((schema) => isSchemaless(schema.name)) ??
-        false;
-
-      // For schemaless databases, treat databases as schemas
-      if (isSchemalessDb) {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const dbToTablesMap: Record<string, any> = {};
-
-        for (const db of connection.databases) {
-          const isDefaultDb = db.name === defaultDb?.name;
-
-          for (const schema of db.schemas) {
-            for (const table of schema.tables) {
-              const columns = table.columns.map((col) => col.name);
-
-              if (isDefaultDb) {
-                // For default database, add tables directly to top level
-                dbToTablesMap[table.name] = columns;
-              } else {
-                // Otherwise nest under database name
-                dbToTablesMap[db.name] = dbToTablesMap[db.name] || {};
-                dbToTablesMap[db.name][table.name] = columns;
-              }
-            }
-          }
-        }
-
-        cacheConfig = {
-          ...baseConfig,
-          schema: dbToTablesMap,
-          defaultSchema: defaultDb?.name,
-        };
-        return cacheConfig;
+    const getTablesMap = () => {
+      const localTables = store.get(datasetTablesAtom);
+      // If there is a conflict with connection tables,
+      // the engine will prioritize the connection tables without special handling
+      const tablesMap: TableToCols = {};
+      for (const table of localTables) {
+        const tableColumns = table.columns.map((col) => col.name);
+        tablesMap[table.name] = tableColumns;
       }
+      return tablesMap;
+    };
 
-      // For default db, we can use the schema name directly
-      for (const schema of defaultDb?.schemas ?? []) {
-        schemaMap[schema.name] = {};
-        for (const table of schema.tables) {
-          const columns = table.columns.map((col) => col.name);
-          schemaMap[schema.name][table.name] = columns;
-        }
-      }
+    const schema = this.cache.getOrCreate(connection);
 
-      // Otherwise, we need to use the fully qualified name
-      for (const database of connection.databases) {
-        if (database.name === defaultDb?.name) {
-          continue;
-        }
-        databaseMap[database.name] = {};
-
-        for (const schema of database.schemas) {
-          databaseMap[database.name][schema.name] = {};
-
-          for (const table of schema.tables) {
-            const columns = table.columns.map((col) => col.name);
-            databaseMap[database.name][schema.name][table.name] = columns;
-          }
-        }
-      }
-
-      cacheConfig = {
-        ...baseConfig,
-        schema: { ...databaseMap, ...schemaMap, ...tablesMap },
-        defaultSchema: connection.default_schema ?? undefined,
-      };
-      this.cache.set(cacheKey, cacheConfig);
-    }
-
-    return cacheConfig;
+    return {
+      dialect: guessDialect(connection),
+      schema: schema.shouldAddLocalTables
+        ? { ...schema.schema, ...getTablesMap() }
+        : schema.schema,
+      defaultSchema: schema.defaultSchema,
+      defaultTable: getSingleTable(connection),
+    };
   }
 }
 
 const SCHEMA_CACHE = new SQLCompletionStore();
 
+function getSQLMetadata(state: EditorState): SQLLanguageAdapterMetadata {
+  return state.field(languageMetadataField) as SQLLanguageAdapterMetadata;
+}
+
+/**
+ * Custom keyword completion source that dynamically gets the Dialect.
+ * This also ignores keyword completions on table columns.
+ */
+function customKeywordCompletionSource(): CompletionSource {
+  return (ctx) => {
+    const metadata = getSQLMetadata(ctx.state);
+    const connectionName = metadata.engine;
+    const dialect = SCHEMA_CACHE.getDialect(connectionName);
+
+    // We want to ignore keyword completions on something like
+    // `WHERE my_table.col`
+    //                    ^cursor
+    const textBefore = ctx.matchBefore(/\.\w*/);
+    if (textBefore) {
+      // If there is a match, we are typing after a dot,
+      // so we don't want to trigger SQL keyword completion
+      return null;
+    }
+
+    const result = keywordCompletionSource(dialect)(ctx);
+    return result;
+  };
+}
+
+/**
+ * Custom schema completion source that dynamically gets the Dialect and SQL tables.
+ */
 function tablesCompletionSource(): CompletionSource {
   return (ctx) => {
-    const metadata = ctx.state.field(
-      languageMetadataField,
-    ) as SQLLanguageAdapterMetadata;
+    const metadata = getSQLMetadata(ctx.state);
     const connectionName = metadata.engine;
     const config = SCHEMA_CACHE.getCompletionSource(connectionName);
 
