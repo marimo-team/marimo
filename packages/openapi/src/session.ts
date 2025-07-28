970f172824908037ed1c1d733a31fcb62c82f0c7
Myles Scolnick <myles@marimo.io>
2025-07-28 12:01:54 -0400

    fix

--- packages/openapi/src/session.ts
+++ packages/openapi/src/session.ts
@@ -51,6 +51,7 @@ export interface components {
         | "application/vnd.marimo+mimebundle"
         | "application/vnd.vega.v5+json"
         | "application/vnd.vegalite.v5+json"
+        | "application/vnd.jupyter.widget-view+json"
         | "image/png"
         | "image/svg+xml"
         | "image/tiff"
