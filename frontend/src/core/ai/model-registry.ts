969cbf9afe8d547e60460a6eac66b42f7068bfee
Shahmir Varqha <sham9871@gmail.com>
2025-08-14 16:35:53 +0800

    json nesting and readme

--- frontend/src/core/ai/model-registry.ts
+++ frontend/src/core/ai/model-registry.ts
@@ -69,7 +69,7 @@ export class AiModelRegistry {
       ]);
     }
 
-    for (const model of models.default) {
+    for (const model of models.models) {
       const modelId = model.model as ShortModelId;
       const modelInfo: AiModel = model as AiModel;
 
