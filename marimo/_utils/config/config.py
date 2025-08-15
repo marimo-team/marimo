d1b1998f9baeb3c1c458b1db414e3d7aa927b1c0
TDYQ-Liu <71477981+TDYQ-Liu@users.noreply.github.com>
2025-08-15 16:25:01 +0800

    Using MARIMO_ROOT_DIR environment variable to support changing root dir
    The default root dir is ".marimo" and cannot be modified, it will autocreate ".marimo" folder at the user's home in Linux platform.
    
    Add the environment variable MARIMO_ROOT_DIR to allow the user to set the root dir location.

--- marimo/_utils/config/config.py
+++ marimo/_utils/config/config.py
@@ -9,7 +9,10 @@ from typing import Any, Optional, TypeVar
 from marimo._utils.parse_dataclass import parse_raw
 from marimo._utils.toml import is_toml_error, read_toml
 
-ROOT_DIR = ".marimo"
+if (_root_dir := os.environ.get("MARIMO_ROOT_DIR")) is None:
+    ROOT_DIR = ".marimo"
+else:
+    ROOT_DIR = _root_dir
 
 T = TypeVar("T")
 
