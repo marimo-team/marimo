bfda2ae75e6888cd83fb23027e66bf5a5d167e71
Myles Scolnick <myles@marimo.io>
2025-08-21 23:59:48 -0600

    fix test

--- marimo/_utils/config/config.py
+++ marimo/_utils/config/config.py
@@ -1,15 +1,15 @@
 # Copyright 2024 Marimo. All rights reserved.
 from __future__ import annotations
 
-import os
 from dataclasses import asdict
-from pathlib import Path
-from tempfile import TemporaryDirectory
-from typing import Any, TypeVar
+from typing import TYPE_CHECKING, Any, TypeVar
 
 from marimo._utils.parse_dataclass import parse_raw
 from marimo._utils.toml import is_toml_error, read_toml
-from marimo._utils.xdg import home_path, marimo_state_dir
+from marimo._utils.xdg import marimo_state_dir
+
+if TYPE_CHECKING:
+    from pathlib import Path
 
 ROOT_DIR = marimo_state_dir()
 
@@ -27,8 +27,7 @@ class ConfigReader:
 
     @staticmethod
     def for_filename(filename: str) -> ConfigReader:
-        home_directory = ConfigReader._get_home_directory()
-        filepath = home_directory / ROOT_DIR / filename
+        filepath = ROOT_DIR / filename
         return ConfigReader(filepath)
 
     def read_toml(self, cls: type[T], *, fallback: T) -> T:
@@ -50,17 +49,3 @@ class ConfigReader:
         dict_data = {k: v for k, v in dict_data.items() if v is not None}
 
         self.filepath.write_text(tomlkit.dumps(dict_data))
-
-    @staticmethod
-    def _get_home_directory() -> Path:
-        # If in pytest, we want to set a temporary directory
-        if os.environ.get("PYTEST_CURRENT_TEST"):
-            # If the home directory is given by test, take it
-            home_dir = os.environ.get("MARIMO_PYTEST_HOME_DIR")
-            if home_dir is not None:
-                return Path(home_dir)
-            else:
-                tmpdir = TemporaryDirectory()
-                return Path(tmpdir.name)
-        else:
-            return home_path()
