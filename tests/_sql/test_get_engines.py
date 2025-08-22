738bfc48e6f006e7a54d3752829d53a989a48e08
Shahmir Varqha <sham9871@gmail.com>
2025-08-22 18:03:43 +0800

    fix tests

--- tests/_sql/test_get_engines.py
+++ tests/_sql/test_get_engines.py
@@ -51,7 +51,7 @@ def test_engine_to_data_source_connection() -> None:
     assert connection.display_name == "duckdb (my_duckdb)"
     assert connection.default_database == "memory"
     assert connection.default_schema == "main"
-    assert connection.databases == []
+    assert connection.databases == [Database(name="memory", dialect="duckdb")]
 
     # Test with ClickhouseEmbedded engine
     clickhouse_engine = ClickhouseEmbedded(None)
