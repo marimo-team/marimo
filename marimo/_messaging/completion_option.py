7631714094640b68151d10ffff3183a457d89482
Trevor Manz <trevor.j.manz@gmail.com>
2025-09-03 11:12:54 -0400

    Migrate `CompletionOption`

--- marimo/_messaging/completion_option.py
+++ marimo/_messaging/completion_option.py
@@ -3,12 +3,12 @@
 
 from __future__ import annotations
 
-from dataclasses import dataclass
 from typing import Optional
 
+import msgspec
 
-@dataclass
-class CompletionOption:
+
+class CompletionOption(msgspec.Struct, rename="camel"):
     # completed symbol name
     name: str
     # type of symbol
