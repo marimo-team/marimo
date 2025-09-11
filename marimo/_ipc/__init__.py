0b29d1ee2f8606bb38f4299ab3958f217a539420
Trevor Manz <trevor.j.manz@gmail.com>
2025-09-11 11:22:20 -0400

    Add exports to `marimo._ipc`

--- marimo/_ipc/__init__.py
+++ marimo/_ipc/__init__.py
@@ -1,2 +1,11 @@
 # Copyright 2025 Marimo. All rights reserved.
 """Experimental IPC implementation (using ZeroMQ)."""
+
+from marimo._ipc.queue_manager import QueueManager
+from marimo._ipc.types import ConnectionInfo, LaunchKernelArgs
+
+__all__ = [
+    "LaunchKernelArgs",
+    "ConnectionInfo",
+    "QueueManager",
+]
