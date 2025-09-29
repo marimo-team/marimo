dc84192f95e62b0ba54557490db8439b2813e729
Myles Scolnick <myles@marimo.io>
2025-09-29 14:00:47 -0400

    pass down user-config for /snippets

--- marimo/_server/api/endpoints/documentation.py
+++ marimo/_server/api/endpoints/documentation.py
@@ -3,7 +3,10 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from starlette.authentication import requires
+
 from marimo import _loggers
+from marimo._server.api.deps import AppState
 from marimo._server.router import APIRouter
 from marimo._snippets.snippets import Snippets, read_snippets
 
@@ -15,12 +18,14 @@ LOGGER = _loggers.marimo_logger()
 # Router for documentation
 router = APIRouter()
 
-
+# Cache snippets once per server instance
 _SNIPPETS: list[Snippets] = []
 
 
 @router.get("/snippets")
+@requires("edit")
 async def load_snippets(
+    *,
     request: Request,
 ) -> Snippets:
     """
@@ -32,7 +37,8 @@ async def load_snippets(
                     schema:
                         $ref: "#/components/schemas/Snippets"
     """
-    del request
+    app_state = AppState(request)
+    config = app_state.require_current_session().config_manager.get_config()
     if not _SNIPPETS:
-        _SNIPPETS.append(await read_snippets())
+        _SNIPPETS.append(await read_snippets(config))
     return _SNIPPETS[0]
--- tests/_server/api/endpoints/test_documentation.py
+++ tests/_server/api/endpoints/test_documentation.py
@@ -1,9 +1,18 @@
 # Copyright 2024 Marimo. All rights reserved.
 from starlette.testclient import TestClient
 
+from tests._server.mocks import token_header, with_session
 
+SESSION_ID = "session-123"
+HEADERS = {
+    "Marimo-Session-Id": SESSION_ID,
+    **token_header("fake-token"),
+}
+
+
+@with_session(SESSION_ID)
 def test_snippets(client: TestClient) -> None:
-    response = client.get("/api/documentation/snippets")
+    response = client.get("/api/documentation/snippets", headers=HEADERS)
     assert response.status_code == 200, response.text
     content = response.json()
     assert content["snippets"] is not None
@@ -12,7 +21,7 @@ def test_snippets(client: TestClient) -> None:
 
     # call the endpoint a second time and make sure the
     # same snippets are retrieved
-    response = client.get("/api/documentation/snippets")
+    response = client.get("/api/documentation/snippets", headers=HEADERS)
     assert response.status_code == 200, response.text
     content = response.json()
     assert content["snippets"] is not None
