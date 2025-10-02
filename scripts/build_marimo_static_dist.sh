74e3690293293983bc4c9261fd5068ff21849816
Ícaro Guerra <icaro@plasmic.app>
2025-10-02 15:22:12 +0200

    fix(build#oso): copy correct frontend files to marimo WASM

--- scripts/build_marimo_static_dist.sh
+++ scripts/build_marimo_static_dist.sh
@@ -29,6 +29,12 @@ fi
 pushd frontend
 PYODIDE=true NODE_OPTIONS=--max-old-space-size=${NODE_HEAP_SIZE:-6144} pnpm vite build --config oso.viteconfig.mts
 
+# Copy the built files to marimo/_static
+# This makes sure our frontend files match the files in marimo WASM.
+rm -rf "${repo_dir}/marimo/_static"
+mkdir -p "${repo_dir}/marimo/_static"
+cp -r dist/ "${repo_dir}/marimo/_static/"
+
 # Move files to the build directory
 mv dist "${build_path}"
 
