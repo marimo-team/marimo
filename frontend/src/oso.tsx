733ce37ef66edf80b6895b38b5c5b69c46dad394
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2025-10-08 11:45:15 -0700

    fix(frontend#oso): add posthog iframe setup

--- frontend/src/oso.tsx
+++ frontend/src/oso.tsx
@@ -1,11 +1,24 @@
 /* Copyright 2024 Marimo. All rights reserved. */
 
+import posthog from 'posthog-js'
 import { mount } from "@/oso-mount";
+
 
 declare global {
   interface Window {
     __MARIMO_MOUNT_CONFIG__: unknown;
   }
+}
+
+if (import.meta.env.VITE_POSTHOG_PUBLIC_API_KEY) {
+  posthog.init(import.meta.env.VITE_POSTHOG_PUBLIC_API_KEY, {
+    session_recording: {
+      // WARNING: Only enable this if you understand the security implications
+      recordCrossOriginIframes: true,
+    }
+  });
+} else {
+  console.warn("POSTHOG_PUBLIC_API_KEY not set, skipping posthog init");
 }
 
 // eslint-disable-next-line ssr-friendly/no-dom-globals-in-module-scope
