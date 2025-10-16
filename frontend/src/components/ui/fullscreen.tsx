b72135fd35cbb1ae9c737f66e0f112960f193ec3
Trevor Manz <trevor.j.manz@gmail.com>
2025-10-16 15:53:18 -0400

    Trigger rerender

--- frontend/src/components/ui/fullscreen.tsx
+++ frontend/src/components/ui/fullscreen.tsx
@@ -30,39 +30,42 @@ export function withFullScreenAsRoot<
 
   const FindClosestVscodeOutputContainer = (props: T) => {
     const el = useRef<HTMLDivElement>(null);
+    const [, setMounted] = useState(false);
     console.warn("[debug] el.current", el.current);
     if (!el.current) {
       return (
         <>
-          <div ref={el} className="contents invisible" />
-          <span>Loading...</span>
+          <div
+            ref={(element) => {
+              setMounted(true);
+              el.current = element;
+            }}
+            className="contents invisible"
+          />
+          <span>should not show this...</span>
         </>
       );
     }
 
+    const closest = closestThroughShadowDOMs(
+      el.current,
+      "[data-vscode-output-container]",
+    );
     console.warn(
       "[debug] el.current.closest('[data-vscode-output-container]')",
       "el.current.closest('[data-vscode-output-container]')",
-      el.current.closest("[data-vscode-output-container]"),
+      closest,
     );
 
     return (
       <>
         <div ref={el} className="contents invisible" />
-        <Component
-          {...props}
-          container={el.current.closest("[data-vscode-output-container]")}
-        />
+        <Component {...props} container={closest} />
       </>
     );
   };
 
   const Comp = (props: T) => {
-    const fullScreenElement = useFullScreenElement();
-    if (!fullScreenElement) {
-      return <Component {...props} />;
-    }
-
     // If we are in the VSCode extension, we use the VSCode output container
     const vscodeOutputContainer = isInVscodeExtension();
     console.warn("[debug] vscodeOutputContainer", vscodeOutputContainer);
@@ -70,9 +73,38 @@ export function withFullScreenAsRoot<
       return <FindClosestVscodeOutputContainer {...props} />;
     }
 
+    const fullScreenElement = useFullScreenElement();
+    if (!fullScreenElement) {
+      return <Component {...props} />;
+    }
+
     return <Component {...props} container={fullScreenElement} />;
   };
 
   Comp.displayName = Component.displayName;
   return Comp;
+}
+
+function closestThroughShadowDOMs(
+  element: Element,
+  selector: string,
+): Element | null {
+  let currentElement: Element | null = element;
+
+  while (currentElement) {
+    const cellElement = currentElement.closest(selector);
+    if (cellElement) {
+      return cellElement;
+    }
+
+    const root = currentElement.getRootNode();
+    currentElement =
+      root instanceof ShadowRoot ? root.host : currentElement.parentElement;
+
+    if (currentElement === root) {
+      break;
+    }
+  }
+
+  return null;
 }
