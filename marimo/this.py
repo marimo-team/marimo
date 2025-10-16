f09f676e34a6fefed637ac817854e45e633c5b8a
Akshay Agrawal <akshay@marimo.io>
2025-10-16 11:58:04 -0700

    Update marimo/this.py
    Co-authored-by: Trevor Manz <trevor.j.manz@gmail.com>

--- marimo/this.py
+++ marimo/this.py
@@ -3,7 +3,7 @@ never forget that hidden failures are the worst.
 features on paper don't reflect how a user might feel,
 sometimes it's totally fine to reinvent the wheel.
  
-the best features are often hidden without any kind of flair,
+the best details are often hidden without any kind of flair,
 you can lead ai to software, but you cannot make it care.
 some things are certain, others remain a bet.
 the future of programming hasn't been invented yet.""")  # noqa: T201, W293
