75c6cc447aa4173d7ec3c538dae2e86ed9c7ea1f
Shahmir Varqha <sham9871@gmail.com>
2025-10-22 01:24:11 +0800

    remove contents instead

--- frontend/src/components/utils/delay-mount.tsx
+++ frontend/src/components/utils/delay-mount.tsx
@@ -76,12 +76,6 @@ export const DelayMount = ({
   const shouldShow = visibility && !hasBeenVisible ? false : mounted;
 
   return (
-    <div
-      ref={visibility ? ref : null}
-      // Display fallback as contents so it doesn't affect layout flow.
-      className={shouldShow ? undefined : "contents"}
-    >
-      {shouldShow ? children : fallback}
-    </div>
+    <div ref={visibility ? ref : null}>{shouldShow ? children : fallback}</div>
   );
 };
