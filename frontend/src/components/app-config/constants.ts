761572053592408ae404c96842ccb7ef7968a436
Kyrre-Wahl-Kongsgard <kyrre.wahl.kongsgard@dnb.no>
2025-10-28 10:24:56 +0100

    add all aws regions that support bedrock

--- frontend/src/components/app-config/constants.ts
+++ frontend/src/components/app-config/constants.ts
@@ -7,7 +7,10 @@ export const AWS_REGIONS = [
   "us-east-1",
   "us-east-2",
   "us-west-2",
-  "eu-central-1",
   "ap-northeast-1",
   "ap-southeast-1",
+  "ap-southeast-2",
+  "ap-south-1",
+  "eu-west-1",
+  "eu-central-1",
 ] as const;
