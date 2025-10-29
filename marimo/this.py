f1ad2081d2b67bda53b4838ee18f1b6cd582c287
vincent d warmerdam <vincentwarmerdam@gmail.com>
2025-10-29 14:49:54 +0100

    Update marimo/this.py
    Co-authored-by: Shahmir Varqha <Sham9871@gmail.com>

--- marimo/this.py
+++ marimo/this.py
@@ -3,7 +3,7 @@ remember that hidden failures are the worst.
 spend some time, understand how a user might feel,
 sometimes it's totally fine to reinvent the wheel.
 
-take a step back, give yourself some time and space. 
+take a step back, give yourself time and space. 
 because once a mistake goes in, it's hard to erase.
 if you want velocity, remember to grow slow. 
 the antidote to a distraction is the word "no". 
