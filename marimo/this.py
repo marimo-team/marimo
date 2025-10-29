42d52bae903a9d22122e68ba6a4e82c002a5a1a1
vincent d warmerdam <vincentwarmerdam@gmail.com>
2025-10-29 16:43:21 +0100

    Update this.py

--- marimo/this.py
+++ marimo/this.py
@@ -1,12 +1,17 @@
-print("""take a step back, know what to do first.
-remember that hidden failures are the worst.
-spend some time, understand how a user might feel,
+print("""spend some time, understand how a user might feel,
 sometimes it's totally fine to reinvent the wheel.
+take a step back, know what to do first.
+remember that hidden failures are the worst.
 
 take a step back, give yourself time and space. 
 because once a mistake goes in, it's hard to erase.
 if you want velocity, remember to grow slow. 
 the antidote to a distraction is the word "no". 
+
+working with good people goes a long way,
+progress will happen and folks will stay.
+without enthousiasm there's no great story to tell. 
+perfection is in the many little things done well.
 
 the best details are hidden without any kind of flair,
 you can lead ai to software, but you cannot make it care.
