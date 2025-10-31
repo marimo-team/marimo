847cfb2631a2a9b02defe64dea31eb8758803c50
Shahmir Varqha <Sham9871@gmail.com>
2025-11-01 02:11:42 +0800

    disable sql linter (#7022)
    ## 📝 Summary
    
    <!--
    Provide a concise summary of what this pull request is addressing.
    
    If this PR fixes any issues, list them here by number (e.g., Fixes
    #123).
    -->
    
    hotfix for sql cells being stuck when autocompletion popup
    
    ## 🔍 Description of Changes
    
    <!--
    Detail the specific changes made in this pull request. Explain the
    problem addressed and how it was resolved. If applicable, provide before
    and after comparisons, screenshots, or any relevant details to help
    reviewers understand the changes easily.
    -->
    
    ## 📋 Checklist
    
    - [x] I have read the [contributor
    guidelines](https://github.com/marimo-team/marimo/blob/main/CONTRIBUTING.md).
    - [ ] For large changes, or changes that affect the public API: this
    change was discussed or approved through an issue, on
    [Discord](https://marimo.io/discord?ref=pr), or the community
    [discussions](https://github.com/marimo-team/marimo/discussions) (Please
    provide a link if applicable).
    - [ ] I have added tests for the changes made.
    - [x] I have run the code and verified that it works as expected.

--- frontend/src/components/app-config/data-form.tsx
+++ frontend/src/components/app-config/data-form.tsx
@@ -2,7 +2,6 @@
 
 import type { ChangeEvent } from "react";
 import type { FieldPath, UseFormReturn } from "react-hook-form";
-import { Checkbox } from "@/components/ui/checkbox";
 import {
   FormControl,
   FormDescription,
@@ -213,7 +212,8 @@ export const DataForm = ({
           {renderDiscoveryForm("datasources.auto_discover_columns", "Columns")}
         </div>
 
-        <FormField
+        {/* TODO: Issue with frontend being stuck */}
+        {/* <FormField
           control={form.control}
           name="diagnostics.sql_linter"
           render={({ field }) => (
@@ -238,7 +238,7 @@ export const DataForm = ({
               </FormDescription>
             </div>
           )}
-        />
+        /> */}
 
         <FormField
           control={form.control}
