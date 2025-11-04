ea6fcf06a15748c5d06a1f3ae9c10d7395120d91
Myles Scolnick <myles@marimo.io>
2025-11-04 17:46:42 -0500

    Update code.py
    Co-authored-by: Dylan Madisetti <dylan@marimo.io>

--- marimo/_utils/code.py
+++ marimo/_utils/code.py
@@ -5,6 +5,6 @@ from __future__ import annotations
 def hash_code(code: str) -> str:
     import hashlib
 
-    return hashlib.sha256(
+    return hashlib.md5(
         code.encode("utf-8"), usedforsecurity=False
     ).hexdigest()
