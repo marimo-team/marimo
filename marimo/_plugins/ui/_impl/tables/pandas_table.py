9abb264071623197a32770d7d8f30f73b733f832
Shahmir Varqha <sham9871@gmail.com>
2025-11-20 17:41:19 +0800

    add type ignore

--- marimo/_plugins/ui/_impl/tables/pandas_table.py
+++ marimo/_plugins/ui/_impl/tables/pandas_table.py
@@ -133,7 +133,7 @@ class PandasTableManagerFactory(TableManagerFactory):
                         exc_info=e,
                     )
                     return sanitize_json_bigint(
-                        result.to_dict(orient="records")
+                        result.to_dict(orient="records")  # type: ignore
                     )
 
                 # Flatten row multi-index
@@ -187,7 +187,7 @@ class PandasTableManagerFactory(TableManagerFactory):
                                 "Indexes with more than one level are not well supported, call reset_index() or use mo.plain(df)"
                             )
 
-                return sanitize_json_bigint(result.to_dict(orient="records"))
+                return sanitize_json_bigint(result.to_dict(orient="records"))  # type: ignore
 
             def _infer_dtype(self, column: ColumnName) -> str:
                 # Typically, pandas dtypes returns a generic dtype
