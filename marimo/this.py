c0a07b79cadbb4040ac8313a8644d7545d1eea5e
Akshay Agrawal <akshay@marimo.io>
2025-11-20 10:45:47 -0800

    The marimo contract

--- marimo/this.py
+++ marimo/this.py
@@ -1,20 +1,34 @@
-print("""spend some time, understand how a user might feel,
-sometimes it's totally fine to reinvent the wheel.
-take a step back, know what to do first.
-remember that hidden failures are the worst.
+print("""The marimo contract
 
-take a step back, give yourself time and space. 
-because once a mistake goes in, it's hard to erase.
-if you want velocity, remember to grow slow. 
-the antidote to a distraction is the word "no". 
+To use marimo means entering into a social contract.
+In this contract you agree
+to define each variable just once,
+to reference variables without cycles,
+to mutate variables sparingly,
+to bind variables to UI elements.
 
-working with good people goes a long way,
-progress will happen and folks will stay.
-without enthusiasm there's no great story to tell. 
-perfection is in the many little things done well.
+In return marimo assembles from your variables
+something greater than the sum of their cells.
+A reactive notebook,
+a Python program,
+a data app, three turns on the same perspective of
+a dataflow graph.
 
-the best details are hidden without any kind of flair,
-you can lead ai to software, but you cannot make it care.
-some things are certain, others remain a bet.
-the future of programming hasn't been invented yet.
-""")  # noqa: T201, W293
+With this dataflow graph come two simple rules,
+when a cell is run all others that reference its variables run too,
+when a UI element is touched, all cells that reference it run too.
+
+From these two rules marimo is built,
+reactive execution yields reproducible execution,
+interactions flow from you through your code,
+data and models become tangible in a meticulous construction
+that's Python all the way down.
+
+In this construction we hope you will find
+simplicity
+clarity
+immersion
+interactivity
+dualities
+beauty
+joy.""")  # noqa: T201, W293
