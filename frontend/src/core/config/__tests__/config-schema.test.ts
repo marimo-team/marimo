ca5de05fd4ddc3cb352fa717c044b95f3e156b41
Shahmir Varqha <sham9871@gmail.com>
2025-12-12 23:06:54 +0800

    fix test

--- frontend/src/core/config/__tests__/config-schema.test.ts
+++ frontend/src/core/config/__tests__/config-schema.test.ts
@@ -55,8 +55,8 @@ test("default UserConfig - empty", () => {
       },
       "completion": {
         "activate_on_typing": true,
-        "signature_hint_on_typing": false,
         "copilot": false,
+        "signature_hint_on_typing": false,
       },
       "diagnostics": {},
       "display": {
@@ -124,8 +124,8 @@ test("default UserConfig - one level", () => {
       },
       "completion": {
         "activate_on_typing": true,
-        "signature_hint_on_typing": false,
         "copilot": false,
+        "signature_hint_on_typing": false,
       },
       "diagnostics": {},
       "display": {
@@ -226,7 +226,11 @@ test("resolvedMarimoConfigAtom overrides correctly and does not mutate the origi
   const result = store.get(resolvedMarimoConfigAtom);
 
   expect(result).toEqual({
-    completion: { activate_on_typing: true, copilot: "github" },
+    completion: {
+      activate_on_typing: true,
+      copilot: "github",
+      signature_hint_on_typing: false,
+    },
     save: {
       autosave: "after_delay",
       autosave_delay: 1000,
@@ -237,7 +241,11 @@ test("resolvedMarimoConfigAtom overrides correctly and does not mutate the origi
   });
 
   expect(initialUserConfig).toEqual({
-    completion: { activate_on_typing: true, copilot: false },
+    completion: {
+      activate_on_typing: true,
+      copilot: false,
+      signature_hint_on_typing: false,
+    },
     save: {
       autosave: "after_delay",
       autosave_delay: 1000,
