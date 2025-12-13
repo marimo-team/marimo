023c744bf7991a25370f547fcd82a2b2af8a64aa
Shahmir Varqha <sham9871@gmail.com>
2025-12-13 22:51:34 +0800

    add messages and messages_v2, simplify pydantic code  with vercel utils

--- marimo/_utils/assert_never.py
+++ marimo/_utils/assert_never.py
@@ -3,14 +3,6 @@ from __future__ import annotations
 
 from typing import NoReturn
 
-from marimo import _loggers
-
-LOGGER = _loggers.marimo_logger()
-
 
 def assert_never(value: NoReturn) -> NoReturn:
     raise AssertionError(f"Unhandled value: {value} ({type(value).__name__})")
-
-
-def log_never(value: NoReturn) -> None:
-    LOGGER.warning("Unexpected value: %s (%s)", value, type(value).__name__)
