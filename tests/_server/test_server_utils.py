570eadd71ee37cb7ee6c48df31c859a57d8c2647
Myles Scolnick <myles@marimo.io>
2025-12-19 11:22:11 -0500

    fix

--- tests/_server/test_server_utils.py
+++ tests/_server/test_server_utils.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 from marimo._server.api.utils import parse_title
 
 
-# The function to be tested
 def test_parse_title() -> None:
     assert parse_title(None) == "marimo"
     assert parse_title("example_file.txt") == "example file"
