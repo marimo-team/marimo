9b70ac7d2dd77ae0e277ef791fcce0c077414c2c
Akshay Agrawal <akshay@marimo.io>
2026-01-06 10:56:34 -0800

    Smoke test for PDF export

---
+++ marimo/_smoke_tests/pdf_export/basic_example.py
@@ -0,0 +1,58 @@
+import marimo
+
+__generated_with = "0.18.4"
+app = marimo.App(auto_download=["ipynb"])
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import matplotlib.pyplot as plt
+    import plotly.express as px
+    return mo, plt, px
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md("""
+    This notebook contains:
+
+    - Markdown
+    - a marimo UI element
+    - a Plotly scatter plot
+    - a matplotlib plot
+    - a dataframe output
+    """)
+    return
+
+
+@app.cell
+def _(mo):
+    mo.ui.text(label="$f(x)$")
+    return
+
+
+@app.cell
+def _(px):
+    df = px.data.iris()
+    fig = px.scatter(
+        df, x="sepal_width", y="sepal_length", color="species", symbol="species"
+    )
+    fig
+    return (df,)
+
+
+@app.cell
+def _(df, plt):
+    plt.scatter(x=df["sepal_width"], y=df["sepal_length"])
+    return
+
+
+@app.cell
+def _(df):
+    df
+    return
+
+
+if __name__ == "__main__":
+    app.run()
