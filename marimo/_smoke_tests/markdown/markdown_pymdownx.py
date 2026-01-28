05b00213ab6d3f273fc50d40492d54bf560663a7
Trevor Manz <trevor.j.manz@gmail.com>
2026-01-28 09:57:01 -0500

    Update pymdownx admonition smoke tests with new syntax (#8028)
    Closes #8027

--- marimo/_smoke_tests/markdown/markdown_pymdownx.py
+++ marimo/_smoke_tests/markdown/markdown_pymdownx.py
@@ -1,245 +1,222 @@
 import marimo
 
-__generated_with = "0.15.5"
+__generated_with = "0.19.6"
 app = marimo.App(width="medium")
 
 
 @app.cell
 def _(mo):
-    mo.md(
-        r"""
-        ## Task List
+    mo.md(r"""
+    ## Task List
 
-        - item
-        -   [X] item 1
-            *   [X] item A
-            *   [ ] item B
-                more text
-                +   [x] item a
-                +   [ ] item b
-                +   [x] item c
-            *   [X] item C
-            *   non item
-        -   [ ] item 2
-        -   [ ] item 3
-        """
-    )
+    - item
+    -   [X] item 1
+        *   [X] item A
+        *   [ ] item B
+            more text
+            +   [x] item a
+            +   [ ] item b
+            +   [x] item c
+        *   [X] item C
+        *   non item
+    -   [ ] item 2
+    -   [ ] item 3
+    """)
     return
 
 
 @app.cell
 def _(mo):
-    mo.md(
-        r"""
-        ## Base 64
+    mo.md(r"""
+    ## Base 64
 
-        ![picture](../../docs/_static/docs-settings.png)
-        """
-    )
+    ![picture](../../docs/_static/docs-settings.png)
+    """)
     return
 
 
 @app.cell
 def _(mo):
-    mo.md(
-        r"""
-        ## Admonitions
+    mo.md("""
+    ## Admonitions
 
-        !!! important ""
-            This is an admonition box without a title.
-        """
-    )
+    /// important |
+    This is an admonition box without a title.
+    ///
+    """)
     return
 
 
 @app.cell
 def _(mo):
-    mo.md(
-        r"""
-        ## Caption
+    mo.md(r"""
+    ## Caption
 
-        Fruit      | Amount
-        ---------- | ------
-        Apple      | 20
-        Peach      | 10
-        Banana     | 3
-        Watermelon | 1
+    Fruit      | Amount
+    ---------- | ------
+    Apple      | 20
+    Peach      | 10
+    Banana     | 3
+    Watermelon | 1
 
-        /// caption
-        Fruit Count
-        ///
-        """
-    )
+    /// caption
+    Fruit Count
+    ///
+    """)
     return
 
 
 @app.cell
 def _(mo):
-    mo.md(
-        r"""
-        ## Tabs
+    mo.md(r"""
+    ## Tabs
 
-        /// tab | Tab 1 title
-        Tab 1 content
-        ///
+    /// tab | Tab 1 title
+    Tab 1 content
+    ///
 
-        /// tab | Tab 2 title
-        Tab 2 content
-        ///
-        """
-    )
+    /// tab | Tab 2 title
+    Tab 2 content
+    ///
+    """)
     return
 
 
 @app.cell
 def _(mo):
-    mo.md(
-        """
-        ## Details
+    mo.md("""
+    ## Details
 
-        /// details | Basic details
-        This is a basic details section
-        ///
+    /// details | Basic details
+    This is a basic details section
+    ///
 
-        /// details | Info details
-            type: info
+    /// details | Info details
+        type: info
 
-        This shows important information
-        ///
+    This shows important information
+    ///
 
-        /// details | Warning details  
-            type: warn
+    /// details | Warning details
+        type: warn
 
-        This highlights something to watch out for
-        ///
+    This highlights something to watch out for
+    ///
 
-        /// details | Danger details
-            type: danger
+    /// details | Danger details
+        type: danger
 
-        This indicates a critical warning or dangerous situation
-        ///
+    This indicates a critical warning or dangerous situation
+    ///
 
-        /// details | Success details
-            type: success
+    /// details | Success details
+        type: success
 
-        This indicates a successful outcome or positive note
-        ///
-        """
-    )
+    This indicates a successful outcome or positive note
+    ///
+    """)
     return
 
 
 @app.cell
 def _(mo):
-    mo.md(
-        r"""
-        ## Nested em
+    mo.md(r"""
+    ## Nested em
 
-        This * won't emphasize *
+    This * won't emphasize *
 
-        This *will emphasize*
+    This *will emphasize*
 
-        ***I'm italic and bold* I am just bold.**
+    ***I'm italic and bold* I am just bold.**
 
-        ***I'm bold and italic!** I am just italic.*
-        """
-    )
+    ***I'm bold and italic!** I am just italic.*
+    """)
     return
 
 
 @app.cell
 def _(mo):
-    mo.md(
-        r"""
-        ## Critic
+    mo.md(r"""
+    ## Critic
 
-        Here is some {--*incorrect*--} Markdown.  I am adding this{++ here++}.  Here is some more {--text
-         that I am removing--}text.  And here is even more {++text that I 
-         am ++}adding.{~~
+    Here is some {--*incorrect*--} Markdown.  I am adding this{++ here++}.  Here is some more {--text
+     that I am removing--}text.  And here is even more {++text that I
+     am ++}adding.{~~
 
-        ~>  ~~}Paragraph was deleted and replaced with some spaces.{~~  ~>
+    ~>  ~~}Paragraph was deleted and replaced with some spaces.{~~  ~>
 
-        ~~}Spaces were removed and a paragraph was added.
+    ~~}Spaces were removed and a paragraph was added.
 
-        And here is a comment on {==some
-         text==}{>>This works quite well. I just wanted to comment on it.<<}. Substitutions {~~is~>are~~} great!
+    And here is a comment on {==some
+     text==}{>>This works quite well. I just wanted to comment on it.<<}. Substitutions {~~is~>are~~} great!
 
-        General block handling.
+    General block handling.
 
-        {--
+    {--
 
+    * test remove
+    * test remove
+    * test remove
         * test remove
-        * test remove
-        * test remove
-            * test remove
-        * test remove
+    * test remove
 
-        --}
+    --}
 
-        {++
+    {++
 
+    * test add
+    * test add
+    * test add
         * test add
-        * test add
-        * test add
-            * test add
-        * test add
+    * test add
 
-        ++}
-        """
-    )
+    ++}
+    """)
     return
 
 
 @app.cell
 def _(mo):
-    mo.md(
-        r"""
-        ## Emoji
+    mo.md(r"""
+    ## Emoji
 
-        :smile: :heart: :thumbsup:
-        """
-    )
+    :smile: :heart: :thumbsup:
+    """)
     return
 
 
 @app.cell
 def _(mo):
-    mo.md(
-        r"""
-        ## Keys
+    mo.md(r"""
+    ## Keys
 
-        ++ctrl+alt+delete++
-        """
-    )
+    ++ctrl+alt+delete++
+    """)
     return
 
 
 @app.cell
 def _(mo):
-    mo.md(
-        r"""
-        ## Magic link
+    mo.md(r"""
+    ## Magic link
 
-        - Just paste links directly in the document like this: https://google.com.
-        - Or even an email address: fake.email@email.com.
-        """
-    )
+    - Just paste links directly in the document like this: https://google.com.
+    - Or even an email address: fake.email@email.com.
+    """)
     return
 
 
 @app.cell
 def _(mo):
-    mo.md(
-        r"""
-        ## Subscripts and strikethrough
+    mo.md(r"""
+    ## Subscripts and strikethrough
 
-        ~~Delete me~~
+    ~~Delete me~~
 
-        CH~3~CH~2~OH
+    CH~3~CH~2~OH
 
-        text~a\ subscript~
-        """
-    )
+    text~a\ subscript~
+    """)
     return
 
 
