e4211a66d594c829fffececa17b4a62fb351c49a
Andrew Soon <andrewsoonqn@gmail.com>
2026-02-16 12:27:21 +0800

    Add sum and average stats

--- frontend/src/components/data-table/range-focus/cell-selection-stats.tsx
+++ frontend/src/components/data-table/range-focus/cell-selection-stats.tsx
@@ -1,10 +1,18 @@
 /* Copyright 2026 Marimo. All rights reserved. */
 
+import type { Table } from "@tanstack/react-table";
 import { useAtomValue } from "jotai";
 import { cn } from "@/utils/cn";
-import { selectedCellsAtom } from "./atoms";
+import { type SelectedCells, selectedCellsAtom } from "./atoms";
+import { getNumericValuesFromSelectedCells } from "./utils";
 
-export const CellSelectionStats = ({ className }: { className?: string }) => {
+export const CellSelectionStats = <TData,>({
+  table,
+  className,
+}: {
+  table: Table<TData>;
+  className?: string;
+}) => {
   const selectedCells = useAtomValue(selectedCellsAtom);
 
   if (selectedCells.size < 2) {
@@ -18,9 +26,56 @@ export const CellSelectionStats = ({ className }: { className?: string }) => {
         className,
       )}
     >
-      <span>Count: {selectedCells.size}</span>
-      <span>Sum: --</span>
-      <span>Avg: --</span>
+      <CountStat selectedCells={selectedCells} />
+      <SumStat table={table} selectedCells={selectedCells} />
+      <AverageStat table={table} selectedCells={selectedCells} />
     </div>
   );
+};
+
+const StatSpan = (statName: string, statValue: number) => {
+  return (
+    <span>
+      {statName}: {statValue}
+    </span>
+  );
+};
+
+const CountStat = ({ selectedCells }: { selectedCells: SelectedCells }) => {
+  return StatSpan("Count", selectedCells.size);
+};
+
+const SumStat = <TData,>({
+  table,
+  selectedCells,
+}: {
+  table: Table<TData>;
+  selectedCells: SelectedCells;
+}) => {
+  const numericValues = getNumericValuesFromSelectedCells(table, selectedCells);
+  if (numericValues.length === 0) {
+    return null;
+  }
+
+  const sum = numericValues.reduce((acc, n) => acc + n, 0);
+  const sumRounded = Number(sum.toFixed(8));
+  return StatSpan("Sum", sumRounded);
+};
+
+const AverageStat = <TData,>({
+  table,
+  selectedCells,
+}: {
+  table: Table<TData>;
+  selectedCells: SelectedCells;
+}) => {
+  const numericValues = getNumericValuesFromSelectedCells(table, selectedCells);
+  if (numericValues.length === 0) {
+    return null;
+  }
+
+  const average =
+    numericValues.reduce((acc, n) => acc + n, 0) / numericValues.length;
+  const averageRounded = Number(average.toFixed(8));
+  return StatSpan("Average", averageRounded);
 };
