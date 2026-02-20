ccaf85fbdd3d3499607a52baa446d30f8e8f123c
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2026-02-20 16:08:18 -0600

    feat: Expose FileState, DirectoryState, and PathState in mo.watch

--- marimo/_runtime/watch/__init__.py
+++ marimo/_runtime/watch/__init__.py
@@ -1,7 +1,8 @@
 # Copyright 2026 Marimo. All rights reserved.
 
-from marimo._runtime.watch._directory import directory
-from marimo._runtime.watch._file import file
+from marimo._runtime.watch._directory import DirectoryState, directory
+from marimo._runtime.watch._file import FileState, file
+from marimo._runtime.watch._path import PathState
 
 # NB. _runtime/reload captures module level changes and
 # marimo/_server/sessions.py captures notebook level changes.
@@ -9,4 +10,7 @@ from marimo._runtime.watch._file import file
 __all__ = [
     "file",
     "directory",
+    "FileState",
+    "DirectoryState",
+    "PathState",
 ]
