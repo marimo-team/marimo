38cb2294abcfd288d0795589e7b6d380b33e1d94
Shahmir Varqha <sham9871@gmail.com>
2026-02-25 23:15:53 +0800

    css change

--- frontend/src/components/data-table/data-table.tsx
+++ frontend/src/components/data-table/data-table.tsx
@@ -324,7 +324,10 @@ const DataTableInternal = <TData,>({
             />
           </Table>
         </div>
-        <CellSelectionStats table={table} />
+        <CellSelectionStats
+          table={table}
+          className="px-2 pt-1 mb-0.5 ml-auto"
+        />
       </CellSelectionProvider>
       <TableActions
         enableSearch={enableSearch}
