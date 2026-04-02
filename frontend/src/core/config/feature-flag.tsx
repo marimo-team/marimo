4c48788651c9a0c58e6a71bd92d7b3e077e92e79
Shahmir Varqha <sham9871@gmail.com>
2026-04-02 13:08:20 +0800

    remove feat flag

--- frontend/src/core/config/feature-flag.tsx
+++ frontend/src/core/config/feature-flag.tsx
@@ -11,7 +11,6 @@ export interface ExperimentalFeatures {
   rtc_v2: boolean;
   cache_panel: boolean;
   external_agents: boolean;
-  storage_inspector: boolean;
   // Add new feature flags here
 }
 
@@ -21,7 +20,6 @@ const defaultValues: ExperimentalFeatures = {
   rtc_v2: false,
   cache_panel: false,
   external_agents: import.meta.env.DEV,
-  storage_inspector: true,
 };
 
 export function getFeatureFlag<T extends keyof ExperimentalFeatures>(
