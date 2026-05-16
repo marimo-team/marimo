15566b59b27d42d60c558162a5afb498052bbcfe
Myles Scolnick <myles@marimo.io>
2026-05-16 09:04:40 -0700

    test: add data-testid to top-level chrome elements

--- frontend/src/components/editor/chrome/wrapper/panels.tsx
+++ frontend/src/components/editor/chrome/wrapper/panels.tsx
@@ -3,7 +3,10 @@ import type { PropsWithChildren } from "react";
 
 export const PanelsWrapper: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="flex flex-col flex-1 overflow-hidden absolute inset-0 print:relative">
+    <div
+      data-testid="chrome-wrapper"
+      className="flex flex-col flex-1 overflow-hidden absolute inset-0 print:relative"
+    >
       {children}
     </div>
   );
