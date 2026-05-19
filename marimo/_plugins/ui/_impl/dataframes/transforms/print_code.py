ff063e58727f99a66f0f105bd68913de24971cdd
Shamik <39588365+Shamik-07@users.noreply.github.com>
2026-05-19 19:48:44 -0400

    fix: changing the expand dict print code function for pandas to using json normalise following the handlers code.

--- marimo/_plugins/ui/_impl/dataframes/transforms/print_code.py
+++ marimo/_plugins/ui/_impl/dataframes/transforms/print_code.py
@@ -222,8 +222,11 @@ def python_print_pandas(
 
     elif transform.type == TransformType.EXPAND_DICT:
         column_id = _as_literal(transform.column_id)
-        args = f"{df_name}.pop({column_id}).values.tolist()"
-        return f"{df_name}.join(pd.DataFrame({args}))"
+        return (
+            f"{df_name}.join("
+            f"pd.json_normalize({df_name}.pop({column_id})).set_axis({df_name}.index, axis=0)"
+            f")"
+        )
 
     elif transform.type == TransformType.UNIQUE:
         column_ids = transform.column_ids
